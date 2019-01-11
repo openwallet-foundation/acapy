30f4bd9b3624c4bb5ef7141abfda2bbcdf0d9436
Andrew Whitehead <cywolf@gmail.com>
2019-01-11 15:45:14 -0800

    add initial storage (non-secrets) API and test in-memory implementation
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

---
+++ agent/indy_catalyst_agent/storage/record.py
@@ -0,0 +1,15 @@
+"""
+Record instance stored and searchable by BaseStorage implementation
+"""
+
+from collections import namedtuple
+from uuid import uuid4
+
+class StorageRecord(namedtuple('StorageRecord', 'type value tags id')):
+    __slots__ = ()
+    def __new__(cls, type, value, tags: dict = None, id: str = None):
+        if not id:
+            id = uuid4().hex
+        if not tags:
+            tags = {}
+        return super(cls, StorageRecord).__new__(cls, type, value, tags, id)
