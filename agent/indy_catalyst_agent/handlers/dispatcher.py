12c2e84a80def35af00c91d0ec2234d87b567984
Nicholas Rempel <nbrempel@gmail.com>
2019-01-14 11:43:43 -0800

    Adds dispatcher class stub
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

---
+++ agent/indy_catalyst_agent/handlers/dispatcher.py
@@ -0,0 +1,8 @@
+"""
+The dispatcher is responsible for coordinating data flow between handlers, providing lifecycle hook callbacks
+storing state for message threads, etc.
+"""
+
+class Dispatcher:
+
+    pass
