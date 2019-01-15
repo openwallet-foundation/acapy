0f7e834fbb18eea7bbd2b8213d258f34e890ac5d
Nicholas Rempel <nbrempel@gmail.com>
2019-01-15 14:38:21 -0800

    Add some architecture scaffold
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- agent/indy_catalyst_agent/dispatcher.py
+++ agent/indy_catalyst_agent/dispatcher.py
@@ -1,15 +1,24 @@
 """
-The dispatcher is responsible for coordinating data flow between handlers, providing lifecycle hook callbacks
-storing state for message threads, etc.
+The dispatcher is responsible for coordinating data flow between handlers, providing lifecycle
+hook callbacks storing state for message threads, etc.
 """
 
 import logging
 
+from .lifecycle import Lifecycle
 from .storage.base import BaseStorage
 
 
 class Dispatcher:
-    def __init__(self, storage: BaseStorage):
+    def __init__(self, storage: BaseStorage):  # TODO: take in wallet impl as well
         self.logger = logging.getLogger(__name__)
         self.storage = storage
+
+    def dispatch(self, message):
+        # TODO:
+        # Create an instance of some kind of "ThreadState" or "Context"
+        # using a thread id found in the message data. Messages do not
+        # yet have the notion of threading
+        context = {}
+        message.handler.handle(Lifecycle, context)
 
--- agent/indy_catalyst_agent/messaging/agent_message.py
+++ agent/indy_catalyst_agent/messaging/agent_message.py
@@ -4,4 +4,4 @@ from abc import ABC, abstractproperty
 class AgentMessage(ABC):
     @abstractproperty
     def _type(self) -> str:
-        raise NotImplementedError() # pragma: no cover
+        pass
