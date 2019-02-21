7b5a0ea4bdf229ecfc35a760b4d0ab3ab9b33bae
Andrew Whitehead <cywolf@gmail.com>
2019-02-20 23:02:17 -0700

    minor cleanup
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- agent/indy_catalyst_agent/wallet/error.py
+++ agent/indy_catalyst_agent/wallet/error.py
@@ -8,16 +8,10 @@ from ..error import BaseError
 class WalletError(BaseError):
     """General wallet exception"""
 
-    pass
-
 
 class WalletNotFoundError(WalletError):
     """Record not found exception"""
 
-    pass
-
 
 class WalletDuplicateError(WalletError):
     """Duplicate record exception"""
-
-    pass
