132d8face66f4529cf9dece63f22475e158bfed6
Andrew Whitehead <cywolf@gmail.com>
2019-02-21 11:59:20 -0700

    support optional trust ping properties
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- agent/indy_catalyst_agent/messaging/trustping/messages/ping.py
+++ agent/indy_catalyst_agent/messaging/trustping/messages/ping.py
@@ -2,12 +2,13 @@
 Represents a trust ping message
 """
 
+from marshmallow import fields
+
 from ...agent_message import AgentMessage, AgentMessageSchema
 from ..message_types import PING
 
 HANDLER_CLASS = (
-    "indy_catalyst_agent.messaging.trustping."
-    + "handlers.ping_handler.PingHandler"
+    "indy_catalyst_agent.messaging.trustping." + "handlers.ping_handler.PingHandler"
 )
 
 
@@ -17,7 +18,17 @@ class Ping(AgentMessage):
         message_type = PING
         schema_class = "PingSchema"
 
+    def __init__(
+        self, *, response_requested: bool = None, comment: str = None, **kwargs
+    ):
+        super(Ping, self).__init__(**kwargs)
+        self.comment = comment
+        self.response_requested = response_requested
+
 
 class PingSchema(AgentMessageSchema):
     class Meta:
         model_class = Ping
+
+    response_requested = fields.Bool(default=True, required=False)
+    comment = fields.Str(required=False, allow_none=True)
--- agent/indy_catalyst_agent/messaging/trustping/messages/ping_response.py
+++ agent/indy_catalyst_agent/messaging/trustping/messages/ping_response.py
@@ -2,6 +2,8 @@
 Represents an response to a trust ping message
 """
 
+from marshmallow import fields
+
 from ...agent_message import AgentMessage, AgentMessageSchema
 from ..message_types import PING_RESPONSE
 
@@ -17,7 +19,13 @@ class PingResponse(AgentMessage):
         message_type = PING_RESPONSE
         schema_class = "PingResponseSchema"
 
+    def __init__(self, *, comment: str = None, **kwargs):
+        super(PingResponse, self).__init__(**kwargs)
+        self.comment = comment
+
 
 class PingResponseSchema(AgentMessageSchema):
     class Meta:
         model_class = PingResponse
+
+    comment = fields.Str(required=False, allow_none=True)
