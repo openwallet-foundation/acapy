ed363ffd369201421e2b1956a24bfa4aaf0963d0
Andrew Whitehead <cywolf@gmail.com>
2019-03-04 15:13:59 -0800

    add routes for creating, receiving, accepting connection invitations
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- agent/indy_catalyst_agent/messaging/connections/routes.py
+++ agent/indy_catalyst_agent/messaging/connections/routes.py
@@ -1,10 +1,15 @@
 """Connection handling admin routes."""
 
 from aiohttp import web
-from aiohttp_apispec import docs, response_schema
+from aiohttp_apispec import docs, request_schema, response_schema
 
 from marshmallow import fields, Schema
 
+from .manager import ConnectionManager
+from .messages.connection_invitation import (
+    ConnectionInvitation,
+    ConnectionInvitationSchema,
+)
 from .models.connection_record import ConnectionRecord, ConnectionRecordSchema
 from ...storage.error import StorageNotFoundError
 
@@ -13,6 +18,14 @@ class ConnectionListSchema(Schema):
     """Result schema for connection list."""
 
     results = fields.List(fields.Nested(ConnectionRecordSchema()))
+
+
+class InvitationResultSchema(Schema):
+    """Result schema for a new connection invitation."""
+
+    connection_id = fields.Str()
+    invitation = fields.Nested(ConnectionInvitationSchema())
+    invitation_url = fields.Str()
 
 
 @docs(
@@ -107,8 +120,111 @@ async def connections_retrieve(request: web.BaseRequest):
     return web.json_response(record.serialize())
 
 
+@docs(tags=["connection"], summary="Create a new connection invitation")
+@response_schema(InvitationResultSchema(), 200)
+async def connections_create_invitation(request: web.BaseRequest):
+    """
+    Request handler for creating a new connection invitation.
+
+    Args:
+        request: aiohttp request object
+
+    Returns:
+        The connection invitation details
+
+    """
+    context = request.app["request_context"]
+    connection_mgr = ConnectionManager(context)
+    connection, invitation = await connection_mgr.create_invitation()
+    result = {
+        "connection_id": connection.connection_id,
+        "invitation": invitation.serialize(),
+        "invitation_url": invitation.to_url(),
+    }
+    return web.json_response(result)
+
+
+@docs(tags=["connection"], summary="Receive a new connection invitation")
+@request_schema(ConnectionInvitationSchema())
+@response_schema(ConnectionRecordSchema(), 200)
+async def connections_receive_invitation(request: web.BaseRequest):
+    """
+    Request handler for receiving a new connection invitation.
+
+    Args:
+        request: aiohttp request object
+
+    Returns:
+        The resulting connection record details
+
+    """
+    context = request.app["request_context"]
+    connection_mgr = ConnectionManager(context)
+    invitation_json = await request.json()
+    invitation = ConnectionInvitation.deserialize(invitation_json)
+    connection = await connection_mgr.receive_invitation(invitation)
+    return web.json_response(connection.serialize())
+
+
+@docs(
+    tags=["connection"],
+    summary="Accept a stored connection invitation",
+    parameters=[
+        {
+            "name": "my_endpoint",
+            "in": "query",
+            "schema": {"type": "string"},
+            "required": False,
+        },
+        {
+            "name": "my_label",
+            "in": "query",
+            "schema": {"type": "string"},
+            "required": False,
+        },
+    ],
+)
+@response_schema(ConnectionRecordSchema(), 200)
+async def connections_accept_invitation(request: web.BaseRequest):
+    """
+    Request handler for accepting a stored connection invitation.
+
+    Args:
+        request: aiohttp request object
+
+    Returns:
+        The resulting connection record details
+
+    """
+    context = request.app["request_context"]
+    outbound_handler = request.app["outbound_message_router"]
+    connection_id = request.match_info["id"]
+    try:
+        connection = await ConnectionRecord.retrieve_by_id(
+            context.storage, connection_id
+        )
+    except StorageNotFoundError:
+        return web.HTTPNotFound()
+    connection_mgr = ConnectionManager(context)
+    my_label = request.query.get("my_label") or None
+    my_endpoint = request.query.get("my_endpoint") or None
+    request = await connection_mgr.create_request(connection, my_label, my_endpoint)
+    target = await connection_mgr.get_connection_target(connection)
+    await outbound_handler(request, target)
+    return web.json_response(connection.serialize())
+
+
 async def register(app: web.Application):
     """Register routes."""
 
     app.add_routes([web.get("/connections", connections_list)])
     app.add_routes([web.get("/connections/{id}", connections_retrieve)])
+    app.add_routes(
+        [web.post("/connections/create-invitation", connections_create_invitation)]
+    )
+    app.add_routes(
+        [web.post("/connections/receive-invitation", connections_receive_invitation)]
+    )
+    app.add_routes(
+        [web.post("/connections/{id}/accept-invitation", connections_accept_invitation)]
+    )
