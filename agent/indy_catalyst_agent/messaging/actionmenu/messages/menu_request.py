3ef0a53b138192a455c2145987cfeec6374ba742
Andrew Whitehead <cywolf@gmail.com>
2019-04-05 16:59:57 -0700

    initial action-menu message classes and models
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

---
+++ agent/indy_catalyst_agent/messaging/actionmenu/messages/menu_request.py
@@ -0,0 +1,35 @@
+"""Represents a request for an action menu."""
+
+from ...agent_message import AgentMessage, AgentMessageSchema
+from ..message_types import MENU_REQUEST
+
+HANDLER_CLASS = (
+    "indy_catalyst_agent.messaging.actionmenu.handlers.menu_request_handler"
+    + ".MenuRequestHandler"
+)
+
+
+class MenuRequest(AgentMessage):
+    """Class representing a request for an action menu."""
+
+    class Meta:
+        """Metadata for action menu request."""
+
+        handler_class = HANDLER_CLASS
+        message_type = MENU_REQUEST
+        schema_class = "MenuRequestSchema"
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a menu request object.
+        """
+        super(MenuRequest, self).__init__(**kwargs)
+
+
+class MenuRequestSchema(AgentMessageSchema):
+    """MenuRequest schema class."""
+
+    class Meta:
+        """MenuRequest schema metadata."""
+
+        model_class = MenuRequest
---
+++ agent/indy_catalyst_agent/messaging/actionmenu/models/menu_option.py
@@ -0,0 +1,55 @@
+"""Record used to represent individual menu options in an action menu."""
+
+from marshmallow import fields
+
+from ....models.base import BaseModel, BaseModelSchema
+from .menu_form import MenuForm, MenuFormSchema
+
+
+class MenuOption(BaseModel):
+    """Instance of a menu option associated with an action menu."""
+
+    class Meta:
+        """Menu option metadata."""
+
+        schema_class = "MenuOptionSchema"
+
+    def __init__(
+        self,
+        *,
+        name: str = None,
+        title: str = None,
+        description: str = None,
+        disabled: bool = None,
+        form: MenuForm = None,
+    ):
+        """
+        Initialize a MenuOption instance.
+
+        Args:
+            name: The menu option name (unique ID)
+            title: The menu option title
+            description: Additional descriptive text for the menu option
+            disabled: If the option should be shown as disabled
+            form: A form to display when the option is selected
+        """
+        self.name = name
+        self.title = title
+        self.description = description
+        self.disabled = disabled
+        self.form = form
+
+
+class MenuOptionSchema(BaseModelSchema):
+    """MenuOption schema."""
+
+    class Meta:
+        """MenuOptionSchema metadata."""
+
+        model_class = MenuOption
+
+    name = fields.Str(required=True)
+    title = fields.Str(required=True)
+    description = fields.Str(required=False)
+    disabled = fields.Bool(required=False)
+    form = fields.Nested(MenuFormSchema(), required=False)
