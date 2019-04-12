fcd9964c7ec84c4ed900b30d271981cbe4439fe3
Nicholas Rempel <nbrempel@gmail.com>
2019-04-12 15:56:54 -0700

    Add retries to webhooks
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- agent/indy_catalyst_agent/messaging/util.py
+++ agent/indy_catalyst_agent/messaging/util.py
@@ -15,4 +15,8 @@ async def send_webhook(topic, payload, retries=5):
     async with aiohttp.ClientSession() as session:
         full_webhook_url = f"{WEBHOOK_URL}/topic/{topic}"
         LOGGER.info(f"Sending webhook to {full_webhook_url}")
-        await session.post(full_webhook_url, json=payload)
+        try:
+            await session.post(full_webhook_url, json=payload)
+        except aiohttp.client_exceptions.ClientConnectorError:
+            if retries > 0:
+                await send_webhook(topic, payload, retries - 1)
