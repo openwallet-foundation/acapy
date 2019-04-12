be28dfc5e4be67e688ece24a41b23c388e76073a
Nicholas Rempel <nbrempel@gmail.com>
2019-04-12 15:59:41 -0700

    Add sleep between retries
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- agent/indy_catalyst_agent/messaging/util.py
+++ agent/indy_catalyst_agent/messaging/util.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import logging
 
@@ -16,7 +17,10 @@ async def send_webhook(topic, payload, retries=5):
         full_webhook_url = f"{WEBHOOK_URL}/topic/{topic}"
         LOGGER.info(f"Sending webhook to {full_webhook_url}")
         try:
-            await session.post(full_webhook_url, json=payload)
-        except aiohttp.client_exceptions.ClientConnectorError:
+            response = await session.post(full_webhook_url, json=payload)
+            if response.status < 200 or response.status > 299:
+                raise Exception()
+        except:
             if retries > 0:
+                await asyncio.sleep(5)
                 await send_webhook(topic, payload, retries - 1)
