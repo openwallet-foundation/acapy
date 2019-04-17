650fd3dc8a6a3cdcc02545e31f76aa739b139001
Andrew Whitehead <cywolf@gmail.com>
2019-04-16 22:23:27 -0700

    add admin method for sending a menu to a connection
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- agent/indy_catalyst_agent/messaging/actionmenu/routes.py
+++ agent/indy_catalyst_agent/messaging/actionmenu/routes.py
@@ -9,8 +9,10 @@ from marshmallow import fields, Schema
 
 from ..connections.manager import ConnectionManager
 from ..connections.models.connection_record import ConnectionRecord
+from .messages.menu import Menu
 from .messages.menu_request import MenuRequest
 from .messages.perform import Perform
+from .models.menu_option import MenuOptionSchema
 from ...storage.error import StorageNotFoundError
 from .util import retrieve_connection_menu, save_connection_menu
 
@@ -22,6 +24,21 @@ class PerformRequestSchema(Schema):
 
     name = fields.Str()
     params = fields.Dict(required=False)
+
+
+class MenuJsonSchema(Schema):
+    """Matches MenuSchema but without the inherited AgentMessage properties."""
+
+    title = fields.Str(required=False)
+    description = fields.Str(required=False)
+    errormsg = description = fields.Str(required=False)
+    options = fields.List(fields.Nested(MenuOptionSchema), required=True)
+
+
+class SendMenuSchema(Schema):
+    """Request schema for sending a menu to a connection."""
+
+    menu = fields.Nested(MenuJsonSchema(), required=True)
 
 
 @docs(
@@ -66,7 +83,7 @@ async def actionmenu_fetch(request: web.BaseRequest):
 
 
 @docs(tags=["action-menu"], summary="Perform an action associated with the active menu")
-@request_schema(PerformRequestSchema)
+@request_schema(PerformRequestSchema())
 async def actionmenu_perform(request: web.BaseRequest):
     """
     Request handler for performing a menu action.
@@ -128,6 +145,41 @@ async def actionmenu_request(request: web.BaseRequest):
     return web.HTTPForbidden()
 
 
+@docs(tags=["action-menu"], summary="Send an action menu to a connection")
+@request_schema(SendMenuSchema())
+async def actionmenu_send(request: web.BaseRequest):
+    """
+    Request handler for requesting a menu from the connection target.
+
+    Args:
+        request: aiohttp request object
+
+    """
+    context = request.app["request_context"]
+    connection_id = request.match_info["id"]
+    connection_mgr = ConnectionManager(context)
+    outbound_handler = request.app["outbound_message_router"]
+    menu_json = await request.json()
+    msg = Menu(**menu_json["menu"])
+
+    try:
+        connection = await ConnectionRecord.retrieve_by_id(
+            context.storage, connection_id
+        )
+    except StorageNotFoundError:
+        LOGGER.debug(
+            "Connection not found for action menu send request: %s", connection_id
+        )
+        return web.HTTPNotFound()
+
+    if connection.state == "active":
+        target = await connection_mgr.get_connection_target(connection)
+        await outbound_handler(msg, target)
+        return web.HTTPOk()
+
+    return web.HTTPForbidden()
+
+
 async def register(app: web.Application):
     """Register routes."""
 
@@ -137,5 +189,6 @@ async def register(app: web.Application):
             web.post("/action-menu/{id}/fetch", actionmenu_fetch),
             web.post("/action-menu/{id}/perform", actionmenu_perform),
             web.post("/action-menu/{id}/request", actionmenu_request),
+            web.post("/connections/{id}/send-menu", actionmenu_send),
         ]
     )
