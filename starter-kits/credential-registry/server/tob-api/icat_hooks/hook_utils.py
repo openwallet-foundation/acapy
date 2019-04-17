52b0e8545f08a87dde423f85793bbcb20df3be9f
Emiliano Suñé <emiliano.sune@gmail.com>
2019-04-17 11:28:12 -0700

    Check registration expiry date on hook fire event, and deactivate hook if expired.
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- starter-kits/credential-registry/server/tob-api/icat_hooks/hook_utils.py
+++ starter-kits/credential-registry/server/tob-api/icat_hooks/hook_utils.py
@@ -1,5 +1,8 @@
 from .models.CredentialHook import CredentialHook
+from .models.HookUser import HookUser
 from .models.Subscription import Subscription
+
+import datetime
 
 
 def find_and_fire_hook(event_name, instance, **kwargs):
@@ -7,7 +10,7 @@ def find_and_fire_hook(event_name, instance, **kwargs):
 
     hooks = CredentialHook.objects.filter(**filters)
     for hook in hooks:
-        if hook.is_active:
+        if hook.is_active and is_registration_valid(hook):
             send_hook = False
             # TODO find subscription(s) related to this hook
             subscriptions = Subscription.objects.filter(hook=hook).all()
@@ -35,3 +38,20 @@ def find_and_fire_hook(event_name, instance, **kwargs):
             # TODO logic around whether we hook or not
             if send_hook:
                 hook.deliver_hook(instance)
+
+
+def is_registration_valid(hook: CredentialHook):
+    is_valid = True
+    hook_user = HookUser.objects.get(id=hook.user_id)
+
+    if hook_user.registration_expiry < datetime.datetime.now().date():
+        is_valid = False
+        deactivate_hook(hook.id)
+
+    return is_valid
+
+
+def deactivate_hook(hook_id: int):
+    cred_hook = CredentialHook.objects.get(id=hook_id)
+    cred_hook.is_active = False
+    cred_hook.save()
