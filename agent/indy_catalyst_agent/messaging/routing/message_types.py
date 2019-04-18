ad8b2765aab65754636d3831a6cbc0428b4abb31
Andrew Whitehead <cywolf@gmail.com>
2019-04-17 20:51:40 -0700

    merge updated messages, models, and messaging tests
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- agent/indy_catalyst_agent/messaging/routing/message_types.py
+++ agent/indy_catalyst_agent/messaging/routing/message_types.py
@@ -8,12 +8,22 @@ FORWARD = f"{MESSAGE_FAMILY}/forward"
 GET_ROUTES = f"{MESSAGE_FAMILY}/get"
 ROUTES = f"{MESSAGE_FAMILY}/routes"
 
+ROUTE_QUERY_REQUEST = f"{MESSAGE_FAMILY}route-query-request"
+ROUTE_QUERY_RESPONSE = f"{MESSAGE_FAMILY}route-query-response"
+ROUTE_UPDATE_REQUEST = f"{MESSAGE_FAMILY}route-update-request"
+ROUTE_UPDATE_RESPONSE = f"{MESSAGE_FAMILY}route-update-response"
+
 MESSAGE_PACKAGE = "indy_catalyst_agent.messaging.routing.messages"
 
 MESSAGE_TYPES = {
-    CREATE_ROUTES: f"{MESSAGE_PACKAGE}.create_routes.CreateRoutes",
-    DELETE_ROUTES: f"{MESSAGE_PACKAGE}.delete_routes.DeleteRoutes",
-    FORWARD: f"{MESSAGE_PACKAGE}.forward.Forward",
-    GET_ROUTES: f"{MESSAGE_PACKAGE}.get_routes.GetRoutes",
-    ROUTES: f"{MESSAGE_PACKAGE}.routes.Routes",
+    # CREATE_ROUTES: f"{MESSAGE_PACKAGE}.create_routes.CreateRoutes",
+    # DELETE_ROUTES: f"{MESSAGE_PACKAGE}.delete_routes.DeleteRoutes",
+    # FORWARD: f"{MESSAGE_PACKAGE}.forward.Forward",
+    # GET_ROUTES: f"{MESSAGE_PACKAGE}.get_routes.GetRoutes",
+    # ROUTES: f"{MESSAGE_PACKAGE}.routes.Routes",
+    ROUTE_QUERY_REQUEST: f"{MESSAGE_PACKAGE}.route_query_request.RouteQueryRequest",
+    ROUTE_QUERY_RESPONSE: f"{MESSAGE_PACKAGE}.route_query_response.RouteQueryResponse",
+    ROUTE_UPDATE_REQUEST: f"{MESSAGE_PACKAGE}.route_update_request.RouteUpdateRequest",
+    ROUTE_UPDATE_RESPONSE: f"{MESSAGE_PACKAGE}.route_update_request."
+    "RouteUpdateResponse",
 }
---
+++ agent/indy_catalyst_agent/messaging/routing/messages/route_query_request.py
@@ -0,0 +1,49 @@
+"""Query existing forwarding routes."""
+
+from marshmallow import fields
+
+from ...agent_message import AgentMessage, AgentMessageSchema
+from ..message_types import ROUTE_QUERY_REQUEST
+from ..models.paginate import Paginate, PaginateSchema
+
+HANDLER_CLASS = (
+    "indy_catalyst_agent.messaging.routing.handlers"
+    + ".route_query_request_handler.RouteQueryRequestHandler"
+)
+
+
+class RouteQueryRequest(AgentMessage):
+    """Query existing routes from a routing agent."""
+
+    class Meta:
+        """RouteQueryRequest metadata."""
+
+        handler_class = HANDLER_CLASS
+        message_type = ROUTE_QUERY_REQUEST
+        schema_class = "RouteQueryRequestSchema"
+
+    def __init__(self, *, filter: dict = None, paginate: Paginate = None, **kwargs):
+        """
+        Initialize a RouteQueryRequest message instance.
+
+        Args:
+            filter: Filter results according to specific field values
+        """
+
+        super(RouteQueryRequest, self).__init__(**kwargs)
+        self.filter = filter
+        self.paginate = paginate
+
+
+class RouteQueryRequestSchema(AgentMessageSchema):
+    """RouteQueryRequest message schema used in serialization/deserialization."""
+
+    class Meta:
+        """RouteQueryRequestSchema metadata."""
+
+        model_class = RouteQueryRequest
+
+    filter = fields.Dict(
+        fields.Str(), fields.List(fields.Str()), required=False, allow_none=True
+    )
+    paginate = fields.Nested(PaginateSchema(), required=False, allow_none=True)
