6425df6beace4f82038baa2db760932a4ed25cdc
Nicholas Rempel <nbrempel@gmail.com>
2019-05-14 16:00:30 -0700

    Add tests for indy ledger implementation
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- agent/indy_catalyst_agent/ledger/tests/test_indy.py
+++ agent/indy_catalyst_agent/ledger/tests/test_indy.py
@@ -1,15 +1,175 @@
+import asyncio
 import json
 from unittest import mock
 
 from asynctest import TestCase as AsyncTestCase
 from asynctest import mock as async_mock
 
-from indy_catalyst_agent.ledger.indy import IndyLedger
+from indy_catalyst_agent.ledger.indy import (
+    IndyLedger,
+    GENESIS_TRANSACTION_PATH,
+    ClosedPoolError,
+    LedgerTransactionError,
+)
 
 
 class TestIndyLedger(AsyncTestCase):
-    def test_init(self):
+    @async_mock.patch("builtins.open")
+    def test_init(self, mock_open):
+        mock_open.return_value = async_mock.MagicMock()
+
         ledger = IndyLedger("name", "wallet", "genesis_transactions")
 
         assert ledger.name == "name"
         assert ledger.wallet == "wallet"
+
+        mock_open.assert_called_once_with(GENESIS_TRANSACTION_PATH, "w")
+        mock_open.return_value.__enter__.return_value.write.assert_called_once_with(
+            "genesis_transactions"
+        )
+
+    @async_mock.patch("indy.pool.set_protocol_version")
+    @async_mock.patch("indy.pool.create_pool_ledger_config")
+    @async_mock.patch("indy.pool.open_pool_ledger")
+    @async_mock.patch("indy.pool.close_pool_ledger")
+    async def test_aenter_aexit(
+        self, mock_close_pool, mock_open_ledger, mock_create_config, mock_set_proto
+    ):
+        ledger = IndyLedger("name", "wallet", "genesis_transactions")
+        async with ledger as l:
+            mock_set_proto.assert_called_once_with(2)
+            mock_create_config.assert_called_once_with(
+                "name", json.dumps({"genesis_txn": GENESIS_TRANSACTION_PATH})
+            )
+            mock_open_ledger.assert_called_once_with("name", "{}")
+            assert l == ledger
+            mock_close_pool.assert_not_called()
+            assert l.pool_handle == mock_open_ledger.return_value
+
+        mock_close_pool.assert_called_once()
+        assert ledger.pool_handle == None
+
+    @async_mock.patch("indy.pool.set_protocol_version")
+    @async_mock.patch("indy.pool.create_pool_ledger_config")
+    @async_mock.patch("indy.pool.open_pool_ledger")
+    @async_mock.patch("indy.pool.close_pool_ledger")
+    async def test_submit_pool_closed(
+        self, mock_close_pool, mock_open_ledger, mock_create_config, mock_set_proto
+    ):
+        ledger = IndyLedger("name", "wallet", "genesis_transactions")
+
+        with self.assertRaises(ClosedPoolError) as context:
+            await ledger._submit("{}")
+        assert "sign and submit request to closed pool" in str(context.exception)
+
+    @async_mock.patch("indy.pool.set_protocol_version")
+    @async_mock.patch("indy.pool.create_pool_ledger_config")
+    @async_mock.patch("indy.pool.open_pool_ledger")
+    @async_mock.patch("indy.pool.close_pool_ledger")
+    @async_mock.patch("indy.ledger.sign_and_submit_request")
+    async def test_submit_signed(
+        self,
+        mock_sign_submit,
+        mock_close_pool,
+        mock_open_ledger,
+        mock_create_config,
+        mock_set_proto,
+    ):
+
+        mock_did = async_mock.MagicMock()
+
+        future = asyncio.Future()
+        future.set_result(mock_did)
+
+        mock_sign_submit.return_value = '{"op": "REPLY"}'
+
+        mock_wallet = async_mock.MagicMock()
+        mock_wallet.get_public_did.return_value = future
+
+        ledger = IndyLedger("name", mock_wallet, "genesis_transactions")
+
+        async with ledger:
+            await ledger._submit("{}", True)
+
+            mock_wallet.get_public_did.assert_called_once_with()
+
+            mock_sign_submit.assert_called_once_with(
+                ledger.pool_handle, mock_wallet.handle, mock_did.did, "{}"
+            )
+
+    @async_mock.patch("indy.pool.set_protocol_version")
+    @async_mock.patch("indy.pool.create_pool_ledger_config")
+    @async_mock.patch("indy.pool.open_pool_ledger")
+    @async_mock.patch("indy.pool.close_pool_ledger")
+    @async_mock.patch("indy.ledger.submit_request")
+    async def test_submit_unsigned(
+        self,
+        mock_submit,
+        mock_close_pool,
+        mock_open_ledger,
+        mock_create_config,
+        mock_set_proto,
+    ):
+
+        mock_did = async_mock.MagicMock()
+
+        future = asyncio.Future()
+        future.set_result(mock_did)
+
+        mock_submit.return_value = '{"op": "REPLY"}'
+
+        mock_wallet = async_mock.MagicMock()
+        mock_wallet.get_public_did.return_value = future
+
+        ledger = IndyLedger("name", mock_wallet, "genesis_transactions")
+
+        async with ledger:
+            await ledger._submit("{}", False)
+
+            mock_wallet.get_public_did.assert_called_once_with()
+
+            mock_submit.assert_called_once_with(ledger.pool_handle, "{}")
+
+    @async_mock.patch("indy.pool.set_protocol_version")
+    @async_mock.patch("indy.pool.create_pool_ledger_config")
+    @async_mock.patch("indy.pool.open_pool_ledger")
+    @async_mock.patch("indy.pool.close_pool_ledger")
+    @async_mock.patch("indy.ledger.submit_request")
+    async def test_submit_rejected(
+        self,
+        mock_submit,
+        mock_close_pool,
+        mock_open_ledger,
+        mock_create_config,
+        mock_set_proto,
+    ):
+
+        mock_did = async_mock.MagicMock()
+
+        future = asyncio.Future()
+        future.set_result(mock_did)
+
+        mock_submit.return_value = '{"op": "REQNACK", "reason": "a reason"}'
+
+        mock_wallet = async_mock.MagicMock()
+        mock_wallet.get_public_did.return_value = future
+
+        ledger = IndyLedger("name", mock_wallet, "genesis_transactions")
+
+        async with ledger:
+            with self.assertRaises(LedgerTransactionError) as context:
+                await ledger._submit("{}", False)
+            assert "Ledger rejected transaction request" in str(context.exception)
+
+        mock_submit.return_value = '{"op": "REJECT", "reason": "another reason"}'
+
+        mock_wallet = async_mock.MagicMock()
+        mock_wallet.get_public_did.return_value = future
+
+        ledger = IndyLedger("name", mock_wallet, "genesis_transactions")
+
+        async with ledger:
+            with self.assertRaises(LedgerTransactionError) as context:
+                await ledger._submit("{}", False)
+            assert "Ledger rejected transaction request" in str(context.exception)
+
