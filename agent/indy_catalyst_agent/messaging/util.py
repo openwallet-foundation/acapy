65beff416baab773533b862e25727c4d9458526e
Andrew Whitehead <cywolf@gmail.com>
2019-05-17 15:11:44 -0700

    add standard date processing methods and related unit tests; update TimingDecorator to accept datetime arguments
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- agent/indy_catalyst_agent/messaging/util.py
+++ agent/indy_catalyst_agent/messaging/util.py
@@ -1,9 +1,11 @@
 """Utils for messages."""
 
 import asyncio
-import datetime
+from datetime import datetime, timedelta, timezone
 import os
 import logging
+from math import floor
+import re
 from typing import Union
 
 import aiohttp
@@ -32,9 +34,69 @@ async def send_webhook(topic, payload, retries=5):
                 await send_webhook(topic, payload, retries - 1)
 
 
-def time_now(datetime_format=False) -> Union[str, datetime.datetime]:
+def datetime_to_str(dt: Union[str, datetime]) -> str:
+    """Convert a datetime object to an indy-standard datetime string.
+
+    Args:
+        dt: May be a string or datetime to allow automatic conversion
+    """
+    if isinstance(dt, datetime):
+        dt = dt.replace(tzinfo=timezone.utc).isoformat(" ").replace("+00:00", "Z")
+    return dt
+
+
+def str_to_datetime(dt: Union[str, datetime]) -> datetime:
+    """Convert an indy-standard datetime string to a datetime.
+
+    Using a fairly lax regex pattern to match slightly different formats.
+    In Python 3.7 datetime.fromisoformat might be used.
+
+    Args:
+        dt: May be a string or datetime to allow automatic conversion
+    """
+    if isinstance(dt, str):
+        match = re.match(
+            r"^(\d{4})-(\d\d)-(\d\d)[T ](\d\d):(\d\d)"
+            r"(?:\:(\d\d(?:\.\d{1,6})?))?([+-]\d\d:?\d\d|Z)$",
+            dt,
+        )
+        if not match:
+            raise ValueError("String does not match expected time format")
+        year, month, day = match[1], match[2], match[3]
+        hour, minute, second = match[4], match[5], match[6]
+        tz = match[7]
+        if not second:
+            second = 0
+            microsecond = 0
+        else:
+            flt_second = float(second)
+            second = floor(flt_second)
+            microsecond = round((flt_second - second) * 1_000_000)
+        result = datetime(
+            int(year),
+            int(month),
+            int(day),
+            int(hour),
+            int(minute),
+            int(second),
+            microsecond,
+            timezone.utc,
+        )
+        if tz != "Z":
+            tz_sgn = int(tz[0] + "1")
+            tz_hours = int(tz[1:3])
+            tz_mins = int(tz[-2:])
+            if tz_hours or tz_mins:
+                result = result - timedelta(minutes=tz_sgn * (tz_hours * 60 + tz_mins))
+        return result
+    return dt
+
+
+def datetime_now() -> datetime:
+    """Timestamp in UTC."""
+    return datetime.utcnow().replace(tzinfo=timezone.utc)
+
+
+def time_now() -> str:
     """Timestamp in ISO format."""
-    dt = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
-    if datetime_format:
-        return dt
-    return dt.isoformat(" ")
+    return datetime_to_str(datetime_now())
--- agent/indy_catalyst_agent/models/timing_decorator.py
+++ agent/indy_catalyst_agent/models/timing_decorator.py
@@ -5,7 +5,12 @@ This decorator allows the timing of agent messages to be communicated
 and constrained.
 """
 
+from datetime import datetime
+from typing import Union
+
 from marshmallow import fields
+
+from ..messaging.util import datetime_to_str
 
 from .base import BaseModel, BaseModelSchema
 
@@ -23,12 +28,12 @@ class TimingDecorator(BaseModel):
     def __init__(
         self,
         *,
-        in_time: str = None,
-        out_time: str = None,
-        stale_time: str = None,
-        expires_time: str = None,
+        in_time: Union[str, datetime] = None,
+        out_time: Union[str, datetime] = None,
+        stale_time: Union[str, datetime] = None,
+        expires_time: Union[str, datetime] = None,
         delay_milli: int = None,
-        wait_until_time: str = None,
+        wait_until_time: Union[str, datetime] = None,
     ):
         """
         Initialize a TimingDecorator instance.
@@ -40,16 +45,14 @@ class TimingDecorator(BaseModel):
             expires_time: When the message should be considered expired
             delay_milli: The number of milliseconds to delay processing
             wait_until_time: The earliest time at which to perform processing
-
-        # TODO: accept datetime instances for dates
         """
         super(TimingDecorator, self).__init__()
-        self.in_time = in_time
-        self.out_time = out_time
-        self.stale_time = stale_time
-        self.expires_time = expires_time
+        self.in_time = datetime_to_str(in_time)
+        self.out_time = datetime_to_str(out_time)
+        self.stale_time = datetime_to_str(stale_time)
+        self.expires_time = datetime_to_str(expires_time)
         self.delay_milli = delay_milli
-        self.wait_until_time = wait_until_time
+        self.wait_until_time = datetime_to_str(wait_until_time)
 
 
 class TimingDecoratorSchema(BaseModelSchema):
@@ -60,9 +63,9 @@ class TimingDecoratorSchema(BaseModelSchema):
 
         model_class = TimingDecorator
 
-    in_time = fields.Date(format=DT_FORMAT, required=False)
-    out_time = fields.Date(format=DT_FORMAT, required=False)
-    stale_time = fields.Date(format=DT_FORMAT, required=False)
-    expires_time = fields.Date(format=DT_FORMAT, required=False)
+    in_time = fields.Str(format=DT_FORMAT, required=False)
+    out_time = fields.Str(format=DT_FORMAT, required=False)
+    stale_time = fields.Str(format=DT_FORMAT, required=False)
+    expires_time = fields.Str(format=DT_FORMAT, required=False)
     delay_milli = fields.Int(required=False)
-    wait_until_time = fields.Date(format=DT_FORMAT, required=False)
+    wait_until_time = fields.Str(format=DT_FORMAT, required=False)
