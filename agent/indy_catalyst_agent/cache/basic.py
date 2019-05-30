e4c939427614c6fa7f5d00789087c05e428162a6
Nicholas Rempel <nbrempel@gmail.com>
2019-05-30 15:51:02 -0700

    fix some issues with cache, add some tests
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- agent/indy_catalyst_agent/cache/basic.py
+++ agent/indy_catalyst_agent/cache/basic.py
@@ -22,11 +22,13 @@ class BasicCache(BaseCache):
         """
         Removes all expired items from cache.
         """
-        for key in self._cache:
-            cache_item_expiry = self._cache["key"]["expires"]
+        for key in self._cache.copy():  # iterate copy, del from original
+            cache_item_expiry = self._cache[key]["expires"]
+            if cache_item_expiry is None:
+                continue
             now = datetime.now().timestamp()
             if now >= cache_item_expiry:
-                del self._cache["key"]
+                del self._cache[key]
 
     async def get(self, key: Text):
         """
@@ -40,9 +42,9 @@ class BasicCache(BaseCache):
 
         """
         self._remove_expired_cache_items()
-        return self._cache.get(key)
+        return self._cache.get(key)["value"] if self._cache.get(key) else None
 
-    async def set(self, key: Text, value: Any, ttl: int = 0):
+    async def set(self, key: Text, value: Any, ttl: int = None):
         """
         Adds an item to the cache with an optional ttl.
         Overwrites existing cache entries
@@ -55,10 +57,12 @@ class BasicCache(BaseCache):
         """
         self._remove_expired_cache_items()
         now = datetime.now()
-        expires = now + timedelta(seconds=ttl)
-        expires_ts = expires.timestamp()
-
-        self._cache[key] = {"expires": expires_ts, "value": value}
+        if ttl:
+            expires = now + timedelta(seconds=ttl)
+            expires_ts = expires.timestamp()
+            self._cache[key] = {"expires": expires_ts, "value": value}
+        else:
+            self._cache[key] = {"expires": None, "value": value}
 
     async def flush(self):
         """
---
+++ agent/indy_catalyst_agent/cache/tests/test_basic_cache.py
@@ -0,0 +1,39 @@
+import pytest
+
+from ..basic import BasicCache
+
+
+@pytest.fixture()
+async def cache():
+    cache = BasicCache()
+    await cache.set("valid key", "value")
+    return cache
+
+
+class TestBasicCache:
+    @pytest.mark.asyncio
+    async def test_get_none(self, cache):
+        item = await cache.get("doesn't exist")
+        assert item is None
+
+    @pytest.mark.asyncio
+    async def test_get_valid(self, cache):
+        item = await cache.get("valid key")
+        assert item == "value"
+
+    @pytest.mark.asyncio
+    async def test_set_str(self, cache):
+        item = await cache.set("key", "newval")
+        assert cache._cache["key"] is not None
+        assert cache._cache["key"]["value"] == "newval"
+
+    @pytest.mark.asyncio
+    async def test_set_dict(self, cache):
+        item = await cache.set("key", {"dictkey": "dval"})
+        assert cache._cache["key"] is not None
+        assert cache._cache["key"]["value"] == {"dictkey": "dval"}
+
+    @pytest.mark.asyncio
+    async def test_flush(self, cache):
+        await cache.flush()
+        assert cache._cache == {}
