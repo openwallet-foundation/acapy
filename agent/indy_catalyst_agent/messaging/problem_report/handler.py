f6a988d19a5a46eaab2d6561449bd100fcbd975d
Andrew Whitehead <cywolf@gmail.com>
2019-06-02 23:07:10 -0700

    update generic problem report message handler
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- agent/indy_catalyst_agent/messaging/problem_report/handler.py
+++ agent/indy_catalyst_agent/messaging/problem_report/handler.py
@@ -1,44 +1,27 @@
-"""Ping handler."""
+"""Generic problem report handler."""
 
 from ...base_handler import BaseHandler, BaseResponder, RequestContext
-from ..messages.ping import Ping
-from ..messages.ping_response import PingResponse
+
+from .message import ProblemReport
 
 
-class PingHandler(BaseHandler):
-    """Ping handler class."""
+class ProblemReportHandler(BaseHandler):
+    """Problem report handler class."""
 
     async def handle(self, context: RequestContext, responder: BaseResponder):
         """
-        Handle ping message.
+        Handle problem report message.
 
         Args:
             context: Request context
             responder: Responder used to reply
 
         """
-        self._logger.debug(f"PingHandler called with context {context}")
-        assert isinstance(context.message, Ping)
+        self._logger.debug(f"ProblemReportHandler called with context {context}")
+        assert isinstance(context.message, ProblemReport)
 
         self._logger.info(
-            "Received trust ping from: %s", context.message_delivery.sender_did
+            "Received problem report from: %s, %r",
+            context.message_delivery.sender_did,
+            context.message,
         )
-
-        if not context.connection_active:
-            self._logger.info(
-                "Connection not active, skipping ping response: %s",
-                context.message_delivery.sender_did,
-            )
-            return
-
-        await context.connection_record.log_activity(
-            context, "ping", context.connection_record.DIRECTION_RECEIVED
-        )
-
-        if context.message.response_requested:
-            reply = PingResponse()
-            reply.assign_thread_from(context.message)
-            await responder.send_reply(reply)
-            await context.connection_record.log_activity(
-                context, "ping", context.connection_record.DIRECTION_SENT
-            )
