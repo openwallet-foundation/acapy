201d49bb65e62fd9addfb317fa33c70ae643415f
Andrew Whitehead <cywolf@gmail.com>
2019-06-05 22:11:31 -0700

    implement credential deletion in Holder and add admin route
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- agent/indy_catalyst_agent/messaging/credentials/routes.py
+++ agent/indy_catalyst_agent/messaging/credentials/routes.py
@@ -6,13 +6,14 @@ from aiohttp import web
 from aiohttp_apispec import docs, request_schema, response_schema
 from marshmallow import fields, Schema
 
-from .manager import CredentialManager
-from .models.credential_exchange import CredentialExchange, CredentialExchangeSchema
+from ...holder.base import BaseHolder
+from ...storage.error import StorageNotFoundError
+from ...wallet.error import WalletNotFoundError
 
 from ..connections.models.connection_record import ConnectionRecord
 
-from ...holder.base import BaseHolder
-from ...storage.error import StorageNotFoundError
+from .manager import CredentialManager
+from .models.credential_exchange import CredentialExchange, CredentialExchangeSchema
 
 
 class CredentialOfferRequestSchema(Schema):
@@ -70,6 +71,31 @@ class CredentialListSchema(Schema):
 @response_schema(CredentialSchema(), 200)
 async def credentials_get(request: web.BaseRequest):
     """
+    Request handler for retrieving a credential.
+
+    Args:
+        request: aiohttp request object
+
+    Returns:
+        The credential response
+
+    """
+    context = request.app["request_context"]
+
+    credential_id = request.match_info["id"]
+
+    holder: BaseHolder = await context.inject(BaseHolder)
+    try:
+        credential = await holder.get_credential(credential_id)
+    except WalletNotFoundError:
+        return web.HTTPNotFound()
+
+    return web.json_response(credential)
+
+
+@docs(tags=["credentials"], summary="Remove a credential from the wallet by id")
+async def credentials_remove(request: web.BaseRequest):
+    """
     Request handler for searching connection records.
 
     Args:
@@ -84,9 +110,12 @@ async def credentials_get(request: web.BaseRequest):
     credential_id = request.match_info["id"]
 
     holder: BaseHolder = await context.inject(BaseHolder)
-    credential = await holder.get_credential(credential_id)
+    try:
+        await holder.delete_credential(credential_id)
+    except WalletNotFoundError:
+        return web.HTTPNotFound()
 
-    return web.json_response(credential)
+    return web.HTTPOk()
 
 
 @docs(
@@ -111,13 +140,13 @@ async def credentials_get(request: web.BaseRequest):
 @response_schema(CredentialListSchema(), 200)
 async def credentials_list(request: web.BaseRequest):
     """
-    Request handler for searching connection records.
+    Request handler for searching credential records.
 
     Args:
         request: aiohttp request object
 
     Returns:
-        The connection list response
+        The credential list response
 
     """
     context = request.app["request_context"]
@@ -143,13 +172,13 @@ async def credentials_list(request: web.BaseRequest):
 @response_schema(CredentialExchangeListSchema(), 200)
 async def credential_exchange_list(request: web.BaseRequest):
     """
-    Request handler for searching connection records.
+    Request handler for searching credential exchange records.
 
     Args:
         request: aiohttp request object
 
     Returns:
-        The connection list response
+        The credential exchange list response
 
     """
     context = request.app["request_context"]
@@ -171,13 +200,13 @@ async def credential_exchange_list(request: web.BaseRequest):
 @response_schema(CredentialExchangeSchema(), 200)
 async def credential_exchange_retrieve(request: web.BaseRequest):
     """
-    Request handler for fetching a single connection record.
+    Request handler for fetching a single credential exchange record.
 
     Args:
         request: aiohttp request object
 
     Returns:
-        The connection record response
+        The credential exchange record response
 
     """
     context = request.app["request_context"]
@@ -359,6 +388,7 @@ async def register(app: web.Application):
     app.add_routes(
         [
             web.get("/credential/{id}", credentials_get),
+            web.post("/credential/{id}/remove", credentials_remove),
             web.get("/credentials", credentials_list),
             web.get("/credential_exchange", credential_exchange_list),
             web.get("/credential_exchange/{id}", credential_exchange_retrieve),
