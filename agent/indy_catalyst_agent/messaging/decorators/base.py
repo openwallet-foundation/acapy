44890803dff746b7e6beac31260053e63d7a0821
Andrew Whitehead <cywolf@gmail.com>
2019-06-06 17:47:38 -0700

    add auto-loading and serialization of base model decorators; add unit tests
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- agent/indy_catalyst_agent/messaging/decorators/base.py
+++ agent/indy_catalyst_agent/messaging/decorators/base.py
@@ -1,7 +1,7 @@
 """Classes for managing a collection of decorators."""
 
 from collections import OrderedDict
-from typing import Mapping
+from typing import Mapping, Type
 
 from ...error import BaseError
 
@@ -18,16 +18,41 @@ class DecoratorError(BaseError):
 class DecoratorSet(OrderedDict):
     """Collection of decorators."""
 
-    def __init__(self):
+    def __init__(self, models: dict = None):
         """Initialize a decorator set."""
+        self._models: Mapping[str, Type[BaseModel]] = models.copy() if models else {}
         self._prefix = DECORATOR_PREFIX
+
+    @property
+    def models(self) -> dict:
+        """Accessor for the models dictionary."""
+        return self._models.copy()
+
+    @property
+    def prefix(self) -> str:
+        """Accessor for the decorator prefix."""
+        return self._prefix
+
+    def add_model(self, key: str, model: Type[BaseModel]):
+        """Add a registered decorator model."""
+        self._models[key] = model
+
+    def remove_model(self, key: str):
+        """Remove a registered decorator model."""
+        del self._models[key]
 
     def __setitem__(self, key, value):
         """Add a decorator."""
-
         if not isinstance(value, (str, dict, BaseModel)):
             raise ValueError(f"Unsupported decorator value: {value}")
         super().__setitem__(key, value)
+
+    def load_decorator(self, key: str, value):
+        """Convert a decorator value to its loaded representation."""
+        if key in self._models and isinstance(value, dict):
+            value = self._models[key].deserialize(value)
+        if value is not None:
+            self[key] = value
 
     def extract_decorators(self, message: Mapping) -> OrderedDict:
         """Extract decorators and return the remaining properties."""
@@ -36,7 +61,8 @@ class DecoratorSet(OrderedDict):
             pfx_len = len(self._prefix)
             for key, value in message.items():
                 if key.startswith(self._prefix):
-                    self[key[pfx_len:]] = value
+                    key = key[pfx_len:]
+                    self.load_decorator(key, value)
                 else:
                     remain[key] = value
         return remain
@@ -45,10 +71,13 @@ class DecoratorSet(OrderedDict):
         """Convert to a dictionary (serialize)."""
         result = {}
         for k in self:
-            result[self._prefix + k] = self[k]
+            value = self[k]
+            if isinstance(value, BaseModel):
+                value = value.serialize()
+            result[self._prefix + k] = value
         return result
 
     def __repr__(self) -> str:
         """Create a string representation of the decorator set."""
-        items = ("{}={}".format(k, repr(v)) for k, v in self.to_dict())
-        return "<{}({})>".format(self.__class__.__name__, ", ".join(items))
+        items = ("{}: {}".format(k, repr(v)) for k, v in self.to_dict().items())
+        return "<{}{{{}}}>".format(self.__class__.__name__, ", ".join(items))
--- agent/indy_catalyst_agent/messaging/decorators/tests/test_decorator_set.py
+++ agent/indy_catalyst_agent/messaging/decorators/tests/test_decorator_set.py
@@ -1,6 +1,25 @@
+from marshmallow import fields
+from unittest import TestCase
+
+from ...models.base import BaseModel, BaseModelSchema
+
 from ..base import DecoratorSet
 
-from unittest import TestCase
+
+class SimpleModel(BaseModel):
+    class Meta:
+        schema_class = "SimpleModelSchema"
+
+    def __init__(self, *, value: str = None, **kwargs):
+        super().__init__(**kwargs)
+        self.value = value
+
+
+class SimpleModelSchema(BaseModelSchema):
+    class Meta:
+        model_class = SimpleModel
+
+    value = fields.Str()
 
 
 class TestDecoratorSet(TestCase):
@@ -12,7 +31,7 @@ class TestDecoratorSet(TestCase):
         decors = DecoratorSet()
         remain = decors.extract_decorators(message)
 
-        # check unmodified
+        # check original is unmodified
         assert "~decorator" in message
 
         assert decors["decorator"] is decor_value
@@ -22,5 +41,21 @@ class TestDecoratorSet(TestCase):
 
         decors = DecoratorSet()
         decors["test"] = "TEST"
+        assert decors["test"] == "TEST"
         result = decors.to_dict()
         assert result == {"~test": "TEST"}
+
+    def test_decorator_model(self):
+
+        decor_value = {}
+        message = {"~test": {"value": "TEST"}}
+
+        decors = DecoratorSet()
+        decors.add_model("test", SimpleModel)
+        remain = decors.extract_decorators(message)
+
+        tested = decors["test"]
+        assert isinstance(tested, SimpleModel) and tested.value == "TEST"
+
+        result = decors.to_dict()
+        assert result == message
