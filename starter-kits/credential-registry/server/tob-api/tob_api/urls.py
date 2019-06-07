adc08b00cee76aee26f0197e818668892fecbd67
Emiliano Suñé <emiliano.sune@gmail.com>
2019-06-07 10:35:48 -0700

    Enable icat_cbs urls
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- starter-kits/credential-registry/server/tob-api/tob_api/urls.py
+++ starter-kits/credential-registry/server/tob-api/tob_api/urls.py
@@ -2,8 +2,9 @@
 Definition of urls for tob_api.
 """
 
-from django.views.generic import RedirectView
 from django.urls import include, path
+from django.views.generic import RedirectView
+
 from . import views
 
 # Uncomment the next two lines to enable the admin:
@@ -12,12 +13,9 @@ from . import views
 
 urlpatterns = [
     path("", RedirectView.as_view(url="api/v2/")),
-    path("api-auth/",
-        include("rest_framework.urls", namespace="rest_framework"),
-    ),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api/v2/", include("api_v2.urls")),
     path("health", views.health),
     path("hooks/", include("icat_hooks.urls")),
-    # comment out for now due to vonx dependencies
-    #path("agentcb/", include("icat_cbs.urls")),
+    path("agentcb/", include("icat_cbs.urls")),
 ]
