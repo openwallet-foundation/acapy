cfde565f7bb03947ac34c0fc7f03cefe23d49e3e
Nicholas Rempel <nbrempel@gmail.com>
2019-06-11 12:40:48 -0700

    Don't use model schema in admin routes spec
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- credential-registry/message_families/issuer_registration/indy_catalyst_issuer_registration/routes.py
+++ credential-registry/message_families/issuer_registration/indy_catalyst_issuer_registration/routes.py
@@ -10,11 +10,48 @@ from indy_catalyst_agent.messaging.connections.models.connection_record import (
 )
 from indy_catalyst_agent.storage.error import StorageNotFoundError
 
-from .messages.register import IssuerRegistration, IssuerRegistrationSchema
+from .messages.register import IssuerRegistration
+
+
+class IssuerRegistrationRequestSchema(Schema):
+    """Request schema for issuer registration"""
+
+    class IssuerSchema(Schema):
+        """Isuer nested schema."""
+
+        did = fields.Str(required=True)
+        name = fields.Str(required=True)
+        abbreviation = fields.Str(required=False)
+        email = fields.Str(required=False)
+        url = fields.Str(required=False)
+        endpoint = fields.Str(required=False)
+        logo_b64 = fields.Str(required=False)
+
+    class CredentialType(Schema):
+        """Isuer credential type schema."""
+
+        name = fields.Str(required=True)
+        schema = fields.Str(required=True)
+        version = fields.Str(required=True)
+        description = fields.Str(required=False)
+        cardinality_fields = fields.List(fields.Dict, required=False)
+        credential = fields.Str(required=False)
+        mapping = fields.Dict(required=False)
+        topic = fields.Str(required=False)
+        caregory_labels = fields.List(fields.Str, required=False)
+        claim_descriptions = fields.List(fields.Str, required=False)
+        claim_labels = fields.List(fields.Str, required=False)
+        logo_b64 = fields.Str(required=False)
+        credential_def_id = fields.Str(required=True)
+        endpoint = fields.Str(required=False)
+        visible_fields = fields.List(fields.Str, required=False)
+
+    fields.Nested(IssuerSchema, required=True)
+    fields.List(fields.Nested(CredentialType), required=False)
 
 
 @docs(tags=["issuer_registration"], summary="Send an issuer registration to a target")
-@request_schema(IssuerRegistrationSchema())
+@request_schema(IssuerRegistrationRequestSchema())
 async def issuer_registration_send(request: web.BaseRequest):
     """
     Request handler for sending an issuer registration message to a connection.
