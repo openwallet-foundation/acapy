caa197c94aea4708daa12e1f41175de9b6261100
Emiliano Suñé <emiliano.sune@gmail.com>
2019-06-12 12:35:35 -0700

    Added tests for api_v2/views/misc.py
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- starter-kits/credential-registry/server/tob-api/api_v2/views/misc.py
+++ starter-kits/credential-registry/server/tob-api/api_v2/views/misc.py
@@ -6,8 +6,12 @@ from django.http import JsonResponse
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import permissions
-from rest_framework.decorators import (api_view, authentication_classes,
-                                       parser_classes, permission_classes)
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    parser_classes,
+    permission_classes,
+)
 from rest_framework.parsers import FormParser
 
 from api_v2.feedback import email_feedback
