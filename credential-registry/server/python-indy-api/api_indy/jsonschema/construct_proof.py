a8f33220468468be29ef155fcd40f91e2b957132
Emiliano Suñé <emiliano.sune@gmail.com>
2019-06-19 10:15:50 -0700

    Clean up old files
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- credential-registry/server/python-indy-api/api_indy/jsonschema/construct_proof.py
+++
@@ -1,8 +0,0 @@
-CONSTRUCT_PROOF_JSON_SCHEMA = {
-    # TODO: use newer draft of spec?
-    "$schema": "http://json-schema.org/draft-04/schema",
-    "type": "object",
-    # TODO: Flesh out definitions further?
-    "properties": {"proof_request": {"type": "object"}},
-    "required": ["proof_request"],
-}
