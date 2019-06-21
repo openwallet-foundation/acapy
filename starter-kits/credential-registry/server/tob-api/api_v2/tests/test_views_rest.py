8e00212868aa5165d729100a96d5e6420317c4d6
Emiliano Suñé <emiliano.sune@gmail.com>
2019-06-21 13:45:44 -0700

    Add tests for CredentialTypeViewSet
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- starter-kits/credential-registry/server/tob-api/api_v2/tests/test_views_rest.py
+++ starter-kits/credential-registry/server/tob-api/api_v2/tests/test_views_rest.py
@@ -61,7 +61,7 @@ class IssuerViewSetTest(APITestCase):
         self.assertEqual(response.data[0]["credential_def_id"], "123456")
         self.assertEqual(response.data[0]["schema"]["name"], "test-schema")
 
-    def test_get_issuer_logo_success(self):
+    def test_get_issuer_logo(self):
         url = reverse("issuer-list")
         response1 = self.client.get(url + "/1/logo")
         self.assertEqual(response1.status_code, status.HTTP_200_OK)
@@ -130,3 +130,82 @@ class SchemaViewSetTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["id"], 3)
         self.assertEqual(response.data["origin_did"], "not:a:did:123")
+
+
+class CredentialTypeViewSetTest(APITestCase):
+    def setUp(self):
+        # create test schemas
+        schema1 = Schema.objects.create(
+            name="test-schema-1", version="0.5.0", origin_did="not:a:did:123"
+        )
+        schema2 = Schema.objects.create(
+            name="test-schema-2", version="1.0.0", origin_did="not:a:did:456"
+        )
+
+        # create test issuers
+        issuer1 = Issuer.objects.create(
+            did="not:a:did:456",
+            name="Test Issuer 1",
+            abbreviation="TI-1",
+            email="test1@issuer.io",
+            url="http://www.issuer-1.fake.io",
+            logo_b64="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==",
+        )
+        issuer2 = Issuer.objects.create(
+            did="not:a:did:123",
+            name="Test Issuer 2",
+            abbreviation="TI-2",
+            email="test2@issuer.io",
+            url="http://www.issuer-2.fake.io",
+        )
+
+        # create test CredentialType and bind it to issuer1
+        self.credType1 = CredentialType.objects.create(
+            schema=schema1,
+            issuer=issuer1,
+            credential_def_id="123456",
+            logo_b64="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==",
+        )
+        self.credType2 = CredentialType.objects.create(
+            schema=schema2, issuer=issuer2, credential_def_id="987654"
+        )
+
+    def test_get_credentialtype_list(self):
+        url = reverse("credentialtype-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["total"], 2)
+        self.assertEqual(len(response.data["results"]), 2)
+
+        credType1 = response.data["results"][0]
+        self.assertEqual(credType1["issuer"]["did"], self.credType1.issuer.did)
+        self.assertEqual(credType1["schema"]["name"], self.credType1.schema.name)
+
+        credType2 = response.data["results"][1]
+        self.assertEqual(credType2["issuer"]["did"], self.credType2.issuer.did)
+        self.assertEqual(credType2["schema"]["name"], self.credType2.schema.name)
+
+    def test_get_credentialtype_by_id(self):
+        url = reverse("credentialtype-list")
+        response = self.client.get(url + "/2")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["id"], 2)
+        self.assertEqual(response.data["issuer"]["did"], self.credType2.issuer.did)
+        self.assertEqual(response.data["schema"]["name"], self.credType2.schema.name)
+
+    def test_get_credentialtype_language(self):
+        url = reverse("credentialtype-list")
+        response = self.client.get(url + "/2/language")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # we are only checking the fields exists, not its content
+        self.assertIsNone(response.data["category_labels"])
+        self.assertIsNone(response.data["claim_descriptions"])
+        self.assertIsNone(response.data["claim_labels"])
+
+    def test_get_credentialtype_logo(self):
+        url = reverse("credentialtype-list")
+        response1 = self.client.get(url + "/1/logo")
+        self.assertEqual(response1.status_code, status.HTTP_200_OK)
+
+        response2 = self.client.get(url + "/2/logo")
+        self.assertEqual(response2.status_code, status.HTTP_404_NOT_FOUND)
