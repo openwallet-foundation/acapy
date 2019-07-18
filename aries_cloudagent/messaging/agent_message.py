cb266585359bb963945d8cd94504dd99eb8e72d8
Andrew Whitehead <cywolf@gmail.com>
2019-07-18 10:23:14 -0700

    allow override of empty DecoratorSet
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- aries_cloudagent/messaging/agent_message.py
+++ aries_cloudagent/messaging/agent_message.py
@@ -61,7 +61,9 @@ class AgentMessage(BaseModel):
         else:
             self._message_id = str(uuid.uuid4())
             self._message_new_id = True
-        self._message_decorators = _decorators or DecoratorSet()
+        self._message_decorators = (
+            _decorators if _decorators is not None else DecoratorSet()
+        )
         if not self.Meta.message_type:
             raise TypeError(
                 "Can't instantiate abstract class {} with no message_type".format(
