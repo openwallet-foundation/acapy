12a6754563ae0d9a8e2ded9621ed4c92c736a1e0
Nicholas Rempel <nbrempel@gmail.com>
2019-07-23 15:26:51 -0700

    add route tests
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- aries_cloudagent/messaging/credentials/tests/test_routes.py
+++ aries_cloudagent/messaging/credentials/tests/test_routes.py
@@ -355,6 +355,113 @@ class TestCredentialRoutes(AsyncTestCase):
 
 
 
+    async def test_credential_exchange_store(self):
+        mock = async_mock.MagicMock()
+        mock.json = async_mock.CoroutineMock()
+
+        mock.app = {
+            "request_context": "context",
+        }
+
+        with async_mock.patch.object(
+            test_module, "ConnectionRecord", autospec=True
+        ) as mock_connection_record, async_mock.patch.object(
+            test_module, "CredentialManager", autospec=True
+        ) as mock_connection_manager, async_mock.patch.object(
+            test_module, "CredentialExchange", autospec=True
+        ) as mock_cred_ex:
+
+            mock_cred_ex.retrieve_by_id = async_mock.CoroutineMock()
+            mock_cred_ex.retrieve_by_id.return_value.state = mock_cred_ex.STATE_CREDENTIAL_RECEIVED
+
+            test_module.web.json_response = async_mock.CoroutineMock()
+
+            mock_cred_ex_record = async_mock.MagicMock()
+
+            mock_connection_manager.return_value.store_credential.return_value = (
+                mock_cred_ex_record
+            )
+
+            await test_module.credential_exchange_store(mock)
+
+            test_module.web.json_response.assert_called_once_with(
+                mock_cred_ex_record.serialize.return_value
+            )
+
+    async def test_credential_exchange_store_no_conn_record(self):
+        mock = async_mock.MagicMock()
+        mock.json = async_mock.CoroutineMock()
+
+        mock.app = {
+            "request_context": "context",
+        }
+
+        with async_mock.patch.object(
+            test_module, "ConnectionRecord", autospec=True
+        ) as mock_connection_record, async_mock.patch.object(
+            test_module, "CredentialManager", autospec=True
+        ) as mock_connection_manager, async_mock.patch.object(
+            test_module, "CredentialExchange", autospec=True
+        ) as mock_cred_ex:
+
+            test_module.web.json_response = async_mock.CoroutineMock()
+
+            mock_cred_ex.retrieve_by_id = async_mock.CoroutineMock()
+            mock_cred_ex.retrieve_by_id.return_value.state = mock_cred_ex.STATE_CREDENTIAL_RECEIVED
+            
+
+            # Emulate storage not found (bad connection id)
+            mock_connection_record.retrieve_by_id = async_mock.CoroutineMock(
+                side_effect=StorageNotFoundError
+            )
+
+            mock_connection_manager.return_value.store_credential.return_value = (
+                async_mock.MagicMock(),
+            )
+
+            with self.assertRaises(test_module.web.HTTPBadRequest):
+                await test_module.credential_exchange_store(mock)
+
+    async def test_credential_exchange_store_not_ready(self):
+        mock = async_mock.MagicMock()
+        mock.json = async_mock.CoroutineMock()
+
+        mock.app = {
+            "request_context": "context",
+        }
+
+        with async_mock.patch.object(
+            test_module, "ConnectionRecord", autospec=True
+        ) as mock_connection_record, async_mock.patch.object(
+            test_module, "CredentialManager", autospec=True
+        ) as mock_connection_manager, async_mock.patch.object(
+            test_module, "CredentialExchange", autospec=True
+        ) as mock_cred_ex:
+
+            mock_cred_ex.retrieve_by_id = async_mock.CoroutineMock()
+            mock_cred_ex.retrieve_by_id.return_value.state = mock_cred_ex.STATE_CREDENTIAL_RECEIVED
+
+            test_module.web.json_response = async_mock.CoroutineMock()
+
+            # Emulate connection not ready
+            mock_connection_record.retrieve_by_id = async_mock.CoroutineMock()
+            mock_connection_record.retrieve_by_id.return_value.is_ready = False
+
+            mock_connection_manager.return_value.store_credential.return_value = (
+                async_mock.MagicMock(),
+            )
+
+            with self.assertRaises(test_module.web.HTTPForbidden):
+                await test_module.credential_exchange_store(mock)
+
+
+
+
+
+
+
+
+
 
 
 
