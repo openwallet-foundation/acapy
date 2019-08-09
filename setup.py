882cfbfbff11a1c0cf3bdb234a0d239c4a8f0508
Andrew Whitehead <cywolf@gmail.com>
2019-08-08 17:11:04 -0700

    add optional test dependencies and indy dependencies
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ PACKAGE_NAME = "aries_cloudagent"
 version_meta = runpy.run_path("./{}/version.py".format(PACKAGE_NAME))
 VERSION = version_meta["__version__"]
 
+
 with open(os.path.abspath("./README.md"), "r") as fh:
     long_description = fh.read()
 
@@ -28,6 +29,8 @@ if __name__ == "__main__":
         include_package_data=True,
         package_data={"aries_cloudagent": ["requirements.txt"]},
         install_requires=parse_requirements("requirements.txt"),
+        tests_require=parse_requirements("requirements.dev.txt"),
+        extras_require={"indy": parse_requirements("requirements.indy.txt")},
         python_requires=">=3.6.3",
         scripts=["bin/aca-py"],
     )
