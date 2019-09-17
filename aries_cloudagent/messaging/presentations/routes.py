d6472f6f5e46234db67fb97f34cb1c7ab13be00b
Nicholas Rempel <nbrempel@gmail.com>
2019-09-17 13:46:26 -0700

    Update presentation exchange route to accept multiple referents
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- aries_cloudagent/messaging/presentations/routes.py
+++ aries_cloudagent/messaging/presentations/routes.py
@@ -148,7 +148,7 @@ async def presentation_exchange_credentials_list(request: web.BaseRequest):
     context = request.app["request_context"]
 
     presentation_exchange_id = request.match_info["id"]
-    presentation_referent = request.match_info.get("referent")
+    presentation_referents = request.match_info.get("referent").split(",")
 
     try:
         presentation_exchange_record = await PresentationExchange.retrieve_by_id(
@@ -171,7 +171,7 @@ async def presentation_exchange_credentials_list(request: web.BaseRequest):
     holder: BaseHolder = await context.inject(BaseHolder)
     credentials = await holder.get_credentials_for_presentation_request_by_referent(
         presentation_exchange_record.presentation_request,
-        (presentation_referent,) if presentation_referent else (),
+        presentation_referents,
         start,
         count,
         extra_query,
@@ -182,7 +182,7 @@ async def presentation_exchange_credentials_list(request: web.BaseRequest):
         "Retrieved presentation credentials",
         {
             "presentation_exchange_id": presentation_exchange_id,
-            "referent": presentation_referent,
+            "referents": presentation_referents,
             "extra_query": extra_query,
             "credentials": credentials,
         },
