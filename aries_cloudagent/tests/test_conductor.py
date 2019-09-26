5da3ad8e79901e33df9220a0882e8411fc8b3d74
Andrew Whitehead <cywolf@gmail.com>
2019-09-26 12:21:54 -0700

    fix usage of wait_for in unit test
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- aries_cloudagent/tests/test_conductor.py
+++ aries_cloudagent/tests/test_conductor.py
@@ -102,11 +102,8 @@ class TestConductor(AsyncTestCase, Config):
         await conductor.setup()
 
         with async_mock.patch.object(
-            conductor.dispatcher, "dispatch", autospec=True
+            conductor.dispatcher, "dispatch", new_callable=async_mock.CoroutineMock
         ) as mock_dispatch:
-            dispatch_result = """{"@type": "..."}"""
-            mock_dispatch.return_value.return_value = asyncio.Future()
-            mock_dispatch.return_value.return_value.set_result(None)
 
             delivery = MessageDelivery()
             parsed_msg = {}
@@ -116,8 +113,7 @@ class TestConductor(AsyncTestCase, Config):
 
             message_body = "{}"
             transport = "http"
-            complete = await conductor.inbound_message_router(message_body, transport)
-            asyncio.wait_for(complete, 1.0)
+            await conductor.inbound_message_router(message_body, transport)
 
             mock_serializer.parse_message.assert_awaited_once_with(
                 conductor.context, message_body, transport
@@ -165,7 +161,7 @@ class TestConductor(AsyncTestCase, Config):
             complete = await conductor.inbound_message_router(
                 message_body, transport, None, single_response
             )
-            asyncio.wait_for(complete, 1.0)
+            await asyncio.wait_for(complete, 1.0)
 
             assert single_response.result() == dispatch_result
 
