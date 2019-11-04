105fb778a819d706d73f3a8a765a5bae85bd68ca
Andrew Whitehead <cywolf@gmail.com>
2019-11-04 14:46:19 -0800

    fix shutdown error when module import fails
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- aries_cloudagent/conductor.py
+++ aries_cloudagent/conductor.py
@@ -228,8 +228,10 @@ class Conductor:
         tasks = []
         if self.admin_server:
             tasks.append(self.admin_server.stop())
-        tasks.append(self.inbound_transport_manager.stop())
-        tasks.append(self.outbound_transport_manager.stop())
+        if self.inbound_transport_manager:
+            tasks.append(self.inbound_transport_manager.stop())
+        if self.outbound_transport_manager:
+            tasks.append(self.outbound_transport_manager.stop())
         await asyncio.wait_for(asyncio.gather(*tasks), timeout)
 
     async def register_socket(
