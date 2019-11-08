01a7ae830643923d60fa8424d22811dcd0067e96
Andrew Whitehead <cywolf@gmail.com>
2019-11-08 14:33:59 -0800

    add unit tests to ClassLoader
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- aries_cloudagent/classloader.py
+++ aries_cloudagent/classloader.py
@@ -156,9 +156,10 @@ class ClassLoader:
         if not pkg_resources.resource_isdir(package, sub_pkg):
             raise ModuleLoadError(f"Undefined package {package}")
         found = []
+        joiner = "" if sub_pkg == "." else f"{sub_pkg}."
         for sub_path in pkg_resources.resource_listdir(package, sub_pkg):
             if pkg_resources.resource_exists(
                 package, f"{sub_pkg}/{sub_path}/__init__.py"
             ):
-                found.append(f"{package}.{sub_pkg}.{sub_path}")
+                found.append(f"{package}.{joiner}{sub_path}")
         return found
