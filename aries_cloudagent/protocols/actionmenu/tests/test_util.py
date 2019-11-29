2322aee08b95e52916bad345f6420ea0fa3acf4b
sklump <srklump@hotmail.com>
2019-11-29 11:20:39 -0500

    unit test coverage for actionmenu, basic message protocols; l10n as decorator in basicmessage
    Signed-off-by: sklump <srklump@hotmail.com>

---
+++ aries_cloudagent/protocols/actionmenu/tests/test_util.py
@@ -0,0 +1,97 @@
+from asynctest import TestCase as AsyncTestCase
+
+from ....config.injection_context import InjectionContext
+from ....connections.models.connection_record import ConnectionRecord
+from ....messaging.responder import MockResponder
+from ....storage.base import BaseStorage
+from ....storage.basic import BasicStorage
+from ....wallet.base import BaseWallet
+from ....wallet.basic import BasicWallet
+
+from .. import util as test_module
+from ..models.menu_form_param import MenuFormParam
+from ..models.menu_form import MenuForm
+from ..models.menu_option import MenuOption
+
+class TestUtil(AsyncTestCase):
+    async def test_save_retrieve_delete_connection_menu(self):
+        self.storage = BasicStorage()
+        self.context = InjectionContext()
+        self.context.injector.bind_instance(BaseStorage, self.storage)
+        self.context.injector.bind_instance(BaseWallet, BasicWallet())
+
+        self.responder = MockResponder()
+        self.context.injector.bind_instance(test_module.BaseResponder, self.responder)
+
+        menu = test_module.Menu(
+            title='title',
+            description='description',
+            errormsg=None,
+            options=[
+                MenuOption(
+                    name=f"name-{i}",
+                    title=f"option-title-{i}",
+                    description=f"option-description-{i}",
+                    disabled=bool(i % 2),
+                    form=MenuForm(
+                        title=f"form-title-{i}",
+                        description=f"form-description-{i}",
+                        params=[
+                            MenuFormParam(
+                                name=f"param-name-{i}-{j}",
+                                title=f"param-title-{i}-{j}",
+                                default=f"param-default-{i}-{j}",
+                                description=f"param-description-{i}-{j}",
+                                input_type="int",
+                                required=bool((i + j) % 2)
+                            ) for j in range(2)
+                        ]
+                    )
+                ) for i in range(3)
+            ]
+        )
+        connection_id = "connid"
+
+        for i in range(2):  # once to add, once to update
+            await test_module.save_connection_menu(
+                menu,
+                connection_id,
+                self.context
+            )
+
+            webhooks = self.responder.webhooks
+            assert len(webhooks) == 1
+            (result, target) = webhooks[0]
+            assert result == "actionmenu"
+            assert target["connection_id"] == connection_id
+            assert target["menu"] == menu.serialize()
+            self.responder.webhooks.clear()
+
+        # retrieve connection menu
+        assert (await test_module.retrieve_connection_menu(
+            connection_id,
+            self.context
+        )).serialize() == menu.serialize()
+
+        # delete connection menu
+        await test_module.save_connection_menu(
+            None,
+            connection_id,
+            self.context
+        )
+
+        webhooks = self.responder.webhooks
+        assert len(webhooks) == 1
+        (result, target) = webhooks[0]
+        assert result == "actionmenu"
+        assert target == {
+            "connection_id": connection_id,
+            "menu": None
+        }
+        self.responder.webhooks.clear()
+
+        # retrieve no menu
+        assert await test_module.retrieve_connection_menu(
+            connection_id,
+            self.context
+        ) is None
--- aries_cloudagent/protocols/basicmessage/handlers/tests/test_basicmessage_handler.py
+++ aries_cloudagent/protocols/basicmessage/handlers/tests/test_basicmessage_handler.py
@@ -19,7 +19,7 @@ class TestBasicMessageHandler:
     @pytest.mark.asyncio
     async def test_basic_message(self, request_context):
         request_context.connection_record = mock.MagicMock()
-        test_message_content = "hello"
+        test_message_content = "http://aries.ca/hello"
         request_context.message = BasicMessage(content=test_message_content)
         request_context.connection_ready = True
         handler = BasicMessageHandler()
@@ -46,6 +46,27 @@ class TestBasicMessageHandler:
         request_context.default_label = "agent"
         test_message_content = "hello"
         request_context.message = BasicMessage(content=test_message_content)
+        request_context.connection_ready = True
+        handler = BasicMessageHandler()
+        responder = MockResponder()
+        await handler.handle(request_context, responder)
+        messages = responder.messages
+        assert len(messages) == 1
+        reply, target = messages[0]
+        assert isinstance(reply, BasicMessage)
+        assert reply._thread_id == request_context.message._thread_id
+        assert not target
+
+    @pytest.mark.asyncio
+    async def test_basic_message_response_reply_with(self, request_context):
+        request_context.update_settings({"debug.auto_respond_messages": False})
+        request_context.connection_record = mock.MagicMock()
+        request_context.default_label = "agent"
+        test_message_content = "Reply with: g'day"
+        request_context.message = BasicMessage(
+            content=test_message_content,
+            localization="en-CA"
+        )
         request_context.connection_ready = True
         handler = BasicMessageHandler()
         responder = MockResponder()
