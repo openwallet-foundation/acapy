43986a6b0f5bee075959649bd29291c94956c423
Andrew Whitehead <cywolf@gmail.com>
2019-12-10 13:55:54 -0800

    Add tests for taa, taa/accept admin routes
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- aries_cloudagent/ledger/tests/test_routes.py
+++ aries_cloudagent/ledger/tests/test_routes.py
@@ -2,7 +2,7 @@ from asynctest import TestCase as AsyncTestCase
 from asynctest import mock as async_mock
 import pytest
 
-from aiohttp.web import HTTPForbidden
+from aiohttp.web import HTTPBadRequest, HTTPForbidden
 
 from ...config.injection_context import InjectionContext
 from ...ledger.base import BaseLedger
@@ -37,7 +37,6 @@ class TestLedgerRoutes(AsyncTestCase):
         with self.assertRaises(HTTPForbidden):
             await test_module.get_did_endpoint(request)
 
-
     async def test_get_verkey(self):
         request = async_mock.MagicMock()
         request.app = self.app
@@ -48,7 +47,7 @@ class TestLedgerRoutes(AsyncTestCase):
             self.ledger.get_key_for_did.return_value = self.test_verkey
             result = await test_module.get_did_verkey(request)
             json_response.assert_called_once_with(
-                {"verkey": self.ledger.get_key_for_did.return_value }
+                {"verkey": self.ledger.get_key_for_did.return_value}
             )
             assert result is json_response.return_value
 
@@ -66,20 +65,82 @@ class TestLedgerRoutes(AsyncTestCase):
             )
             assert result is json_response.return_value
 
-
     async def test_register_nym(self):
         request = async_mock.MagicMock()
         request.app = self.app
-        request.query = {
-            "did": self.test_did,
-            "verkey": self.test_verkey
-        }
+        request.query = {"did": self.test_did, "verkey": self.test_verkey}
         with async_mock.patch.object(
             test_module.web, "json_response", async_mock.Mock()
         ) as json_response:
             self.ledger.register_nym.return_value = True
             result = await test_module.register_ledger_nym(request)
             json_response.assert_called_once_with(
-                {"success": self.ledger.register_nym.return_value }
+                {"success": self.ledger.register_nym.return_value}
+            )
+            assert result is json_response.return_value
+
+    async def test_taa_forbidden(self):
+        request = async_mock.MagicMock()
+        request.app = self.app
+
+        with self.assertRaises(HTTPForbidden):
+            await test_module.ledger_get_taa(request)
+
+    async def test_get_taa(self):
+        request = async_mock.MagicMock()
+        request.app = self.app
+        with async_mock.patch.object(
+            test_module.web, "json_response", async_mock.Mock()
+        ) as json_response:
+            self.ledger.LEDGER_TYPE = "indy"
+            self.ledger.get_txn_author_agreement.return_value = {"taa_required": False}
+            self.ledger.get_latest_txn_author_acceptance.return_value = None
+            result = await test_module.ledger_get_taa(request)
+            json_response.assert_called_once_with(
+                {"result": {"taa_accepted": None, "taa_required": False}}
+            )
+            assert result is json_response.return_value
+
+    async def test_taa_accept_not_required(self):
+        request = async_mock.MagicMock()
+        request.app = self.app
+        request.json = async_mock.CoroutineMock(
+            return_value={
+                "version": "version",
+                "text": "text",
+                "mechanism": "mechanism",
+            }
+        )
+
+        with self.assertRaises(HTTPBadRequest):
+            self.ledger.LEDGER_TYPE = "indy"
+            self.ledger.get_txn_author_agreement.return_value = {"taa_required": False}
+            await test_module.ledger_accept_taa(request)
+
+    async def test_accept_taa(self):
+        request = async_mock.MagicMock()
+        request.app = self.app
+        request.json = async_mock.CoroutineMock(
+            return_value={
+                "version": "version",
+                "text": "text",
+                "mechanism": "mechanism",
+            }
+        )
+
+        with async_mock.patch.object(
+            test_module.web, "json_response", async_mock.Mock()
+        ) as json_response:
+            self.ledger.LEDGER_TYPE = "indy"
+            self.ledger.get_txn_author_agreement.return_value = {"taa_required": True}
+            result = await test_module.ledger_accept_taa(request)
+            json_response.assert_called_once_with({})
+            self.ledger.accept_txn_author_agreement.assert_awaited_once_with(
+                {
+                    "version": "version",
+                    "text": "text",
+                    "digest": self.ledger.taa_digest.return_value,
+                },
+                "mechanism",
             )
             assert result is json_response.return_value
