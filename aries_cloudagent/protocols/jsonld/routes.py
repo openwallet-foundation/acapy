48689abf4be623ae3043125039da11d82505a7e6
Sam Curren <telegramsam@gmail.com>
2020-04-17 09:58:10 -0600

    jsonld improvement
    Correct form, but signature not quite correct.
    Signed-off-by: Sam Curren <telegramsam@gmail.com>

--- aries_cloudagent/protocols/jsonld/routes.py
+++ aries_cloudagent/protocols/jsonld/routes.py
@@ -1,4 +1,5 @@
 """jsonld admin routes."""
+import datetime
 import json
 from pyld import jsonld
 
@@ -8,9 +9,16 @@ from aries_cloudagent.wallet.base import BaseWallet
 
 from marshmallow import fields, Schema
 
-from aries_cloudagent.wallet.util import bytes_to_b64
-from ...connections.models.connection_record import ConnectionRecord
-from ...storage.error import StorageNotFoundError
+from ...wallet.util import (
+    b58_to_bytes,
+    b64_to_bytes,
+    b64_to_str,
+    bytes_to_b58,
+    bytes_to_b64,
+    set_urlsafe_b64,
+    str_to_b64,
+    unpad,
+)
 
 
 class SignRequestSchema(Schema):
@@ -21,6 +29,21 @@ class SignRequestSchema(Schema):
 class SignResponseSchema(Schema):
     """Response schema for a signed jsonld doc."""
     signed_doc = fields.Dict(required=True)
+
+MULTIBASE_B58_BTC = "z"
+MULTICODEC_ED25519_PUB = b"\xed"
+
+def did_key(verkey: str) -> str:
+    """Qualify verkey into DID key if need be."""
+
+    if verkey.startswith(f"did:key:{MULTIBASE_B58_BTC}"):
+        return verkey
+
+    return f"did:key:{MULTIBASE_B58_BTC}" + bytes_to_b58(
+        MULTICODEC_ED25519_PUB + b58_to_bytes(verkey)
+    )
+
+
 
 @docs(tags=["jsonld"], summary="Sign a JSON-LD structure and return it")
 @request_schema(SignRequestSchema())
@@ -42,18 +65,30 @@ async def sign(request: web.BaseRequest):
     body = await request.json()
     verkey = body.get("verkey")
     doc = body.get("doc")
-    #comment = body.get("comment")
-    normalized = jsonld.normalize(
-        doc['credential'], {'algorithm': 'URDNA2015', 'format': 'application/n-quads'})
-    #message_bin = json.dumps(doc).encode("ascii")
-    message_bin = normalized.encode("ascii")
-    signature_bin = await wallet.sign_message(message_bin, verkey)
+    credential = doc['credential']
+    options = doc['options']
 
+    normalized = jsonld.normalize(
+        credential, {'algorithm': 'URDNA2015', 'format': 'application/n-quads'})
+
+    message_bin = normalized #.encode("ascii")
+    jose_header = {
+        "alg": "EdDSA",
+        "kid": did_key(verkey),
+    }
+    encoded_header = str_to_b64(json.dumps(jose_header), urlsafe=True, pad=False)
+    signature_bin = await wallet.sign_message((encoded_header + "." + message_bin).encode("ascii"), verkey)
+    proof = {
+        "type": "Ed25519Signature2018",
+        "created": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "verificationMethod": options["verificationMethod"],
+        "proofPurpose": options["proofPurpose"],
+        "jws": encoded_header + ".." + bytes_to_b64(signature_bin, urlsafe=True, pad=False)
+    }
+    credential['proof'] = proof
 
     return web.json_response({
-        "signed_doc": {
-            'signature': bytes_to_b64(signature_bin, urlsafe=True),
-        }
+        "signed_doc": doc,
     })
 
 
