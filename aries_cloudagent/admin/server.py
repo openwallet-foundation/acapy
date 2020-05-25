26410d0804e06745216774e4c941c2e6a84493fb
Matthias Binzer <matthias.binzer.ext@bosch.com>
2020-05-25 10:47:55 +0200

    Fix #427 websocket --admin-api-key connect issue
    This is the proposed option number 2. It uses the websocket
    to expect a message with the admin api key before it is being
    used to transmit any other messages than 'ping'
    
    Signed-off-by: Matthias Binzer <matthias.binzer.ext@bosch.com>

--- aries_cloudagent/admin/server.py
+++ aries_cloudagent/admin/server.py
@@ -170,6 +170,7 @@ class AdminServer(BaseAdminServer):
                 "/api/doc",
                 "/api/docs/swagger.json",
                 "/favicon.ico",
+                "/ws",  # ws handler checks authentication
             ] or path.startswith("/static/swagger/")
 
         # If admin_api_key is None, then admin_insecure_mode must be set so
@@ -181,7 +182,7 @@ class AdminServer(BaseAdminServer):
                 header_admin_api_key = request.headers.get("x-api-key")
                 valid_key = self.admin_api_key == header_admin_api_key
 
-                if valid_key or is_unprotected_path(request.rel_url.path.lower()):
+                if valid_key or is_unprotected_path(request.path):
                     return await handler(request)
                 else:
                     raise web.HTTPUnauthorized()
@@ -367,12 +368,21 @@ class AdminServer(BaseAdminServer):
         queue = BasicMessageQueue()
         loop = asyncio.get_event_loop()
 
+        if self.admin_insecure_mode:
+            # open to send websocket messages without api key auth
+            queue.authenticated = True
+        else:
+            header_admin_api_key = request.headers.get("x-api-key")
+            # authenticated via http header?
+            queue.authenticated = header_admin_api_key == self.admin_api_key
+
         try:
             self.websocket_queues[socket_id] = queue
             await queue.enqueue(
                 {
                     "topic": "settings",
                     "payload": {
+                        "authenticated": queue.authenticated,
                         "label": self.context.settings.get("default_label"),
                         "endpoint": self.context.settings.get("default_endpoint"),
                         "no_receive_invites": self.context.settings.get(
@@ -384,7 +394,7 @@ class AdminServer(BaseAdminServer):
             )
 
             closed = False
-            receive = loop.create_task(ws.receive())
+            receive = loop.create_task(ws.receive_json())
             send = loop.create_task(queue.dequeue(timeout=5.0))
 
             while not closed:
@@ -396,9 +406,22 @@ class AdminServer(BaseAdminServer):
                         closed = True
 
                     if receive.done():
-                        # ignored
                         if not closed:
-                            receive = loop.create_task(ws.receive())
+                            msg_received = None
+                            msg_api_key = None
+                            try:
+                                # this call can re-raise exeptions from inside the task
+                                msg_received = receive.result()
+                                msg_api_key = msg_received.get("x-api-key")
+                            except Exception:
+                                LOGGER.exception(
+                                    "Exception in websocket receiving task:"
+                                )
+                            if self.admin_api_key and self.admin_api_key == msg_api_key:
+                                # authenticated via websocket message
+                                queue.authenticated = True
+
+                            receive = loop.create_task(ws.receive_json())
 
                     if send.done():
                         try:
@@ -409,7 +432,10 @@ class AdminServer(BaseAdminServer):
                         if msg is None:
                             # we send fake pings because the JS client
                             # can't detect real ones
-                            msg = {"topic": "ping"}
+                            msg = {
+                                "topic": "ping",
+                                "authenticated": queue.authenticated,
+                            }
                         if not closed:
                             if msg:
                                 await ws.send_json(msg)
@@ -453,4 +479,5 @@ class AdminServer(BaseAdminServer):
                     )
 
         for queue in self.websocket_queues.values():
-            await queue.enqueue({"topic": topic, "payload": payload})
+            if queue.authenticated or topic in ("ping", "settings"):
+                await queue.enqueue({"topic": topic, "payload": payload})
