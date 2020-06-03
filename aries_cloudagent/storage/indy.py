25a019da06db1572239f4498e48884148dbf73eb
sklump <srklump@hotmail.com>
2020-06-03 14:39:58 +0000

    work in progress
    Signed-off-by: sklump <srklump@hotmail.com>
    Signed-off-by: spsingh <bot@spsinghs-MacBook-Pro.local>

--- aries_cloudagent/storage/indy.py
+++ aries_cloudagent/storage/indy.py
@@ -251,7 +251,7 @@ class IndyStorageRecordSearch(BaseStorageRecordSearch):
             page_size: Size of page to return
 
         """
-        super(IndyStorageRecordSearch, self).__init__(
+        super().__init__(
             store, type_filter, tag_query, page_size, options
         )
         self._handle = None
