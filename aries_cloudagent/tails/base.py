e4f99c58a480b7742af849b7969283697222fe87
Nicholas Rempel <nick@lucent.is>
2020-07-17 15:12:06 -0700

    Remove tails download from interface

--- aries_cloudagent/tails/base.py
+++ aries_cloudagent/tails/base.py
@@ -18,14 +18,3 @@ class BaseTailsServer(ABC, metaclass=ABCMeta):
             revo_reg_def_id: The Revocation registry definition ID
             tails_file: The path to the tails file to upload
         """
-
-    @abstractmethod
-    async def download_tails_file(
-        self, context: InjectionContext, revo_reg_def_id: str, location: str
-    ) -> str:
-        """Download tails file from tails server.
-
-        Args:
-            revo_reg_def_id: The Revocation registry definition ID
-            loction: Path to download destination
-        """
