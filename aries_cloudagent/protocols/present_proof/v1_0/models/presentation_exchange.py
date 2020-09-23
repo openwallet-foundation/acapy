4453073c64e28f8b3a7d28e3f91b89c317f5a9d1
Ian Costanzo <ian@anon-solutions.ca>
2020-09-23 15:13:23 -0700

    Use unencrypted tags for exchange records thread_id
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- aries_cloudagent/protocols/present_proof/v1_0/models/presentation_exchange.py
+++ aries_cloudagent/protocols/present_proof/v1_0/models/presentation_exchange.py
@@ -19,7 +19,7 @@ class V10PresentationExchange(BaseExchangeRecord):
     RECORD_TYPE = "presentation_exchange_v10"
     RECORD_ID_NAME = "presentation_exchange_id"
     WEBHOOK_TOPIC = "present_proof"
-    TAG_NAMES = {"thread_id"}
+    TAG_NAMES = {"~thread_id"}
 
     INITIATOR_SELF = "self"
     INITIATOR_EXTERNAL = "external"
