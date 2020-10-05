a15b30c2b71f56874bf8012ff01b87fe1661efc6
Ian Costanzo <ian@anon-solutions.ca>
2020-10-05 09:44:59 -0700

    Remove git dependency in setup script
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(os.path.abspath("./README.md"), "r") as fh:
 def parse_requirements(filename):
     """Load requirements from a pip requirements file."""
     lineiter = (line.strip() for line in open(filename))
-    return [line for line in lineiter if line and not line.startswith("#")]
+    return [line for line in lineiter if line and not line.startswith("#") and not line.startswith("git+")]
 
 
 if __name__ == "__main__":
