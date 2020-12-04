86d5ed70e7d0089b5290623ee3744afb92688dbc
Daniel Bluhm <dbluhm@pm.me>
2020-12-04 16:34:08 -0500

    test(mediation): mediation request handler tests
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- aries_cloudagent/protocols/coordinate_mediation/v1_0/handlers/tests/test_mediation_request_handler.py
+++ aries_cloudagent/protocols/coordinate_mediation/v1_0/handlers/tests/test_mediation_request_handler.py
@@ -1,77 +1,73 @@
+"""Test mediate request message handler."""
+import pytest
 from asynctest import TestCase as AsyncTestCase
 from asynctest import mock as async_mock
 
 from aries_cloudagent.config.injection_context import InjectionContext
+
+from ......connections.models.connection_record import ConnectionRecord
 from ......messaging.base_handler import HandlerException
 from ......messaging.request_context import RequestContext
 from ......messaging.responder import MockResponder
+from ......protocols.problem_report.v1_0.message import ProblemReport
+from ......storage.base import BaseStorage
+from ......storage.basic import BasicStorage
+from ......wallet.base import BaseWallet
+from ......wallet.basic import BasicWallet
+from ...messages.mediate_request import MediationRequest
+from ...messages.mediate_grant import MediationGrant
+from ...models.mediation_record import MediationRecord
+from ..mediate_request_handler import MediationRequestHandler
 
-from .. import mediate_request_handler as TestModule
-_handler = TestModule.MediationRequestHandler()
-from ...messages.mediate_request import MediationRequest as request
-from ...messages.mediate_grant import MediationGrant as response
-from ...messages.mediate_deny import MediationDeny as denied
+TEST_CONN_ID = 'conn-id'
+TEST_VERKEY = "3Dn1SJNPaCXcvvJvSbsFWP2xaCjMom3can8CQNhWrTRx"
 
-"""
-    Tests for Mediation based on "0211: Mediator Coordination Protocol" aries-rfc. 
-"""
+class TestMediationRequestHandler(AsyncTestCase):
+    """Test mediate request message handler."""
 
-
-class TestMediationRequestHandler:
-    async def setUp(self): # needed?
+    async def setUp(self):
         """setup dependencies of messaging"""
-
         self.context = RequestContext(
             base_context=InjectionContext(enforce_typing=False)
         )
-        self.context.message = request()
+        self.context.message = MediationRequest()
+        self.context.connection_ready = True
+        self.context.connection_record = ConnectionRecord(connection_id=TEST_CONN_ID)
+        self.context.injector.bind_instance(BaseStorage, BasicStorage())
+        self.context.injector.bind_instance(BaseWallet, BasicWallet())
+
+    async def test_handler_no_active_connection(self):
+        """ test mediation handler """
+        handler, responder = MediationRequestHandler(), MockResponder()
+        self.context.connection_ready = False
+        with pytest.raises(HandlerException) as exc:
+            await handler.handle(self.context,responder)
+            assert 'no active connection' in str(exc.value)
+
+    async def test_handler_mediation_record_already_exists(self):
+        handler, responder = MediationRequestHandler(), MockResponder()
+        await MediationRecord(connection_id=TEST_CONN_ID).save(self.context)
+        await handler.handle(self.context, responder)
+        messages = responder.messages
+        assert len(messages) == 1
+        result, _target = messages[0]
+        assert isinstance(result, ProblemReport)
 
     async def test_handler(self):
-        """ test mediation handler """
-        handler, responder = _handler(), MockResponder()
+        handler, responder = MediationRequestHandler(), MockResponder()
+        await handler.handle(self.context, responder)
+        record = await MediationRecord.retrieve_by_connection_id(self.context, TEST_CONN_ID)
+        assert record
+        assert record.state == MediationRecord.STATE_REQUEST_RECEIVED
 
-        mediate_request = request( mediator_terms = [], recipient_terms = [] )
-        with async_mock.patch.object(
-            TestModule, "ConnectionManager", autospec=True
-        ) as mock_mgr:
-            await handler.handle(self.context,responder)
-
-
-    async def test_for_denied_mediation_request(self):
-        """ Test for MEDIATE_REQUEST request that results in MEDIATE_DENY response. """
-        handler, responder = _handler(), MockResponder()
-
-        #TODO: update terms to real terms
-        mediate_request = request( mediator_terms = [False], recipient_terms = [False] ) 
-        with async_mock.patch.object(
-            TestModule, "ConnectionManager", autospec=True
-        ) as mock_mgr:
-            await handler.handle(self.context,responder)
-            messages = responder.messages
-
-            assert messages
-            assert len(messages) == 1
-            
-            (result, _) = messages[0]
-            assert type(result) == denied
-            assert result._thread._thid == self.context.message._message_id
-
-    
-    async def test_for_granted_mediation_request(self):
-        """ Test for MEDIATE_REQUEST request that results in MEDIATE_GRANT response. """
-        handler, responder = _handler(), MockResponder()
-
-        #TODO: update terms to real terms
-        mediate_request = request( mediator_terms = [True], recipient_terms = [True] ) 
-        with async_mock.patch.object(
-            TestModule, "ConnectionManager", autospec=True
-        ) as mock_mgr:
-            await handler.handle(self.context,responder)
-            messages = responder.messages
-
-            assert messages
-            assert len(messages) == 1
-            
-            (result, _) = messages[0]
-            assert type(result) == response
-            assert result._thread._thid == self.context.message._message_id
+    async def test_handler_open_mediation(self):
+        handler, responder = MediationRequestHandler(), MockResponder()
+        self.context.settings.set_value('mediation.open', True)
+        await handler.handle(self.context, responder)
+        record = await MediationRecord.retrieve_by_connection_id(self.context, TEST_CONN_ID)
+        assert record
+        assert record.state == MediationRecord.STATE_GRANTED
+        messages = responder.messages
+        assert len(messages) == 1
+        result, _target = messages[0]
+        assert isinstance(result, MediationGrant)
