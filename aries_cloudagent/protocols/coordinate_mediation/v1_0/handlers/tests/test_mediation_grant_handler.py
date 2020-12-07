dda02a3ab1e60e4ef3e415d60cf938d2742ff15d
Daniel Bluhm <dbluhm@pm.me>
2020-12-07 13:19:58 -0500

    test(mediation): grant handler tests
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

---
+++ aries_cloudagent/protocols/coordinate_mediation/v1_0/handlers/tests/test_mediation_grant_handler.py
@@ -0,0 +1,62 @@
+"""Test mediate grant message handler."""
+import pytest
+from asynctest import TestCase as AsyncTestCase
+from asynctest import mock as async_mock
+
+from ......config.injection_context import InjectionContext
+from ......connections.models.connection_record import ConnectionRecord
+from ......messaging.base_handler import HandlerException
+from ......messaging.request_context import RequestContext
+from ......messaging.responder import MockResponder
+from ......storage.base import BaseStorage
+from ......storage.basic import BasicStorage
+from ......wallet.base import BaseWallet
+from ......wallet.basic import BasicWallet
+from ...messages.mediate_grant import MediationGrant
+from ...models.mediation_record import MediationRecord
+from ..mediation_grant_handler import MediationGrantHandler
+
+TEST_CONN_ID = 'conn-id'
+TEST_VERKEY = "3Dn1SJNPaCXcvvJvSbsFWP2xaCjMom3can8CQNhWrTRx"
+TEST_ENDPOINT = 'https://example.com'
+
+
+class TestMediationGrantHandler(AsyncTestCase):
+    """Test mediate grant message handler."""
+
+    async def setUp(self):
+        """Setup test dependencies."""
+        self.context = RequestContext(
+            base_context=InjectionContext(enforce_typing=False)
+        )
+        self.context.message = MediationGrant(
+            endpoint=TEST_ENDPOINT,
+            routing_keys=[TEST_VERKEY]
+        )
+        self.context.connection_ready = True
+        self.context.connection_record = ConnectionRecord(connection_id=TEST_CONN_ID)
+        self.context.injector.bind_instance(BaseStorage, BasicStorage())
+        self.context.injector.bind_instance(BaseWallet, BasicWallet())
+
+    async def test_handler_no_active_connection(self):
+        handler, responder = MediationGrantHandler(), MockResponder()
+        self.context.connection_ready = False
+        with pytest.raises(HandlerException) as exc:
+            await handler.handle(self.context,responder)
+            assert 'no active connection' in str(exc.value)
+
+    async def test_handler_no_mediation_record(self):
+        handler, responder = MediationGrantHandler(), MockResponder()
+        with pytest.raises(HandlerException) as exc:
+            await handler.handle(self.context, responder)
+            assert 'has not been requested' in str(exc.value)
+
+    async def test_handler(self):
+        handler, responder = MediationGrantHandler(), MockResponder()
+        await MediationRecord(connection_id=TEST_CONN_ID).save(self.context)
+        await handler.handle(self.context, responder)
+        record = await MediationRecord.retrieve_by_connection_id(self.context, TEST_CONN_ID)
+        assert record
+        assert record.state == MediationRecord.STATE_GRANTED
+        assert record.endpoint == TEST_ENDPOINT
+        assert record.routing_keys == [TEST_VERKEY]
