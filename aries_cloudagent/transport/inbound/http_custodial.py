d74438ece936e3640dc85218ebb91bbee6a895e4
Ethan Sung <baegjae@gmail.com>
2020-12-07 14:57:59 +0900

    Fix wallet error when getting wallet at inbound message handler
    Signed-off-by: Ethan Sung <baegjae@gmail.com>

--- aries_cloudagent/transport/inbound/http_custodial.py
+++ aries_cloudagent/transport/inbound/http_custodial.py
@@ -102,7 +102,8 @@ class CustodialHttpTransport(BaseInboundTransport):
             wallet_ids = await wallet_handler.get_wallet_by_msg(body)
             session.context = session.context.copy()
             # FIXME: What if multiple recipients are handled by the agent?
-            session.context.settings.set_value("wallet.id", wallet_ids[0])
+            # prevent getting wallet without opening wallet
+            await wallet_handler.set_instance(wallet_ids[0], session.context)
 
         async with session:
 
