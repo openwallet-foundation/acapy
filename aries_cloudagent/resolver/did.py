19f92fb261341ca630c6ceb221bfd5f1dbe6efae
Daniel Bluhm <dbluhm@pm.me>
2021-01-20 22:07:14 -0500

    feat: add DIDUrl class
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- aries_cloudagent/resolver/did.py
+++ aries_cloudagent/resolver/did.py
@@ -9,21 +9,109 @@ January 2021:
 
 import re
 from typing import Dict
+from urllib.parse import urlparse, parse_qsl, urlencode
+
+DID_PATTERN = re.compile("did:([a-z]+):((?:[a-zA-Z0-9._-]*:)*[a-zA-Z0-9._-]+)")
 
 
 class InvalidDIDError(Exception):
     """Invalid DID."""
 
 
+class InvalidDIDUrlError(Exception):
+    """Invalid DID."""
+
+
+class DIDUrl:
+    """DID URL."""
+
+    def __init__(
+        self,
+        did: str,
+        path: str = None,
+        query: Dict[str, str] = None,
+        fragment: str = None,
+    ):
+        """Initialize DID URL.
+
+        Leading '/' of path inserted if absent.
+        """
+        self.did = did
+        if path and not path.startswith("/"):
+            path = "/" + path
+        self.path = path
+
+        self.query = query
+        self.fragment = fragment
+        self.url = self._stringify()
+
+    def _stringify(self):
+        """Return a DID URL.
+
+        Leading '/' of path inserted if absent.
+        Delimiters for query and fragment will be inserted.
+        """
+        value = self.did
+        if self.path:
+            value += self.path
+
+        if self.query:
+            value += "?" + urlencode(self.query)
+
+        if self.fragment:
+            value += "#" + self.fragment
+
+        return value
+
+    def __str__(self):
+        """Return string representation of DID URL.
+
+        Delimiters for query and fragment will be inserted.
+        """
+        return self.url
+
+    def __repr__(self):
+        """Return debug representation of DID URL."""
+        return "<DIDUrl {}>".format(self.url)
+
+    def __eq__(self, other):
+        """Check equality."""
+        if not isinstance(other, DIDUrl):
+            return False
+        return (
+            self.did == other.did
+            and self.path == other.path
+            and self.query == other.query
+            and self.fragment == other.fragment
+            and self.url == other.url
+        )
+
+    @classmethod
+    def parse(cls, url: str):
+        """Parse DID URL from string."""
+        matches = DID_PATTERN.match(url)
+
+        if not matches:
+            raise InvalidDIDUrlError("DID could not be parsed from URL {}.".format(url))
+
+        did = matches.group(0)
+        _, url = url.split(did)
+        parts = urlparse(url)
+        return cls(
+            did,
+            parts.path or None,
+            dict(parse_qsl(parts.query)) if parts.query else None,
+            parts.fragment or None
+        )
+
+
 class DID:
     """DID Representation and helpers."""
-
-    PATTERN = re.compile("did:([a-z]+):((?:[a-zA-Z0-9._-]*:)*[a-zA-Z0-9._-]+)")
 
     def __init__(self, did: str):
         """Validate and parse raw DID str."""
         self._raw = did
-        matched = self.PATTERN.match(did)
+        matched = DID_PATTERN.match(did)
         if not matched:
             raise InvalidDIDError("Unable to parse DID {}".format(did))
         self._method = matched.group(1)
@@ -45,7 +133,7 @@ class DID:
 
     def __repr__(self):
         """Return debug representation of DID."""
-        return self._raw
+        return "<DID {}>".format(self._raw)
 
     def __eq__(self, other):
         """Test equality."""
@@ -57,24 +145,5 @@ class DID:
         return False
 
     def url(self, path: str = None, query: Dict[str, str] = None, fragment: str = None):
-        """Return a DID URL.
-
-        Leading '/' of path inserted if absent.
-        Delimiters for query and fragment will be inserted.
-        """
-        stem = str(self)
-        path = path or ""
-        if path and not path.startswith("/"):
-            path = "/" + path
-
-        query = query or ""
-        if query:
-            query = "?" + "&".join(
-                ["{}={}".format(key, value) for key, value in query.items()]
-            )
-
-        fragment = fragment or ""
-        if fragment:
-            fragment = "#" + fragment
-
-        return "{}{}{}{}".format(stem, path, query, fragment)
+        """Return a DID URL for this DID."""
+        return DIDUrl(str(self), path, query, fragment)
