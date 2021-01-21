27ea1b4476d7fb6f58f64f86e699be3ad53fda76
Daniel Bluhm <dbluhm@pm.me>
2021-01-20 22:32:40 -0500

    feat: did doc dereferencing by url using index
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- aries_cloudagent/resolver/tests/test_diddoc.py
+++ aries_cloudagent/resolver/tests/test_diddoc.py
@@ -2,10 +2,12 @@
 
 import pytest
 
+from ..diddoc import ExternalResourceError, ResolvedDIDDoc, _index_ids_of_doc
+
 DOC = {
     "@context": "https://w3id.org/did/v1",
     "id": "did:example:1234abcd",
-    "publicKey": [
+    "verificationMethod": [
         {
             "id": "3",
             "type": "RsaVerificationKey2018",
@@ -42,3 +44,29 @@ DOC = {
         }
     ],
 }
+
+
+def test_index():
+    index = _index_ids_of_doc(DOC)
+    assert len(index) == 5
+    assert "did:example:1234abcd" in index
+    assert "3" in index
+    assert "4" in index
+    assert "6" in index
+    assert "did:example:123456789abcdefghi;did-communication" in index
+
+
+def test_dereference():
+    doc = ResolvedDIDDoc(DOC)
+    assert doc.dereference("did:example:1234abcd#4") == DOC["verificationMethod"][1]
+
+def test_dereference_x():
+    doc = ResolvedDIDDoc(DOC)
+    with pytest.raises(ExternalResourceError):
+        doc.dereference("did:example:different#4")
+
+def test_basic():
+    doc = ResolvedDIDDoc(DOC)
+    service, *_ = doc.didcomm_services()
+    for key_ref in service["recipientKeys"]:
+        doc.dereference(key_ref)
