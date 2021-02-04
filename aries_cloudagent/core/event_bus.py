24f0178e5c12307aee395e8b0748f75d2028886d
Daniel Bluhm <dbluhm@pm.me>
2021-02-03 20:54:47 -0500

    feat: add simple event bus
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

---
+++ aries_cloudagent/core/event_bus.py
@@ -0,0 +1,97 @@
+"""A simple event bus."""
+
+from itertools import chain
+from typing import Any, Callable, Dict, Pattern, Sequence
+from .profile import Profile
+
+
+class Event:
+    """A simple event object."""
+
+    def __init__(
+        self,
+        topic: str,
+        payload: Any = None
+    ):
+        """Create a new event."""
+        self._topic = topic
+        self._payload = payload
+
+    @property
+    def topic(self):
+        """Return this event's topic."""
+        return self._topic
+
+    @property
+    def payload(self):
+        """Return this event's payload."""
+        return self._payload
+
+    def __eq__(self, other):
+        """Test equality."""
+        if not isinstance(other, Event):
+            return False
+        return self._topic == other._topic and self._payload == other._payload
+
+    def __repr__(self):
+        return "<Event topic={}, payload={}>".format(self._topic, self._payload)
+
+
+class EventBus:
+    """A simple event bus implementation."""
+
+    def __init__(self):
+        self.topic_patterns_to_subscribers: Dict[Pattern, Sequence[Callable]] = {}
+
+    async def notify(self, profile: Profile, event: Event):
+        """Notify subscribers of event.
+
+        Args:
+            profile (Profile): profile of the event
+            event (Event): event to emit
+
+        """
+        # TODO don't block notifier until subscribers have all been called?
+        # TODO trigger each processor but don't await?
+        # TODO log errors but otherwise ignore?
+
+        matched = [
+            processor
+            for pattern, processor in self.topic_patterns_to_subscribers.items()
+            if pattern.match(event.topic)
+        ]
+
+        for processor in chain(*matched):
+            await processor(profile, event)
+
+    def subscribe(self, pattern: Pattern, processor: Callable):
+        """Subscribe to an event.
+
+        Args:
+            pattern (Pattern): compiled regular expression for matching topics
+            processor (Callable): async callable accepting profile and event
+
+        """
+        if pattern not in self.topic_patterns_to_subscribers:
+            self.topic_patterns_to_subscribers[pattern] = []
+        self.topic_patterns_to_subscribers[pattern].append(processor)
+
+    def unsubscribe(self, pattern: Pattern, processor: Callable):
+        """Unsubscribe from an event.
+
+        This method is idempotent. Repeated calls to unsubscribe will not
+        result in errors.
+
+        Args:
+            pattern (Pattern): regular expression used to subscribe the processor
+            processor (Callable): processor to unsubscribe
+
+        """
+        if pattern in self.topic_patterns_to_subscribers:
+            try:
+                index = self.topic_patterns_to_subscribers[pattern].index(processor)
+            except ValueError:
+                return
+            del self.topic_patterns_to_subscribers[pattern][index]
+            if not self.topic_patterns_to_subscribers[pattern]:
+                del self.topic_patterns_to_subscribers[pattern]
---
+++ aries_cloudagent/core/tests/test_event_bus.py
@@ -0,0 +1,152 @@
+"""Test Event Bus."""
+
+import pytest
+import re
+from asynctest import mock as async_mock
+from ..event_bus import EventBus, Event
+
+# pylint: disable=redefined-outer-name
+
+@pytest.fixture
+def event_bus():
+    yield EventBus()
+
+
+@pytest.fixture
+def profile():
+    yield async_mock.MagicMock()
+
+
+@pytest.fixture
+def event():
+    event = Event(topic="anything", payload="payload")
+    yield event
+
+
+class TestProcessor:
+    def __init__(self):
+        self.profile = None
+        self.event = None
+
+    async def __call__(self, profile, event):
+        self.profile = profile
+        self.event = event
+
+
+@pytest.fixture
+def processor():
+    yield TestProcessor()
+
+
+def test_event(event):
+    assert event.topic == "anything"
+    assert event.payload == "payload"
+    other = Event("anything", "payload")
+    another = Event("nothing", "payload")
+    and_another = Event("anything")
+    assert event == other
+    assert event != another
+    assert event != and_another
+    assert event != "random string"
+    assert repr(event)
+
+
+def test_sub_unsub(event_bus: EventBus, processor):
+    """Test subscribe and unsubscribe."""
+    event_bus.subscribe(re.compile('.*'), processor)
+    assert event_bus.topic_patterns_to_subscribers
+    assert event_bus.topic_patterns_to_subscribers[re.compile('.*')] == [processor]
+    event_bus.unsubscribe(re.compile('.*'), processor)
+    assert not event_bus.topic_patterns_to_subscribers
+
+
+def test_unsub_idempotency(event_bus: EventBus, processor):
+    """Test unsubscribe idempotency."""
+    event_bus.subscribe(re.compile('.*'), processor)
+    event_bus.unsubscribe(re.compile('.*'), processor)
+    assert not event_bus.topic_patterns_to_subscribers
+    event_bus.unsubscribe(re.compile('.*'), processor)
+    assert not event_bus.topic_patterns_to_subscribers
+
+
+@pytest.mark.asyncio
+async def test_sub_notify(event_bus: EventBus, profile, event, processor):
+    """Test subscriber receives event."""
+    event_bus.subscribe(re.compile('.*'), processor)
+    await event_bus.notify(profile, event)
+    assert processor.profile == profile
+    assert processor.event == event
+
+
+@pytest.mark.parametrize(
+    "pattern, topic",
+    [
+        ("test", "test"),
+        (".*", "test"),
+        ("topic::with::namespace", "topic::with::namespace::like::pieces"),
+    ]
+)
+@pytest.mark.asyncio
+async def test_sub_notify_regex_filtering(
+    event_bus: EventBus,
+    profile,
+    processor,
+    pattern,
+    topic
+):
+    """Test events are filtered correctly."""
+    event = Event(topic)
+    event_bus.subscribe(re.compile(pattern), processor)
+    await event_bus.notify(profile, event)
+    assert processor.profile == profile
+    assert processor.event == event
+
+
+@pytest.mark.asyncio
+async def test_sub_notify_no_match(
+    event_bus: EventBus,
+    profile,
+    event,
+    processor
+):
+    """Test event not given to processor when pattern doesn't match."""
+    event_bus.subscribe(re.compile("^$"), processor)
+    await event_bus.notify(profile, event)
+    assert processor.profile is None
+    assert processor.event is None
+
+
+@pytest.mark.asyncio
+async def test_sub_notify_only_one(
+    event_bus: EventBus,
+    profile,
+    event,
+    processor
+):
+    """Test only one subscriber is called when pattern matches only one."""
+    processor1 = TestProcessor()
+    event_bus.subscribe(re.compile(".*"), processor)
+    event_bus.subscribe(re.compile("^$"), processor1)
+    await event_bus.notify(profile, event)
+    assert processor.profile == profile
+    assert processor.event == event
+    assert processor1.profile is None
+    assert processor1.event is None
+
+
+@pytest.mark.asyncio
+async def test_sub_notify_both(
+    event_bus: EventBus,
+    profile,
+    event,
+    processor
+):
+    """Test both subscribers are called when pattern matches both."""
+    processor1 = TestProcessor()
+    event_bus.subscribe(re.compile(".*"), processor)
+    event_bus.subscribe(re.compile("anything"), processor1)
+    await event_bus.notify(profile, event)
+    assert processor.profile == profile
+    assert processor.event == event
+    assert processor1.profile == profile
+    assert processor1.event == event
