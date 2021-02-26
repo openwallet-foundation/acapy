9cf7f6ee524612ed44533004918bca5926cdb976
Daniel Bluhm <dbluhm@pm.me>
2021-02-25 21:15:20 -0500

    fix: indy resolver registration
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- aries_cloudagent/resolver/__init__.py
+++ aries_cloudagent/resolver/__init__.py
@@ -1,15 +1,21 @@
 """Interfaces and base classes for DID Resolution."""
 
+import logging
+
 from ..config.injection_context import InjectionContext
-from .did_resolver_registry import DIDResolverRegistry
 from .default.indy import IndyDIDResolver
-from .default.http_universal import HTTPUniversalDIDResolver
+from .did_resolver_registry import DIDResolverRegistry
+
+LOGGER = logging.getLogger(__name__)
 
 
 async def setup(context: InjectionContext):
     """Set up default resolvers."""
-    registry = context.inject(DIDResolverRegistry)
-    for Resolver in (IndyDIDResolver, HTTPUniversalDIDResolver):
-        resolver = Resolver()
-        await resolver.setup(context)
-        registry.register(resolver)
+    registry = context.inject(DIDResolverRegistry, required=False)
+    if not registry:
+        LOGGER.warning("No DID Resolver Registry instance found in context")
+        return
+
+    resolver = IndyDIDResolver()
+    await resolver.setup(context)
+    registry.register(resolver)
