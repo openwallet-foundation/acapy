05ea56cfe7d42391dc23fe7d46f312e55d495fde
DucaDellaForcoletta <a.vignapiano@gmail.com>
2021-03-04 17:54:11 +0100

    Added ledger keepalive startup parameter ref #1006
    Signed-off-by: DucaDellaForcoletta <a.vignapiano@gmail.com>

--- aries_cloudagent/config/argparse.py
+++ aries_cloudagent/config/argparse.py
@@ -559,6 +559,13 @@ class LedgerGroup(ArgumentGroup):
             This must be set if running in no-ledger mode.  Overrides any\
             specified ledger or genesis configurations.  Default: false.",
         )
+        parser.add_argument(
+            "--ledger-keepalive",
+            default=5,
+            type=ByteSize(min_size=5),
+            env_var="ACAPY_LEDGER_KEEP_ALIVE",
+            help="Specifies how many seconds to keep the ledger open. Default: 5",
+        )
 
     def get_settings(self, args: Namespace) -> dict:
         """Extract ledger settings."""
@@ -580,6 +587,10 @@ class LedgerGroup(ArgumentGroup):
                 )
             if args.ledger_pool_name:
                 settings["ledger.pool_name"] = args.ledger_pool_name
+
+            if args.ledger_keepalive:
+                settings["ledger.keepalive"] = args.ledger_keepalive
+
         return settings
 
 
