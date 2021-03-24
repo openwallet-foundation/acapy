a351c553ca72b26e79158e2d519159e2cead39ba
Timo Glastra <timo@animo.id>
2021-03-24 11:50:00 +0100

    add test
    Signed-off-by: Timo Glastra <timo@animo.id>

--- aries_cloudagent/vc/vc_ld/tests/test_vc_ld.py
+++ aries_cloudagent/vc/vc_ld/tests/test_vc_ld.py
@@ -29,15 +29,19 @@ from .test_credential import (
 
 class TestLinkedDataVerifiableCredential(TestCase):
     test_seed = "testseed000000000000000000000001"
-    key_info: KeyInfo = None
+    test_seed2 = "testseed000000000000000000000002"
+    issuer_key_info: KeyInfo = None
 
     async def setUp(self):
         self.profile = InMemoryProfile.test_profile()
         self.wallet = InMemoryWallet(self.profile)
-        self.key_info = await self.wallet.create_signing_key(self.test_seed)
-        self.verification_method = DIDKey.from_public_key_b58(
-            self.key_info.verkey, KeyType.ED25519
+
+        self.issuer_key_info = await self.wallet.create_signing_key(self.test_seed)
+        self.issuer_verification_method = DIDKey.from_public_key_b58(
+            self.issuer_key_info.verkey, KeyType.ED25519
         ).key_id
+
+        self.holder_key_info = await self.wallet.create_signing_key(self.test_seed2)
         self.presentation_challenge = "2b1bbff6-e608-4368-bf84-67471b27e41c"
 
     async def test_issue(self):
@@ -45,9 +49,9 @@ class TestLinkedDataVerifiableCredential(TestCase):
         # as during verification a lot of information can be extracted
         # from the proof / document
         suite = Ed25519Signature2018(
-            verification_method=self.verification_method,
+            verification_method=self.issuer_verification_method,
             key_pair=Ed25519WalletKeyPair(
-                wallet=self.wallet, public_key_base58=self.key_info.verkey
+                wallet=self.wallet, public_key_base58=self.issuer_key_info.verkey
             ),
             date=datetime.strptime("2019-12-11T03:50:55Z", "%Y-%m-%dT%H:%M:%SZ"),
         )
@@ -81,9 +85,9 @@ class TestLinkedDataVerifiableCredential(TestCase):
         )
 
         suite = Ed25519Signature2018(
-            verification_method=self.verification_method,
+            verification_method=self.issuer_verification_method,
             key_pair=Ed25519WalletKeyPair(
-                wallet=self.wallet, public_key_base58=self.key_info.verkey
+                wallet=self.wallet, public_key_base58=self.issuer_key_info.verkey
             ),
             date=datetime.strptime("2020-12-11T03:50:55Z", "%Y-%m-%dT%H:%M:%SZ"),
         )
@@ -98,6 +102,47 @@ class TestLinkedDataVerifiableCredential(TestCase):
         )
 
         assert presentation == PRESENTATION_SIGNED
+
+    async def test_create_presentation_same_subject(self):
+        # TODO: subject is holder proof purpose or something?
+        issuer_suite = Ed25519Signature2018(
+            verification_method=self.issuer_verification_method,
+            key_pair=Ed25519WalletKeyPair(
+                wallet=self.wallet, public_key_base58=self.issuer_key_info.verkey
+            ),
+            date=datetime.strptime("2020-12-11T03:50:55Z", "%Y-%m-%dT%H:%M:%SZ"),
+        )
+
+        holder_did = DIDKey.from_public_key_b58(
+            self.holder_key_info.verkey, KeyType.ED25519
+        )
+        holder_verification_method = holder_did.key_id
+
+        holder_suite = Ed25519Signature2018(
+            verification_method=holder_verification_method,
+            key_pair=Ed25519WalletKeyPair(
+                wallet=self.wallet, public_key_base58=self.holder_key_info.verkey
+            ),
+            date=datetime.strptime("2021-12-11T03:50:55Z", "%Y-%m-%dT%H:%M:%SZ"),
+        )
+
+        credential_template = CREDENTIAL_TEMPLATE.copy()
+        credential_template["credentialSubject"]["id"] = holder_did.did
+
+        issued = await issue(
+            credential=credential_template,
+            suite=issuer_suite,
+            document_loader=custom_document_loader,
+        )
+
+        unsigned_presentation = await create_presentation(credentials=[issued])
+
+        presentation = await sign_presentation(
+            presentation=unsigned_presentation,
+            suite=holder_suite,
+            document_loader=custom_document_loader,
+            challenge=self.presentation_challenge,
+        )
 
     async def test_verify_presentation(self):
         # TODO: verify with multiple suites
