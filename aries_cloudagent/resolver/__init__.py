fddb76ca01c7a22bd98e397d8f7ddecc38712a38
Timo Glastra <timo@animo.id>
2021-04-12 12:51:37 +0200

    fix: register did:key resolver if no ledger
    Signed-off-by: Timo Glastra <timo@animo.id>

--- aries_cloudagent/resolver/__init__.py
+++ aries_cloudagent/resolver/__init__.py
@@ -16,18 +16,18 @@ async def setup(context: InjectionContext):
         LOGGER.warning("No DID Resolver Registry instance found in context")
         return
 
-    if context.settings.get("ledger.disabled"):
-        LOGGER.warning("Ledger is not configured, not loading IndyDIDResolver")
-        return
-
-    indy_resolver = ClassProvider(
-        "aries_cloudagent.resolver.default.indy.IndyDIDResolver"
-    ).provide(context.settings, context.injector)
-    await indy_resolver.setup(context)
-    registry.register(indy_resolver)
-
     key_resolver = ClassProvider(
         "aries_cloudagent.resolver.default.key.KeyDIDResolver"
     ).provide(context.settings, context.injector)
     await key_resolver.setup(context)
     registry.register(key_resolver)
+
+    if not context.settings.get("ledger.disabled"):
+        indy_resolver = ClassProvider(
+            "aries_cloudagent.resolver.default.indy.IndyDIDResolver"
+        ).provide(context.settings, context.injector)
+        await indy_resolver.setup(context)
+        registry.register(indy_resolver)
+
+    else:
+        LOGGER.warning("Ledger is not configured, not loading IndyDIDResolver")
