c8ccdba41aabdebca9aafede8e1c82e510ce4b72
Daniel Bluhm <dbluhm@pm.me>
2021-04-13 13:54:30 -0400

    test: fix plugin loading tests
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- aries_cloudagent/core/tests/test_plugin_registry.py
+++ aries_cloudagent/core/tests/test_plugin_registry.py
@@ -430,7 +430,7 @@ class TestPluginRegistry(AsyncTestCase):
             ClassLoader, "load_module", async_mock.MagicMock()
         ) as load_module:
             load_module.side_effect = [
-                async_mock.MagicMock(),  # module
+                {},  # module
                 None,  # routes
                 None,  # message types
                 None,  # definition
@@ -442,7 +442,7 @@ class TestPluginRegistry(AsyncTestCase):
             ClassLoader, "load_module", async_mock.MagicMock()
         ) as load_module:
             load_module.side_effect = [
-                async_mock.MagicMock(),  # module
+                {},  # module
                 None,  # routes
                 None,  # message types
                 "str-has-no-versions-attr",  # definition without versions attr
@@ -450,18 +450,27 @@ class TestPluginRegistry(AsyncTestCase):
             assert self.registry.register_plugin("dummy") is None
 
     async def test_register_plugin_has_setup(self):
+        class MODULE:
+            setup = "present"
+
+        obj = MODULE()
         with async_mock.patch.object(
-            ClassLoader, "load_module", async_mock.MagicMock(return_value = {"setup":"valid"})
+            ClassLoader, "load_module", async_mock.MagicMock()
         ) as load_module:
-            self.registry.register_plugin("dummy")
-            assert self.registry._plugins["dummy"] == {"setup":"valid"}
-    
+            load_module.side_effect = [
+                obj,  # module
+                None,  # routes
+                None,  # message types
+                None,  # definition without versions attr
+            ]
+            assert self.registry.register_plugin("dummy") == obj
+
     async def test_register_definitions_malformed(self):
         with async_mock.patch.object(
             ClassLoader, "load_module", async_mock.MagicMock()
         ) as load_module:
             load_module.side_effect = [
-                async_mock.MagicMock(),  # module
+                {},  # module
                 None,  # routes
                 None,  # message types
                 async_mock.MagicMock(versions="not-a-list"),
