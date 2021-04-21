13de1b1b7877eb216e9054af1cb01f3f6367a545
sklump <srklump@hotmail.com>
2021-04-21 10:38:23 +0000

    strip out debug print
    Signed-off-by: sklump <srklump@hotmail.com>

--- aries_cloudagent/storage/base.py
+++ aries_cloudagent/storage/base.py
@@ -91,7 +91,6 @@ class BaseStorage(ABC):
         if not results:
             raise StorageNotFoundError("Record not found")
         if len(results) > 1:
-            print(f"\n!! !! TOO Many results {results}")
             raise StorageDuplicateError("Duplicate records found")
         return results[0]
 
