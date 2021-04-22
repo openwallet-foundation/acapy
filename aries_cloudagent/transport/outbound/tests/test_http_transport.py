ad8416e2bb468dfcbc2f8aa69e9532c43ff0d308
wadeking98 <wkingnumber2@gmail.com>
2021-04-22 12:54:45 -0700

    revert changes to comparison method in test
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- aries_cloudagent/transport/outbound/tests/test_http_transport.py
+++ aries_cloudagent/transport/outbound/tests/test_http_transport.py
@@ -1,7 +1,6 @@
 import asyncio
 import pytest
 
-from hmac import compare_digest
 from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
 from aiohttp import web
 from asynctest import mock as async_mock
@@ -67,7 +66,7 @@ class TestHttpTransport(AioHTTPTestCase):
             send_message(transport, "{}", endpoint=server_addr, api_key=api_key), 5.0
         )
         assert self.message_results == [{}]
-        assert api_key and compare_digest(self.headers.get("x-api-key"), api_key)
+        assert self.headers.get("x-api-key") == api_key
 
     @unittest_run_loop
     async def test_handle_message_packed_compat_mime_type(self):
