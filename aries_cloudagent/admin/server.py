e9b744c76eda7c7df53623f92981db69f5aa2273
wadeking98 <wkingnumber2@gmail.com>
2021-04-22 14:54:10 -0700

    tweak function doc string
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- aries_cloudagent/admin/server.py
+++ aries_cloudagent/admin/server.py
@@ -221,7 +221,7 @@ async def debug_middleware(request: web.BaseRequest, handler: Coroutine):
 
 
 def const_compare(string1, string2):
-    """Compare two strings in constant time. This prevents timing attacks."""
+    """Compare two strings in constant time."""
     if string1 is None or string2 is None:
         return False
     return compare_digest(string1.encode(), string2.encode())
