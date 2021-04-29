a5f2c1e49142ba1739dd9adfd34d409ca52f1045
Jacky Yuan <jackyyuanyyj@gmail.com>
2021-04-29 16:21:25 -0600

    minor changes incorporating feedback
    Signed-off-by: Jacky Yuan <jackyyuanyyj@gmail.com>

--- aries_cloudagent/crypto/derive1PU.py
+++ aries_cloudagent/crypto/derive1PU.py
@@ -5,24 +5,27 @@ from deriveECDH import *
 # One set of secrets from sender/receiver (zs), the second set from ephemeral sender/receiver (ze)
 def derive1PU(ze, zs, alg, apu, apv, keydatalen):
 
-    # ECDH-1PU requires a "round number 1" to be prefixed onto the shared secret z
-    prefix = (1).to_bytes(4, "big")
-    z = prefix + ze + zs
-
+    z = ze + zs
     key = ConcatKDF(z, alg, apu, apv, keydatalen)
     return key
 
+
 # The sender generates two shared secrets (ze, zs)
-def deriveSender1PU(senderEphemeralPriv, senderPriv, recvPub, alg, apu, apv, keydatalen):
-    
+def deriveSender1PU(
+    senderEphemeralPriv, senderPriv, recvPub, alg, apu, apv, keydatalen
+):
+
     ze = DeriveECDHSecret(senderEphemeralPriv, recvPub)
     zs = DeriveECDHSecret(senderPriv, recvPub)
 
     key = derive1PU(ze, zs, alg, apu, apv, keydatalen)
     return key
 
+
 # The receiver generates two shared secrets (ze, zs)
-def deriveReceiver1PU(senderEphemeralPub, senderPub, recvPriv , alg, apu, apv, keydatalen):
+def deriveReceiver1PU(
+    senderEphemeralPub, senderPub, recvPriv, alg, apu, apv, keydatalen
+):
 
     ze = DeriveECDHSecret(recvPriv, senderEphemeralPub)
     zs = DeriveECDHSecret(recvPriv, senderPub)
@@ -30,4 +33,3 @@ def deriveReceiver1PU(senderEphemeralPub, senderPub, recvPriv , alg, apu, apv, k
     key = derive1PU(ze, zs, alg, apu, apv, keydatalen)
 
     return key
-
--- aries_cloudagent/crypto/deriveECDH.py
+++ aries_cloudagent/crypto/deriveECDH.py
@@ -12,6 +12,7 @@ def DeriveECDHSecret(privateKey, publicKey):
     secret = derive.generate_sharedsecret_bytes()
     return secret
 
+
 # Generate a shared secret from your private key and a received public key (keys are in Keys object format)
 # Use a ecdsa.Keys object
 def DeriveECDHSecretFromKey(privateKey, publicKey):
@@ -23,14 +24,19 @@ def DeriveECDHSecretFromKey(privateKey, publicKey):
     secret = derive.generate_sharedsecret_bytes()
     return secret
 
+
 # Generate a shared encryption key from a shared secret and header parameters
 def ConcatKDF(sharedSecret, alg, apu, apv, keydatalen):
 
+    # ECDH-1PU requires a "round number 1" to be prefixed onto the shared secret z
+    prefix = (1).to_bytes(4, "big")
+    sharedSecret = prefix + sharedSecret
+
     # ECDH-1PU requires each of the header parameters to be front padded with their string length
-    AlgID = len(alg).to_bytes(4, "big") + bytes(alg, 'utf-8')
-    PartyUInfo = len(apu).to_bytes(4, "big") + bytes(apu, 'utf-8')
-    PartyVInfo = len(apv).to_bytes(4, "big") + bytes(apv, 'utf-8')
-    SuppPubInfo = (keydatalen*8).to_bytes(4, "big")
+    AlgID = len(alg).to_bytes(4, "big") + bytes(alg, "utf-8")
+    PartyUInfo = len(apu).to_bytes(4, "big") + bytes(apu, "utf-8")
+    PartyVInfo = len(apv).to_bytes(4, "big") + bytes(apv, "utf-8")
+    SuppPubInfo = (keydatalen * 8).to_bytes(4, "big")
 
     otherinfo = AlgID + PartyUInfo + PartyVInfo + SuppPubInfo
 
