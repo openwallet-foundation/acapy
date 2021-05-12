f0477104d3e9f4fa9641b613ffd2d3711ed0a181
Shaanjot Gill <shaangill025@users.noreply.github.com>
2021-05-12 01:05:39 -0700

    formatting fix
    Signed-off-by: Shaanjot Gill <shaangill025@users.noreply.github.com>

--- aries_cloudagent/protocols/present_proof/v2_0/formats/dif/handler.py
+++ aries_cloudagent/protocols/present_proof/v2_0/formats/dif/handler.py
@@ -54,7 +54,9 @@ class DIFPresFormatHandler(V20PresFormatHandler):
 
     if BbsBlsSignature2020.BBS_SUPPORTED:
         ISSUE_SIGNATURE_SUITE_KEY_TYPE_MAPPING[BbsBlsSignature2020] = KeyType.BLS12381G2
-        ISSUE_SIGNATURE_SUITE_KEY_TYPE_MAPPING[BbsBlsSignatureProof2020] = KeyType.BLS12381G2
+        ISSUE_SIGNATURE_SUITE_KEY_TYPE_MAPPING[
+            BbsBlsSignatureProof2020
+        ] = KeyType.BLS12381G2
 
     async def _get_all_suites(self, wallet: BaseWallet):
         """Get all supported suites for verifying presentation."""
