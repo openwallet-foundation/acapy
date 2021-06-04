84035ceee613ce203f1a391103c70601df1045e7
Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>
2021-06-04 16:52:43 +0200

    formatting...
    Signed-off-by: Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>

--- aries_cloudagent/resolver/default/tests/test_web.py
+++ aries_cloudagent/resolver/default/tests/test_web.py
@@ -22,6 +22,6 @@ def test_transformation_domain_with_path(resolver):
 
 
 def test_transformation_domain_with_port(resolver):
-    did = 'did:web:localhost%3A443'
+    did = "did:web:localhost%3A443"
     url = resolver._WebDIDResolver__transform_to_url(did)
     assert url == "https://localhost:443/.well-known/did.json"
