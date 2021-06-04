8d75f44cf4978e85e934282cdda2fa64b3f6c929
Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>
2021-06-04 16:49:33 +0200

    bumping pyDID to 0.2.6 and enabling test with encoded port
    Signed-off-by: Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>

--- aries_cloudagent/resolver/default/tests/test_web.py
+++ aries_cloudagent/resolver/default/tests/test_web.py
@@ -21,8 +21,7 @@ def test_transformation_domain_with_path(resolver):
     assert url == "https://example.com/department/example/did.json"
 
 
-# TODO: Enable this test as soon as pyDID accepts % char in DID
-# def test_transformation_domain_with_port(resolver):
-#     did = 'did:web:localhost%3A8443'
-#     url = resolver._WebDIDResolver__transform_to_url(did)
-#     assert url == "https://localhost:443/.well-known/did.json"
+def test_transformation_domain_with_port(resolver):
+    did = 'did:web:localhost%3A443'
+    url = resolver._WebDIDResolver__transform_to_url(did)
+    assert url == "https://localhost:443/.well-known/did.json"
