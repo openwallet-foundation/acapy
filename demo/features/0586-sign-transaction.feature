76ee7e6748f87db5599aa5c0d4db3ba0e3cc06fb
Ian Costanzo <ian@anon-solutions.ca>
2021-06-05 08:54:18 -0700

    Integration test for endorsement with credential issue and revocation w.i.p.
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- demo/features/0586-sign-transaction.feature
+++ demo/features/0586-sign-transaction.feature
@@ -47,3 +47,35 @@ Feature: RFC 0586 Aries sign (endorse) transactions functions
       Examples:
          | Acme_capabilities          | Bob_capabilities             | Schema_name    |
          | --mediation --multitenant  | --mediation --multitenant    | driverslicense |
+
+
+   @T002-RFC0586
+   Scenario Outline: endorse a schema and cred def transaction, write to the ledger, issue and revoke a credential
+      Given we have "2" agents
+         | name  | role     | capabilities        |
+         | Acme  | endorser | <Acme_capabilities> |
+         | Bob   | author   | <Bob_capabilities>  |
+      And "Acme" and "Bob" have an existing connection
+      When "Acme" has a DID with role "ENDORSER"
+      And "Bob" has a DID with role "AUTHOR"
+      And "Acme" connection has job role "TRANSACTION_ENDORSER"
+      And "Bob" connection has job role "TRANSACTION_AUTHOR"
+      And "Bob" connection sets endorser info
+      And "Bob" authors a schema transaction with <Schema_name>
+      And "Bob" requests endorsement for the transaction
+      And "Acme" endorses the transaction
+      And "Bob" can write the transaction to the ledger
+      And "Bob" has written the schema <Schema_name> to the ledger
+      And "Bob" authors a credential definition transaction with <Schema_name>
+      And "Bob" requests endorsement for the transaction
+      And "Acme" endorses the transaction
+      Then "Bob" can write the transaction to the ledger
+      And "Bob" has written the credential definition for <Schema_name> to the ledger
+
+      Examples:
+         | Acme_capabilities                       | Bob_capabilities             | Schema_name    |
+         | --revocation                            |                              | driverslicense |
+#         | --revocation --did-exchange             | --did-exchange               | driverslicense |
+#         | --revocation --mediation                | --mediation                  | driverslicense |
+#         | --revocation --multitenant              | --multitenant                | driverslicense |
+#         | --revocation --mediation --multitenant  | --mediation --multitenant    | driverslicense |
--- demo/features/steps/0586-sign-transaction.py
+++ demo/features/steps/0586-sign-transaction.py
@@ -129,11 +129,15 @@ def step_impl(context, agent_name):
 def step_impl(context, agent_name):
     agent = context.active_agents[agent_name]
 
+    # find the transaction with state "request_received"
     txns = {"results": []}
     i = 5
     while 0 == len(txns["results"]) and i > 0:
         async_sleep(1.0)
-        txns = agent_container_GET(agent["agent"], "/transactions")
+        txns_queued = agent_container_GET(agent["agent"], "/transactions")
+        for j in range(len(txns_queued["results"])):
+            if txns_queued["results"][j]["state"] == "request_received":
+                txns["results"].append(txns_queued["results"][j])
         i = i - 1
     requested_txn = txns["results"][0]
     assert requested_txn["state"] == "request_received"
@@ -146,15 +150,16 @@ def step_impl(context, agent_name):
     assert endorsed_txn["state"] == "transaction_endorsed"
 
 
+@when('"{agent_name}" can write the transaction to the ledger')
 @then('"{agent_name}" can write the transaction to the ledger')
 def step_impl(context, agent_name):
     agent = context.active_agents[agent_name]
+    txn_id = context.txn_ids["AUTHOR"]
 
     async_sleep(1.0)
-    txns = agent_container_GET(agent["agent"], "/transactions")
-    requested_txn = txns["results"][0]
+    txn = agent_container_GET(agent["agent"], "/transactions/" + txn_id)
+    requested_txn = txn
     assert requested_txn["state"] == "transaction_endorsed"
-    txn_id = requested_txn["transaction_id"]
 
     written_txn = agent_container_POST(
         agent["agent"], "/transactions/" + txn_id + "/write"
@@ -163,6 +168,7 @@ def step_impl(context, agent_name):
     assert written_txn["state"] == "transaction_acked"
 
 
+@when('"{agent_name}" has written the schema {schema_name} to the ledger')
 @then('"{agent_name}" has written the schema {schema_name} to the ledger')
 def step_impl(context, agent_name, schema_name):
     agent = context.active_agents[agent_name]
@@ -176,3 +182,47 @@ def step_impl(context, agent_name, schema_name):
     schema = agent_container_GET(agent["agent"], "/schemas/" + schema_id)
 
     # TODO assert goodness
+
+
+@when('"{agent_name}" authors a credential definition transaction with {schema_name}')
+@then('"{agent_name}" authors a credential definition transaction with {schema_name}')
+def step_impl(context, agent_name, schema_name):
+    agent = context.active_agents[agent_name]
+
+    connection_id = agent["agent"].agent.connection_id
+
+    # TODO for now assume there is a single schema; should find the schema based on the supplied name
+    schemas = agent_container_GET(agent["agent"], "/schemas/created")
+    assert len(schemas["schema_ids"]) == 1
+
+    schema_id = schemas["schema_ids"][0]
+    created_txn = agent_container_POST(
+        agent["agent"],
+        "/credential-definitions",
+        data={"schema_id": schema_id, "tag": "test_cred_def_with_endorsement"},
+        params={"conn_id": connection_id, "create_transaction_for_endorser": "true"},
+    )
+
+    # assert goodness
+    assert created_txn["txn"]["state"] == "transaction_created"
+    if not "txn_ids" in context:
+        context.txn_ids = {}
+    context.txn_ids["AUTHOR"] = created_txn["txn"]["transaction_id"]
+
+
+@when('"{agent_name}" has written the credential definition for {schema_name} to the ledger')
+@then('"{agent_name}" has written the credential definition for {schema_name} to the ledger')
+def step_impl(context, agent_name, schema_name):
+    agent = context.active_agents[agent_name]
+
+    schema_info = read_schema_data(schema_name)
+
+    cred_defs = agent_container_GET(agent["agent"], "/credential-definitions/created")
+    assert len(cred_defs["credential_definition_ids"]) == 1
+
+    cred_def_id = cred_defs["credential_definition_ids"][0]
+    cred_def = agent_container_GET(agent["agent"], "/credential-definitions/" + cred_def_id)
+
+    # TODO assert goodness
+
+
