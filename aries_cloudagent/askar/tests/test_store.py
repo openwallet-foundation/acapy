b900afb4ab6f746a8d7d5649fd92355df302c269
Matic Di Batista <matic@global.id>
2021-08-19 09:57:05 +0200

    test(IDENT-3101): updated tests with newline. moved repeated values into vars
    Signed-off-by: arin.hlaj <arin.hlaj@global.id>

--- aries_cloudagent/askar/tests/test_store.py
+++ aries_cloudagent/askar/tests/test_store.py
@@ -4,25 +4,29 @@ from asynctest import TestCase as AsyncTestCase
 from ..store import AskarStoreConfig
 
 class TestStoreConfig(AsyncTestCase):
+    key_derivation_method = "Raw"
+    key = "key"
+    storage_type = "postgres"
+
     async def test_init_success(self):
         config = {
-            "key_derivation_method": "RAW",
-            "key": "key",
-            "storage_type": "postgres"
+            "key_derivation_method": self.key_derivation_method,
+            "key": self.key,
+            "storage_type": self.storage_type
         }
 
         askar_store = AskarStoreConfig(
             config
         )
 
-        assert askar_store.key_derivation_method == "RAW"
-        assert askar_store.key == "key"
-        assert askar_store.storage_type == "postgres"
+        assert askar_store.key_derivation_method == self.key_derivation_method
+        assert askar_store.key == self.key
+        assert askar_store.storage_type == self.storage_type
 
     async def test_init_should_fail_when_key_missing(self):
         config = {
-            "key_derivation_method": "RAW",
-            "storage_type": "postgres"
+            "key_derivation_method": self.key_derivation_method,
+            "storage_type": self.storage_type
         }
 
         with self.assertRaises(ProfileError):
