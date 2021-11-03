365ecf68e0274612de27b6a6c19059bbd6b2b950
Daniel Bluhm <dbluhm@pm.me>
2021-11-03 19:11:37 -0400

    feat: use rev notification record in revoke event handler
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- aries_cloudagent/protocols/revocation_notification/v1_0/routes.py
+++ aries_cloudagent/protocols/revocation_notification/v1_0/routes.py
@@ -1,17 +1,15 @@
 """Routes for revocation notification."""
 import logging
 import re
+from typing import cast
 
-from ....messaging.responder import BaseResponder
 from ....core.event_bus import Event, EventBus
 from ....core.profile import Profile
-
+from ....messaging.responder import BaseResponder
 from ....revocation.models.issuer_cred_rev_record import IssuerCredRevRecord
-from ....revocation.util import (
-    ISSUER_REVOKE_EVENT,
-    REVOCATION_EVENT_PREFIX,
-)
-from .messages.revoke import Revoke
+from ....revocation.util import ISSUER_REVOKE_EVENT, REVOCATION_EVENT_PREFIX
+from ....storage.error import StorageError, StorageNotFoundError
+from .models.rev_notification_record import RevNotificationRecord
 
 LOGGER = logging.getLogger(__name__)
 
@@ -31,7 +29,21 @@ async def on_issuer_revoke_event(profile: Profile, event: Event):
     LOGGER.debug("Sending notification of revocation to recipient: %s", event.payload)
 
     cred_rev_rec = IssuerCredRevRecord.deserialize(event.payload)
-    notification = Revoke(thread_id=cred_rev_rec.thread_id)
-
-    responder = profile.inject(BaseResponder)
-    await responder.send(notification, connection_id=cred_rev_rec.connection_id)
+    cred_rev_rec = cast(IssuerCredRevRecord, cred_rev_rec)
+    try:
+        async with profile.session() as session:
+            rev_notify_rec = await RevNotificationRecord.query_by_ids(
+                session,
+                rev_reg_id=cred_rev_rec.rev_reg_id,
+                cred_rev_id=cred_rev_rec.cred_rev_id,
+            )
+        notification = rev_notify_rec.to_message()
+        responder = profile.inject(BaseResponder)
+        await responder.send(notification, connection_id=rev_notify_rec.connection_id)
+    except StorageNotFoundError:
+        LOGGER.info(
+            "No revocation notification record found for revoked credential; "
+            "no notification will be sent"
+        )
+    except StorageError:
+        LOGGER.exception("Failed to retrieve revocation notification record")
