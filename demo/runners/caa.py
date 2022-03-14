88f524e7400c2b1f0a00ea0e988b0f9ef55c8091
dc-andysign <andrei.baloiu@gmail.com>
2022-03-14 08:31:03 +0000

    Added the 1st ver of the caa.py script ( the main & the CaaAgent class definition )

---
+++ demo/runners/caa.py
@@ -0,0 +1,97 @@
+import asyncio
+# import json
+import logging
+import os
+import sys
+# import time
+
+# from aiohttp import ClientError
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from runners.agent_container import (  # noqa:E402
+    arg_parser,
+    create_agent_with_args,
+    AriesAgent,
+)
+
+CRED_PREVIEW_TYPE = "https://didcomm.org/issue-credential/2.0/credential-preview"
+SELF_ATTESTED = os.getenv("SELF_ATTESTED")
+TAILS_FILE_COUNT = int(os.getenv("TAILS_FILE_COUNT", 100))
+
+logging.basicConfig(level=logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
+
+class CaaAgent(AriesAgent):
+    def __init__(
+        self,
+        ident: str,
+        http_port: int,
+        admin_port: int,
+        no_auto: bool = False,
+        **kwargs,
+    ):
+        super().__init__(
+            ident,
+            http_port,
+            admin_port,
+            prefix="Caa",
+            no_auto=no_auto,
+            **kwargs,
+        )
+        self.connection_id = None
+        self._connection_ready = None
+        self.cred_state = {}
+        # TODO define a dict to hold credential attributes
+        # based on cred_def_id
+        self.cred_attrs = {}
+
+    async def detect_connection(self):
+        await self._connection_ready
+        self._connection_ready = None
+
+    @property
+    def connection_ready(self):
+        return self._connection_ready.done() and self._connection_ready.result()
+
+
+async def main(args):
+    print(200)
+
+if __name__ == "__main__":
+    parser = arg_parser(ident="caa", port=8050)
+    args = parser.parse_args()
+    # Namespace(aip=20, arg_file=None, cred_type='indy', did_exchange=False, mediation=False, multitenant=False, no_auto=False, port=8050, revocation=False, tails_server_base_url=None, timing=False, wallet_type=None)
+    ENABLE_PYDEVD_PYCHARM = os.getenv("ENABLE_PYDEVD_PYCHARM", "").lower()
+    ENABLE_PYDEVD_PYCHARM = ENABLE_PYDEVD_PYCHARM and ENABLE_PYDEVD_PYCHARM not in (
+        "false",
+        "0",
+    )
+    PYDEVD_PYCHARM_HOST = os.getenv("PYDEVD_PYCHARM_HOST", "localhost")
+    PYDEVD_PYCHARM_CONTROLLER_PORT = int(
+        os.getenv("PYDEVD_PYCHARM_CONTROLLER_PORT", 5001)
+    )
+
+    if ENABLE_PYDEVD_PYCHARM:
+        try:
+            import pydevd_pycharm
+
+            print(
+                "CAA remote debugging to "
+                f"{PYDEVD_PYCHARM_HOST}:{PYDEVD_PYCHARM_CONTROLLER_PORT}"
+            )
+            pydevd_pycharm.settrace(
+                host=PYDEVD_PYCHARM_HOST,
+                port=PYDEVD_PYCHARM_CONTROLLER_PORT,
+                stdoutToServer=True,
+                stderrToServer=True,
+                suspend=False,
+            )
+        except ImportError:
+            print("pydevd_pycharm library was not found")
+
+    try:
+        asyncio.get_event_loop().run_until_complete(main(args))
+    except KeyboardInterrupt:
+        os._exit(1)
