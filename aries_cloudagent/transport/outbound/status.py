42477bc63d5d475d5c222f266298ed3bb4298dc2
Micah Peltier <micah6_8@yahoo.com>
2022-03-25 10:48:13 -0600

    feat: added event topic accessor
    Signed-off-by: Micah Peltier <micah6_8@yahoo.com>

--- aries_cloudagent/transport/outbound/status.py
+++ aries_cloudagent/transport/outbound/status.py
@@ -2,6 +2,8 @@
 
 from enum import Enum
 
+OUTBOUND_STATUS_PREFIX = "acapy::outbound-message::"
+
 
 class OutboundSendStatus(Enum):
     """Send status of outbound messages."""
@@ -21,3 +23,7 @@ class OutboundSendStatus(Enum):
 
     # No endpoint available, and no internal queue for messages.
     UNDELIVERABLE = "undeliverable"
+
+    @property
+    def topic(self):
+        return f"{OUTBOUND_STATUS_PREFIX}{self.value}"
