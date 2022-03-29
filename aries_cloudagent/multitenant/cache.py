a2e306e9e41e10e2d95422da5aeec043a25f9ece
Daniel Bluhm <dbluhm@pm.me>
2022-03-28 20:38:19 -0400

    feat: profile cache track open profiles via weakref
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- aries_cloudagent/multitenant/cache.py
+++ aries_cloudagent/multitenant/cache.py
@@ -3,6 +3,7 @@
 import logging
 from collections import OrderedDict
 from typing import Optional
+from weakref import WeakValueDictionary
 
 from ..core.profile import Profile
 
@@ -21,10 +22,26 @@ class ProfileCache:
         """
 
         self.profiles: OrderedDict[str, Profile] = OrderedDict()
+        self._open_profiles: WeakValueDictionary[str, Profile] = WeakValueDictionary()
         self.capacity = capacity
+
+    def _cleanup(self):
+        """Prune cache until size matches defined capacity."""
+        if len(self.profiles) > self.capacity:
+            LOGGER.debug(
+                f"Profile limit of {self.capacity} reached."
+                " Evicting least recently used profiles..."
+            )
+            while len(self.profiles) > self.capacity:
+                key, _ = self.profiles.popitem(last=False)
+                LOGGER.debug(f"Evicted profile with key {key}")
 
     def get(self, key: str) -> Optional[Profile]:
         """Get profile with associated key from cache.
+
+        If a profile is open but has been evicted from the cache, this will
+        reinsert the profile back into the cache. This prevents attempting to
+        open a profile that is already open. Triggers clean up.
 
         Args:
             key (str): the key to get the profile for.
@@ -33,11 +50,19 @@ class ProfileCache:
             Optional[Profile]: Profile if found in cache.
 
         """
-        if key not in self.profiles:
+        if key not in self._open_profiles:
             return None
         else:
+            value = self._open_profiles[key]
+            if key not in self.profiles:
+                LOGGER.debug(
+                    f"Rescuing profile {key} from eviction from cache; profile "
+                    "will be reinserted into cache"
+                )
+                self.profiles[key] = value
             self.profiles.move_to_end(key)
-            return self.profiles[key]
+            self._cleanup()
+            return value
 
     def has(self, key: str) -> bool:
         """Check whether there is a profile with associated key in the cache.
@@ -62,16 +87,11 @@ class ProfileCache:
             value (Profile): the profile to set
         """
         value.finalizer()
+        self._open_profiles[key] = value
         self.profiles[key] = value
+        LOGGER.debug(f"Setting profile with id {key} in profile cache")
         self.profiles.move_to_end(key)
-        LOGGER.debug(f"setting profile with id {key} in profile cache")
-
-        if len(self.profiles) > self.capacity:
-            LOGGER.debug(
-                f"Profile limit of {self.capacity} reached."
-                " Evicting least recently used profile..."
-            )
-            self.profiles.popitem(last=False)
+        self._cleanup()
 
     def remove(self, key: str):
         """Remove profile with associated key from the cache.
