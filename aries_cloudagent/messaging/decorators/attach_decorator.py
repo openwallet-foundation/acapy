9ce96be741540ff259ad28c848b6b3308fca74e5
Shaanjot Gill <shaangill025@users.noreply.github.com>
2022-04-06 11:59:07 -0700

    update attach_decorator verify method
    Signed-off-by: Shaanjot Gill <shaangill025@users.noreply.github.com>

--- aries_cloudagent/messaging/decorators/attach_decorator.py
+++ aries_cloudagent/messaging/decorators/attach_decorator.py
@@ -428,7 +428,7 @@ class AttachDecoratorData(BaseModel):
         assert self.jws
 
         b64_payload = unpad(set_urlsafe_b64(self.base64, True))
-        verkey_to_check = None
+        verkey_to_check = []
         for sig in [self.jws] if self.signatures == 1 else self.jws.signatures:
             b64_protected = sig.protected
             b64_sig = sig.signature
@@ -439,7 +439,7 @@ class AttachDecoratorData(BaseModel):
             b_sig = b64_to_bytes(b64_sig, urlsafe=True)
             verkey = bytes_to_b58(b64_to_bytes(protected["jwk"]["x"], urlsafe=True))
             encoded_pk = DIDKey.from_did(protected["jwk"]["kid"]).public_key_b58
-            verkey_to_check = encoded_pk
+            verkey_to_check.append(encoded_pk)
             if not await wallet.verify_message(
                 sign_input, b_sig, verkey, KeyType.ED25519
             ):
@@ -448,7 +448,7 @@ class AttachDecoratorData(BaseModel):
                 sign_input, b_sig, encoded_pk, KeyType.ED25519
             ):
                 return False
-        if signer_verkey and signer_verkey != verkey_to_check:
+        if signer_verkey and signer_verkey not in verkey_to_check:
             return False
         return True
 
