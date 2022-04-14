94b0c3e6f797d5b471bab580e7434dfa12fc6f4d
Daniel Bluhm <dbluhm@pm.me>
2022-04-14 15:49:22 -0400

    test: rescuing still open profile from eviction
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- aries_cloudagent/multitenant/tests/test_cache.py
+++ aries_cloudagent/multitenant/tests/test_cache.py
@@ -83,3 +83,28 @@ def test_cleanup_lru():
     assert cache.get("2") is None
     assert cache.get("3")
     assert cache.get("4")
+
+
+def test_rescue_open_profile():
+    cache = ProfileCache(3)
+
+    cache.put("1", MockProfile())
+    cache.put("2", MockProfile())
+    cache.put("3", MockProfile())
+
+    assert len(cache.profiles) == 3
+
+    held = cache.profiles["1"]
+    cache.put("4", MockProfile())
+
+    assert len(cache.profiles) == 4
+    assert len(cache._cache) == 3
+
+    cache.get("1")
+
+    assert len(cache.profiles) == 3
+    assert len(cache._cache) == 3
+    assert cache.get("1")
+    assert cache.get("2") is None
+    assert cache.get("3")
+    assert cache.get("4")
