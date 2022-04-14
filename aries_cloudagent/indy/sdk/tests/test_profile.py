108ecabc0e1fa309417154411f069d90f3d27e4c
Daniel Bluhm <dbluhm@pm.me>
2022-04-14 15:30:20 -0400

    refactor: indy test profile by pytest convention
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- aries_cloudagent/indy/sdk/tests/test_profile.py
+++ aries_cloudagent/indy/sdk/tests/test_profile.py
@@ -25,65 +25,65 @@ async def profile():
     )
 
 
-class TestIndySdkProfile:
-    @pytest.mark.asyncio
-    async def test_properties(self, profile):
-        assert profile.name == "test-profile"
-        assert profile.backend == "indy"
-        assert profile.wallet and profile.wallet.handle == 1
+@pytest.mark.asyncio
+async def test_properties(profile):
+    assert profile.name == "test-profile"
+    assert profile.backend == "indy"
+    assert profile.wallet and profile.wallet.handle == 1
 
-        assert "IndySdkProfile" in str(profile)
-        assert profile.created
-        assert profile.wallet.created
-        assert profile.wallet.master_secret_id == "master-secret"
+    assert "IndySdkProfile" in str(profile)
+    assert profile.created
+    assert profile.wallet.created
+    assert profile.wallet.master_secret_id == "master-secret"
 
-        with async_mock.patch.object(profile, "opened", False):
-            with pytest.raises(ProfileError):
-                await profile.remove()
-
-        with async_mock.patch.object(
-            profile.opened, "close", async_mock.CoroutineMock()
-        ):
+    with async_mock.patch.object(profile, "opened", False):
+        with pytest.raises(ProfileError):
             await profile.remove()
-            assert profile.opened is None
 
-    def test_settings_genesis_transactions(self):
-        context = InjectionContext(
-            settings={"ledger.genesis_transactions": async_mock.MagicMock()}
-        )
-        context.injector.bind_instance(IndySdkLedgerPool, IndySdkLedgerPool("name"))
-        profile = IndySdkProfile(
-            IndyOpenWallet(
-                config=IndyWalletConfig({"name": "test-profile"}),
-                created=True,
-                handle=1,
-                master_secret_id="master-secret",
-            ),
-            context,
-        )
+    with async_mock.patch.object(profile.opened, "close", async_mock.CoroutineMock()):
+        await profile.remove()
+        assert profile.opened is None
 
-    def test_settings_ledger_config(self):
-        context = InjectionContext(settings={"ledger.ledger_config_list": True})
-        context.injector.bind_instance(IndySdkLedgerPool, IndySdkLedgerPool("name"))
-        profile = IndySdkProfile(
-            IndyOpenWallet(
-                config=IndyWalletConfig({"name": "test-profile"}),
-                created=True,
-                handle=1,
-                master_secret_id="master-secret",
-            ),
-            context,
-        )
 
-    def test_read_only(self):
-        context = InjectionContext(settings={"ledger.read_only": True})
-        context.injector.bind_instance(IndySdkLedgerPool, IndySdkLedgerPool("name"))
-        ro_profile = IndySdkProfile(
-            IndyOpenWallet(
-                config=IndyWalletConfig({"name": "test-profile"}),
-                created=True,
-                handle=1,
-                master_secret_id="master-secret",
-            ),
-            context,
-        )
+def test_settings_genesis_transactions():
+    context = InjectionContext(
+        settings={"ledger.genesis_transactions": async_mock.MagicMock()}
+    )
+    context.injector.bind_instance(IndySdkLedgerPool, IndySdkLedgerPool("name"))
+    profile = IndySdkProfile(
+        IndyOpenWallet(
+            config=IndyWalletConfig({"name": "test-profile"}),
+            created=True,
+            handle=1,
+            master_secret_id="master-secret",
+        ),
+        context,
+    )
+
+
+def test_settings_ledger_config():
+    context = InjectionContext(settings={"ledger.ledger_config_list": True})
+    context.injector.bind_instance(IndySdkLedgerPool, IndySdkLedgerPool("name"))
+    profile = IndySdkProfile(
+        IndyOpenWallet(
+            config=IndyWalletConfig({"name": "test-profile"}),
+            created=True,
+            handle=1,
+            master_secret_id="master-secret",
+        ),
+        context,
+    )
+
+
+def test_read_only():
+    context = InjectionContext(settings={"ledger.read_only": True})
+    context.injector.bind_instance(IndySdkLedgerPool, IndySdkLedgerPool("name"))
+    ro_profile = IndySdkProfile(
+        IndyOpenWallet(
+            config=IndyWalletConfig({"name": "test-profile"}),
+            created=True,
+            handle=1,
+            master_secret_id="master-secret",
+        ),
+        context,
+    )
