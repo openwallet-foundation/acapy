36e38372516c109b46c730a4e162e2cb7fa9c0cd
Daniel Bluhm <dbluhm@pm.me>
2022-06-08 21:57:11 -0400

    feat: add route managers
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

---
+++ aries_cloudagent/multitenant/route_manager.py
@@ -0,0 +1,100 @@
+"""Multitenancy route manager."""
+
+
+import logging
+from typing import List, Optional, Tuple
+from aries_cloudagent.core.profile import Profile
+from aries_cloudagent.messaging.responder import BaseResponder
+
+from aries_cloudagent.protocols.coordinate_mediation.v1_0.manager import (
+    MediationManager,
+)
+from aries_cloudagent.protocols.routing.v1_0.manager import RoutingManager
+from aries_cloudagent.protocols.routing.v1_0.models.route_record import RouteRecord
+from aries_cloudagent.storage.error import StorageNotFoundError
+
+from ..protocols.coordinate_mediation.v1_0.models.mediation_record import (
+    MediationRecord,
+)
+from ..protocols.coordinate_mediation.v1_0.route_manager import RouteManager
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class MultitenantRouteManager(RouteManager):
+    """Multitenancy route manager."""
+
+    def __init__(self, root_profile: Profile, sub_profile: Profile, wallet_id: str):
+        self.root_profile = root_profile
+        self.wallet_id = wallet_id
+        super().__init__(sub_profile)
+
+    @property
+    def sub_profile(self) -> Profile:
+        return self.profile
+
+    async def get_base_wallet_mediator(self) -> Optional[MediationRecord]:
+        return await MediationManager(self.root_profile).get_default_mediator()
+
+    async def _route_for_key(
+        self,
+        recipient_key: str,
+        mediation_record: Optional[MediationRecord] = None,
+        *,
+        skip_if_exists: bool = False,
+        replace_key: Optional[str] = None,
+    ):
+        LOGGER.info(
+            f"Add route record for recipient {recipient_key} to wallet {self.wallet_id}"
+        )
+        routing_mgr = RoutingManager(self.root_profile)
+        mediation_mgr = MediationManager(self.root_profile)
+        # Passed in mediation_record is ignored altogether.
+        # Only the base mediator needs updates.
+        mediation_record = await self.get_base_wallet_mediator()
+
+        if skip_if_exists:
+            try:
+                async with self.root_profile.session() as session:
+                    await RouteRecord.retrieve_by_recipient_key(session, recipient_key)
+
+                # If no error is thrown, it means there is already a record
+                return None
+            except (StorageNotFoundError):
+                pass
+
+        await routing_mgr.create_route_record(
+            recipient_key=recipient_key, internal_wallet_id=self.wallet_id
+        )
+
+        # External mediation
+        keylist_updates = None
+        if mediation_record:
+            keylist_updates = await mediation_mgr.add_key(recipient_key)
+            if replace_key:
+                keylist_updates = await mediation_mgr.remove_key(replace_key)
+
+            responder = self.root_profile.inject(BaseResponder)
+            await responder.send(
+                keylist_updates, connection_id=mediation_record.connection_id
+            )
+
+        return keylist_updates
+
+    async def routing_info(
+        self, my_endpoint: str, mediation_record: Optional[MediationRecord] = None
+    ) -> Tuple[List[str], str]:
+        routing_keys = []
+
+        base_mediation_record = await self.get_base_wallet_mediator()
+
+        if base_mediation_record:
+            routing_keys = base_mediation_record.routing_keys
+            my_endpoint = base_mediation_record.endpoint
+
+        if mediation_record:
+            routing_keys = [*routing_keys, *mediation_record.routing_keys]
+            my_endpoint = mediation_record.endpoint
+
+        return routing_keys, my_endpoint
---
+++ aries_cloudagent/protocols/coordinate_mediation/v1_0/route_manager.py
@@ -0,0 +1,199 @@
+"""Route manager.
+
+Set up routing for newly formed connections.
+"""
+
+
+from abc import ABC, abstractmethod
+from typing import List, Optional, Tuple
+
+from aries_cloudagent.protocols.routing.v1_0.models.route_record import RouteRecord
+from aries_cloudagent.storage.error import StorageNotFoundError
+
+from ....connections.models.conn_record import ConnRecord
+from ....core.profile import Profile
+from ....messaging.responder import BaseResponder
+from ....wallet.base import BaseWallet
+from ....wallet.did_info import DIDInfo
+from ....wallet.did_method import DIDMethod
+from ....wallet.key_type import KeyType
+from .manager import MediationManager
+from .models.mediation_record import MediationRecord
+
+
+class RouteManagerError(Exception):
+    """Raised on error from route manager."""
+
+
+class RouteManager(ABC):
+    """Base Route Manager."""
+
+    def __init__(self, profile: Profile):
+        self.profile = profile
+
+    async def get_or_create_my_did(self, conn_record: ConnRecord) -> DIDInfo:
+        if not conn_record.my_did:
+            async with self.profile.session() as session:
+                wallet = session.inject(BaseWallet)
+                # Create new DID for connection
+                my_info = await wallet.create_local_did(DIDMethod.SOV, KeyType.ED25519)
+                conn_record.my_did = my_info.did
+                await conn_record.save(session, reason="Connection my did created")
+        else:
+            async with self.profile.session() as session:
+                wallet = session.inject(BaseWallet)
+                my_info = await wallet.get_local_did(conn_record.my_did)
+
+        return my_info
+
+    async def mediation_record_if_id(
+        self, mediation_id: Optional[str] = None, or_default: bool = False
+    ):
+        """Validate mediation and return record.
+
+        If mediation_id is not None,
+        validate mediation record state and return record
+        else, return None
+        """
+        mediation_record = None
+        if mediation_id:
+            async with self.profile.session() as session:
+                mediation_record = await MediationRecord.retrieve_by_id(
+                    session, mediation_id
+                )
+        elif or_default:
+            mediation_record = await MediationManager(
+                self.profile
+            ).get_default_mediator()
+
+        if mediation_record:
+            if mediation_record.state != MediationRecord.STATE_GRANTED:
+                raise RouteManagerError(
+                    "Mediation is not granted for mediation identified by "
+                    f"{mediation_record.mediation_id}"
+                )
+        return mediation_record
+
+    @abstractmethod
+    async def _route_for_key(
+        self,
+        recipient_key: str,
+        mediation_record: Optional[MediationRecord] = None,
+        *,
+        skip_if_exists: bool = False,
+        replace_key: Optional[str] = None,
+    ):
+        """Route a key."""
+
+    async def route_connection_as_invitee(
+        self,
+        conn_record: ConnRecord,
+        mediation_record: Optional[MediationRecord] = None,
+    ):
+        """Set up routing for a new connection when we are the invitee."""
+        my_info = await self.get_or_create_my_did(conn_record)
+        return await self._route_for_key(
+            my_info.verkey, mediation_record, skip_if_exists=True
+        )
+
+    async def route_connection_as_inviter(
+        self,
+        conn_record: ConnRecord,
+        mediation_record: Optional[MediationRecord] = None,
+    ):
+        """Set up routing for a new connection when we are the inviter."""
+        my_info = await self.get_or_create_my_did(conn_record)
+        return await self._route_for_key(
+            my_info.verkey,
+            mediation_record,
+            replace_key=conn_record.invitation_key,
+            skip_if_exists=True,
+        )
+
+    async def route_invitation(
+        self,
+        conn_record: ConnRecord,
+        mediation_record: Optional[MediationRecord] = None,
+    ):
+        """Set up routing for receiving a response to an invitation."""
+        if mediation_record:
+            # Save that this invitation was created with mediation
+            async with self.profile.session() as session:
+                await conn_record.metadata_set(
+                    session,
+                    MediationManager.METADATA_KEY,
+                    {MediationManager.METADATA_ID: mediation_record.mediation_id},
+                )
+
+        if conn_record.invitation_key:
+            return await self._route_for_key(
+                conn_record.invitation_key, mediation_record, skip_if_exists=True
+            )
+
+        raise ValueError("Expected connection to have invitation_key")
+
+    async def route_public_did(self, verkey: str):
+        """Establish routing for a public DID."""
+        return await self._route_for_key(verkey, skip_if_exists=True)
+
+    async def route_static(
+        self,
+        conn_record: ConnRecord,
+        mediation_record: Optional[MediationRecord] = None,
+    ):
+        my_info = await self.get_or_create_my_did(conn_record)
+        return await self._route_for_key(
+            my_info.verkey, mediation_record, skip_if_exists=True
+        )
+
+    @abstractmethod
+    async def routing_info(
+        self,
+        my_endpoint: str,
+        mediation_record: Optional[MediationRecord] = None,
+    ) -> Tuple[List[str], str]:
+        """Retrieve routing keys."""
+
+
+class CoordinateMediationV1RouteManager(RouteManager):
+    """Manage routes using Coordinate Mediation protocol."""
+
+    async def _route_for_key(
+        self,
+        recipient_key: str,
+        mediation_record: Optional[MediationRecord] = None,
+        *,
+        skip_if_exists: bool = False,
+        replace_key: Optional[str] = None,
+    ):
+        if not mediation_record:
+            return None
+
+        if skip_if_exists:
+            try:
+                async with self.profile.session() as session:
+                    await RouteRecord.retrieve_by_recipient_key(session, recipient_key)
+
+                return None
+            except StorageNotFoundError:
+                pass
+
+        # Keylist update is idempotent, skip_if_exists ignored
+        mediation_mgr = MediationManager(self.profile)
+        keylist_update = await mediation_mgr.add_key(recipient_key)
+        if replace_key:
+            keylist_update = await mediation_mgr.remove_key(replace_key)
+
+        responder = self.profile.inject(BaseResponder)
+        await responder.send(
+            keylist_update, connection_id=mediation_record.connection_id
+        )
+        return keylist_update
+
+    async def routing_info(
+        self, my_endpoint: str, mediation_record: Optional[MediationRecord] = None
+    ) -> Tuple[List[str], str]:
+        if mediation_record:
+            return mediation_record.routing_keys, mediation_record.endpoint
+
+        return [], my_endpoint
