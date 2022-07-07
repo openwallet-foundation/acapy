1b3617f43d176bd4212afc7a56238fc66695b2bb
Daniel Bluhm <dbluhm@pm.me>
2022-07-07 14:39:29 -0500

    chore: relax version restriction a bit
    to accommodate von-image py37 version
    
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
             "bbs": parse_requirements("requirements.bbs.txt"),
             "uvloop": {"uvloop": "^=0.14.0"},
         },
-        python_requires=">=3.7.13",
+        python_requires=">=3.7.10",
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: Apache Software License",
