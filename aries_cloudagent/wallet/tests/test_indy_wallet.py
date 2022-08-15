c3901414221ef90102140325d9d9b7294547915f
Char Howland <char@indicio.tech>
2022-08-15 17:02:24 -0600

    test: set_did_endpoint_ledger with routing_keys
    Signed-off-by: Char Howland <char@indicio.tech>

--- aries_cloudagent/wallet/tests/test_indy_wallet.py
+++ aries_cloudagent/wallet/tests/test_indy_wallet.py
@@ -130,6 +130,7 @@ class TestIndySdkWallet(test_in_memory_wallet.TestInMemoryWallet):
             EndpointType.ENDPOINT,
             endorser_did=None,
             write_ledger=True,
+            routing_keys=None,
         )
         info_pub2 = await wallet.get_public_did()
         assert info_pub2.metadata["endpoint"] == "http://1.2.3.4:8021"
@@ -137,6 +138,27 @@ class TestIndySdkWallet(test_in_memory_wallet.TestInMemoryWallet):
         with pytest.raises(test_module.LedgerConfigError) as excinfo:
             await wallet.set_did_endpoint(info_pub.did, "http://1.2.3.4:8021", None)
         assert "No ledger available" in str(excinfo.value)
+
+    @pytest.mark.asyncio
+    async def test_set_did_endpoint_ledger_with_routing_keys(self, wallet: IndySdkWallet):
+        routing_keys = ['3YJCx3TqotDWFGv7JMR5erEvrmgu5y4FDqjR7sKWxgXn']
+        mock_ledger = async_mock.MagicMock(
+            read_only=False, update_endpoint_for_did=async_mock.CoroutineMock()
+        )
+        info_pub = await wallet.create_public_did(
+            DIDMethod.SOV,
+            KeyType.ED25519,
+        )
+        await wallet.set_did_endpoint(info_pub.did, "http://1.2.3.4:8021", mock_ledger, routing_keys=routing_keys)
+
+        mock_ledger.update_endpoint_for_did.assert_called_once_with(
+            info_pub.did,
+            "http://1.2.3.4:8021",
+            EndpointType.ENDPOINT,
+            endorser_did=None,
+            write_ledger=True,
+            routing_keys=routing_keys,
+        )
 
     @pytest.mark.asyncio
     async def test_set_did_endpoint_readonly_ledger(self, wallet: IndySdkWallet):
