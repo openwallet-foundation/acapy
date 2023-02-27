2e68381e798c3fb768f19e6de2aa7284c42e5cd6
Adam Burdett <burdettadam@gmail.com>
2023-02-27 15:55:49 -0700

    empty methods for anoncreds admin routes
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- aries_cloudagent/anoncreds/anoncreds/routes.py
+++ aries_cloudagent/anoncreds/anoncreds/routes.py
@@ -2,23 +2,76 @@
 # import json
 
 from aiohttp import web
+
 # from aiohttp_apispec import (docs, match_info_schema, querystring_schema,
 #                             request_schema, response_schema)
 
 SPEC_URI = ""
 
 
+async def schemas_post(request: web.BaseRequest):
+    raise NotImplementedError()
+
+
+async def schema_get(request: web.BaseRequest):
+    raise NotImplementedError()
+
+
+async def schemas_get(request: web.BaseRequest):
+    raise NotImplementedError()
+
+
+async def schemas_get_created(request: web.BaseRequest):
+    raise NotImplementedError()
+
+
+async def cred_def_post(request: web.BaseRequest):
+    raise NotImplementedError()
+
+
+async def cred_def_get(request: web.BaseRequest):
+    raise NotImplementedError()
+
+
+async def cred_defs_get(request: web.BaseRequest):
+    raise NotImplementedError()
+
+
 async def register(app: web.Application):
     """Register routes."""
 
-    app.add_routes([
-        web.post("/anoncreds/schemas",  , allow_head=False),
-        web.get("/anoncreds/schemas/{schema_id}", , allow_head=False),
-        web.get("/anoncreds/schemas/created", ,allow_head=False),
-        web.post("/anoncreds/credential-definitions", ,allow_head=False),
-        web.get("/anoncreds/credential-definitions/{credential_definition_id}", ,allow_head=False),
-        web.get("/anoncreds/credential-definitions/created", ,allow_head=False),
-    ])
+    app.add_routes(
+        [
+            web.post("/anoncreds/schema", schemas_post, allow_head=False),
+            web.get("/anoncreds/schema/{schema_id}", schema_get, allow_head=False),
+            web.get(
+                "/anoncreds/schemas/issuer/{issuer_id}", schemas_get, allow_head=False
+            ),
+            web.get(
+                "/anoncreds/schemas/did-method/{did_method}",
+                schemas_get,
+                allow_head=False,
+            ),
+            web.post(
+                "/anoncreds/credential-definition", cred_def_post, allow_head=False
+            ),
+            web.get(
+                "/anoncreds/credential-definition/{credential_definition_id}",
+                cred_def_get,
+                allow_head=False,
+            ),
+            web.get(
+                "/anoncreds/credential-definitions/issuer/{did_method}",
+                cred_defs_get,
+                allow_head=False,
+            ),
+            web.get(
+                "/anoncreds/credential-definitions/did-method/{did_method}",
+                cred_defs_get,
+                allow_head=False,
+            ),
+        ]
+    )
 
 
 def post_process_routes(app: web.Application):
