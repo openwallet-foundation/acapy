ed47a332cfe3b4baff2852f86fa96ab352ee2474
Jason Sherman <tools@usingtechnolo.gy>
2023-05-18 10:38:50 -0700

    Add updated ELK stack for demos.
    Remove outdated EFK stack and update documentation.
    
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>

--- demo/multi-demo/ngrok-wait.sh
+++ demo/multi-demo/ngrok-wait.sh
@@ -2,21 +2,24 @@
 
 # based on code developed by Sovrin:  https://github.com/hyperledger/aries-acapy-plugin-toolbox
 
-echo "using ngrok end point [$NGROK_NAME]"
+if [[ "${ACAPY_AGENT_ACCESS}" == "public" ]]; then
+    echo "using ngrok end point [$NGROK_NAME]"
 
-NGROK_ENDPOINT=null
-while [ -z "$NGROK_ENDPOINT" ] || [ "$NGROK_ENDPOINT" = "null" ]
-do
-    echo "Fetching end point from ngrok service"
-    NGROK_ENDPOINT=$(curl --silent $NGROK_NAME:4040/api/tunnels | ./jq -r '.tunnels[] | select(.proto=="https") | .public_url')
+    NGROK_ENDPOINT=null
+    while [ -z "$NGROK_ENDPOINT" ] || [ "$NGROK_ENDPOINT" = "null" ]
+    do
+        echo "Fetching end point from ngrok service"
+        NGROK_ENDPOINT=$(curl --silent $NGROK_NAME:4040/api/tunnels | ./jq -r '.tunnels[] | select(.proto=="https") | .public_url')
 
-    if [ -z "$NGROK_ENDPOINT" ] || [ "$NGROK_ENDPOINT" = "null" ]; then
-        echo "ngrok not ready, sleeping 5 seconds...."
-        sleep 5
-    fi
-done
+        if [ -z "$NGROK_ENDPOINT" ] || [ "$NGROK_ENDPOINT" = "null" ]; then
+            echo "ngrok not ready, sleeping 5 seconds...."
+            sleep 5
+        fi
+    done
 
-export ACAPY_ENDPOINT=$NGROK_ENDPOINT
+    export ACAPY_ENDPOINT=$NGROK_ENDPOINT
+fi
+
 
 echo "Starting aca-py agent with endpoint [$ACAPY_ENDPOINT]"
 
@@ -36,12 +39,11 @@ exec aca-py start \
     --wallet-name "test_multi" \
     --wallet-key "secret_key" \
     --wallet-storage-type "postgres_storage" \
-    --wallet-storage-config "{\"url\":\"wallet-db:5432\",\"max_connections\":5,\"scheme\":\"MultiWalletSingleTable\"}" \
+    --wallet-storage-config "{\"url\":\"wallet-db:5432\",\"max_connections\":20,\"scheme\":\"DatabasePerWallet\"}" \
     --wallet-storage-creds "{\"account\":\"DB_USER\",\"password\":\"DB_PASSWORD\",\"admin_account\":\"DB_USER\",\"admin_password\":\"DB_PASSWORD\"}" \
     --admin '0.0.0.0' 8010 \
     --label "test_multi" \
     --admin-insecure-mode \
     --multitenant \
     --multitenant-admin \
-    --jwt-secret "very_secret_secret" \
-    --log-level "error" 
+    --jwt-secret "very_secret_secret"
