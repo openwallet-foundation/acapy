687355905fe18ee1d115bfc654299d0b15aa0e15
Adam Burdett <burdettadam@gmail.com>
2023-07-07 11:10:52 -0600

    feat: more passing test for jwt
    Signed-off-by: Adam Burdett <burdettadam@gmail.com>

--- aries_cloudagent/wallet/tests/test_jwt.py
+++ aries_cloudagent/wallet/tests/test_jwt.py
@@ -1,4 +1,4 @@
-from unittest import TestCase as AsyncTestCase
+from unittest import TestCase
 import pytest
 from aries_cloudagent.resolver.did_resolver import DIDResolver
 from aries_cloudagent.resolver.tests.test_did_resolver import MockResolver
@@ -13,7 +13,7 @@ from ...core.in_memory.profile import InMemoryProfile
 from ...wallet.did_method import KEY, DIDMethods
 from ...wallet.in_memory import InMemoryWallet
 
-from ..jwt import jwt_sign, jwt_verify  # , resolve_public_key_by_kid_for_verify
+from ..jwt import jwt_sign, jwt_verify, resolve_public_key_by_kid_for_verify
 
 
 @pytest.fixture()
@@ -76,63 +76,80 @@ class TestJWT:
     """Tests for JWT sign and verify using dids."""
 
     seed = "testseed000000000000000000000001"
-    did = "55GkHamhTU1ZbTbV2ab9DE"
-    verkey = "3Dn1SJNPaCXcvvJvSbsFWP2xaCjMom3can8CQNhWrTRx"
 
     @pytest.mark.asyncio
-    async def test_sign_with_did_key(self, profile, in_memory_wallet):
+    async def test_sign_with_did_key_and_verify(self, profile, in_memory_wallet):
         did_info = await in_memory_wallet.create_local_did(KEY, ED25519, self.seed)
-        self.did = did_info.did
+        did = did_info.did
         verification_method = None
 
         headers = {}
         payload = {}
-        signed = await jwt_sign(
-            profile, headers, payload, self.did, verification_method
+        signed = await jwt_sign(profile, headers, payload, did, verification_method)
+
+        assert signed
+
+        assert await jwt_verify(profile, signed)
+
+    @pytest.mark.asyncio
+    async def test_sign_with_verification_method_and_verify(
+        self, profile, in_memory_wallet
+    ):
+        did_info = await in_memory_wallet.create_local_did(KEY, ED25519, self.seed)
+        did = None
+        verification_method = "did:key:z6Mkgg342Ycpuk263R9d8Aq6MUaxPn1DDeHyGo38EefXmgDL#z6Mkgg342Ycpuk263R9d8Aq6MUaxPn1DDeHyGo38EefXmgDL"
+        headers = {}
+        payload = {}
+        signed: str = await jwt_sign(
+            profile, headers, payload, did, verification_method
         )
 
         assert signed
 
         assert await jwt_verify(profile, signed)
 
+    @pytest.mark.asyncio
+    async def test_sign_x_invalid_did(self, profile):
+        did = "did:key:zzzzgg342Ycpuk263R9d8Aq6MUaxPn1DDeHyGo38EefXmgDL"
+        headers = {}
+        payload = {}
+        verification_method = None
+        with pytest.raises(Exception) as e_info:
+            await jwt_sign(profile, headers, payload, did, verification_method)
+        assert "No key type for prefixed public key" in str(e_info)
 
-"""
-    async def test_sign_x_invalid_secret_key_bytes(self):
-        mock_profile = InMemoryProfile.test_profile()
-        with self.assertRaises(Exception) as context:
-            jwt_sign(SIGN_MESSAGES, "hello")
-        assert "Unable to sign messages" in str(context.exception)
+    @pytest.mark.asyncio
+    async def test_sign_x_invalid_verification_method(self, profile):
+        did = None
+        headers = {}
+        payload = {}
+        verification_method = "did:key:zzzzgg342Ycpuk263R9d8Aq6MUaxPn1DDeHyGo38EefXmgDL#z6Mkgg342Ycpuk263R9d8Aq6MUaxPn1DDeHyGo38EefXmgDL"
+        with pytest.raises(Exception) as e_info:
+            await jwt_sign(profile, headers, payload, did, verification_method)
+        assert "DID not found:" in str(e_info)
 
-    async def test_verify(self):
-        mock_profile = InMemoryProfile.test_profile()
-        assert jwt_verify(
-            SIGN_MESSAGES, SIGNED_BYTES, PUBLIC_KEY_BYTES
-        )
+    @pytest.mark.asyncio
+    async def test_verify_x_invalid_signed(self, profile, in_memory_wallet):
+        did_info = await in_memory_wallet.create_local_did(KEY, ED25519, self.seed)
+        did = did_info.did
+        verification_method = None
 
-    async def test_verify_x_invalid_pk(self):
-        mock_profile = InMemoryProfile.test_profile()
-        with self.assertRaises(Exception):
-            jwt_verify(
-                SIGN_MESSAGES, SIGNED_BYTES, PUBLIC_KEY_BYTES + b"10"
-            )
+        headers = {}
+        payload = {}
+        signed = await jwt_sign(profile, headers, payload, did, verification_method)
 
-    async def test_verify_x_invalid_messages(self):
-        mock_profile = InMemoryProfile.test_profile()
-        with self.assertRaises(Exception):
-            jwt_verify(
-                SIGN_MESSAGES, SIGNED_BYTES, PUBLIC_KEY_BYTES + b"10"
-            )
-        assert not jwt_verify(
-            [SIGN_MESSAGES[0]], SIGNED_BYTES, PUBLIC_KEY_BYTES
-        )
+        assert signed
+        signed = f"{signed[:-2]}2"
 
-    async def test_verify_x_invalid_signed_bytes(self):
-        mock_profile = InMemoryProfile.test_profile()
-        with self.assertRaises(Exception):
-            assert not jwt_verify(
-                SIGN_MESSAGES, SIGNED_BYTES + b"10", PUBLIC_KEY_BYTES
-            )
+        with pytest.raises(Exception) as e_info:
+            await jwt_verify(profile, signed)
 
-    async def test_resolve_public_key_by_kid_for_verify(self):
-        mock_profile = InMemoryProfile.test_profile()
-"""
+    @pytest.mark.asyncio
+    async def test_resolve_public_key_by_kid_for_verify(
+        self, profile, in_memory_wallet
+    ):
+        await in_memory_wallet.create_local_did(KEY, ED25519, self.seed)
+        kid = "did:key:z6Mkgg342Ycpuk263R9d8Aq6MUaxPn1DDeHyGo38EefXmgDL#z6Mkgg342Ycpuk263R9d8Aq6MUaxPn1DDeHyGo38EefXmgDL"
+        key_material = await resolve_public_key_by_kid_for_verify(profile, kid)
+
+        assert key_material == "3Dn1SJNPaCXcvvJvSbsFWP2xaCjMom3can8CQNhWrTRx"
