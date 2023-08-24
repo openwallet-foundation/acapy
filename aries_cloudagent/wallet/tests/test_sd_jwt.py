30c6e5c1957343fe96ed1f53ab5496833e31fb34
Char Howland <char@indicio.tech>
2023-08-24 12:34:40 -0600

    test: fixture for payload
    Signed-off-by: Char Howland <char@indicio.tech>

--- aries_cloudagent/wallet/tests/test_sd_jwt.py
+++ aries_cloudagent/wallet/tests/test_sd_jwt.py
@@ -1,53 +1,35 @@
-from unittest import TestCase
 import pytest
-from aries_cloudagent.resolver.did_resolver import DIDResolver
-from aries_cloudagent.resolver.tests.test_did_resolver import MockResolver
-from aries_cloudagent.wallet.default_verification_key_strategy import (
-    BaseVerificationKeyStrategy,
-    DefaultVerificationKeyStrategy,
-)
-from jsonpath_ng.ext import parse
-import json
 
-
-from ...core.in_memory.profile import InMemoryProfile
-from ...wallet.did_method import KEY, DIDMethods
+from ...wallet.did_method import KEY
 from ...wallet.key_type import ED25519
-from ...wallet.in_memory import InMemoryWallet
 
-from ..jwt import jwt_sign, jwt_verify, resolve_public_key_by_kid_for_verify
-from ..sd_jwt import sd_jwt_sign, sd_jwt_verify
+from ..sd_jwt import SDJWTVerifyResult, sd_jwt_sign, sd_jwt_verify
 
 from .test_jwt import profile, in_memory_wallet
+
+
+@pytest.fixture
+def create_address_payload():
+    return {
+        "address": {
+            "street_address": "123 Main St",
+            "locality": "Anytown",
+            "region": "Anystate",
+            "country": "US",
+        }
+    }
 
 
 class TestSDJWT:
     """Tests for JWT sign and verify using dids."""
 
     seed = "testseed000000000000000000000001"
+    headers = {}
 
     @pytest.mark.asyncio
     async def test_sign_with_did_key_and_verify(self, profile, in_memory_wallet):
         did_info = await in_memory_wallet.create_local_did(KEY, ED25519, self.seed)
-        did = did_info.did
         verification_method = None
-        sd_list = [
-            "address",
-            "address.street_address",
-            "address.street_address.house_number",
-            "address.locality",
-            "address.region",
-            "address.country",
-            "given_name",
-            "family_name",
-            "email",
-            "phone_number",
-            "phone_number_verified",
-            "birthdate",
-            "updated_at",
-            "nationalities[1:3]",
-        ]
-        headers = {}
         payload = {
             "sub": "user_42",
             "given_name": "John",
@@ -68,10 +50,25 @@ class TestSDJWT:
             "updated_at": 1570000000,
             "nationalities": ["US", "DE", "SA"],
         }
+        sd_list = [
+            "address",
+            "address.street_address",
+            "address.street_address.house_number",
+            "address.locality",
+            "address.region",
+            "address.country",
+            "given_name",
+            "family_name",
+            "email",
+            "phone_number",
+            "phone_number_verified",
+            "birthdate",
+            "updated_at",
+            "nationalities[1:3]",
+        ]
         signed = await sd_jwt_sign(
-            profile, headers, payload, sd_list, did, verification_method
+            profile, self.headers, payload, sd_list, did_info.did, verification_method
         )
-
         assert signed
 
         assert await sd_jwt_verify(profile, signed)
