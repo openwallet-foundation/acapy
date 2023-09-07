f9d4abb34d5590829e8d172c0bcb54083fe63828
Jason Sherman <tools@usingtechnolo.gy>
2023-09-07 12:57:13 -0700

    Update devcontainer to read version from aries-cloudagent package
    Update sample files and documentation for ruff usage.
    
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -10,3 +10,6 @@ pip3 install -r demo/requirements.txt -r demo/requirements.behave.txt
 
 # install black for formatting
 pip3 install black
+
+# install a version of aries-cloudagent so the pytests can pick up a version
+pip3 install aries-cloudagent
