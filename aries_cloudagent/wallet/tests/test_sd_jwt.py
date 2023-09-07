a7993fcee8a19803659ec6c396157307e005c6c8
Char Howland <char@indicio.tech>
2023-09-07 12:21:32 -0600

    test: key binding implementation
    Signed-off-by: Char Howland <char@indicio.tech>

--- aries_cloudagent/wallet/tests/test_sd_jwt.py
+++ aries_cloudagent/wallet/tests/test_sd_jwt.py
@@ -2,8 +2,10 @@ from base64 import urlsafe_b64decode
 import json
 import pytest
 
+
 from ...wallet.did_method import KEY
 from ...wallet.key_type import ED25519
+from ...wallet.jwt import jwt_sign
 
 from ..sd_jwt import SDJWTVerifyResult, sd_jwt_sign, sd_jwt_verify
 
@@ -94,6 +96,9 @@ class TestSDJWT:
         assert sorted(revealed) == sorted(
             [disclosure[1] for disclosure in verified.disclosures]
         )
+        assert verified.payload["iss"] == payload["iss"]
+        assert verified.payload["iat"] == payload["iat"]
+        assert verified.payload["exp"] == payload["exp"]
 
     @pytest.mark.asyncio
     async def test_flat_structure(
@@ -216,3 +221,221 @@ class TestSDJWT:
                 assert nationality in payload["nationalities"]
         assert verified.payload["_sd_alg"]
         assert verified.disclosures[0][1] == "US"
+
+    @pytest.mark.asyncio
+    async def test_sd_jwt_key_binding(self, profile, in_memory_wallet):
+        did_info = await in_memory_wallet.create_local_did(KEY, ED25519, self.seed)
+        verification_method = None
+
+        payload = {
+            "given_name": "John",
+            "family_name": "Doe",
+            "iss": "https://example.com/issuer",
+            "iat": 1683000000,
+            "exp": 1883000000,
+            "cnf": {
+                "jwk": {
+                    "kty": "EC",
+                    "crv": "P-256",
+                    "x": "TCAER19Zvu3OHF4j4W4vfSVoHIP1ILilDls7vCeGemc",
+                    "y": "ZxjiWWbZMQGHVWKVQ4hbSIirsVfuecCE6t4jT9F2HZQ",
+                }
+            },
+        }
+        signed = await sd_jwt_sign(
+            profile,
+            self.headers,
+            payload,
+            did=did_info.did,
+            verification_method=verification_method,
+        )
+        assert signed
+
+        # Key binding
+        headers_kb = {"alg": "ES256", "typ": "kb+jwt"}
+        payload_kb = {
+            "nonce": "1234567890",
+            "aud": "https://example.com/verifier",
+            "iat": 1688160483,
+        }
+        signed_kb = await jwt_sign(
+            profile,
+            headers_kb,
+            payload_kb,
+            did_info.did,
+            verification_method,
+        )
+        assert signed_kb
+
+        assert await sd_jwt_verify(
+            profile,
+            f"{signed}{signed_kb}",
+            expected_aud=payload_kb["aud"],
+            expected_nonce=payload_kb["nonce"],
+        )
+
+    test_input = [
+        (
+            "Either both expected_aud and expected_nonce must be provided or both must be None",
+            {
+                "given_name": "John",
+                "family_name": "Doe",
+                "iss": "https://example.com/issuer",
+                "iat": 1683000000,
+                "exp": 1883000000,
+                "cnf": {
+                    "jwk": {
+                        "kty": "EC",
+                        "crv": "P-256",
+                        "x": "TCAER19Zvu3OHF4j4W4vfSVoHIP1ILilDls7vCeGemc",
+                        "y": "ZxjiWWbZMQGHVWKVQ4hbSIirsVfuecCE6t4jT9F2HZQ",
+                    }
+                },
+            },
+            {"alg": "ES256", "typ": "kb+jwt"},
+            "https://example.com/verifier",
+            None,
+        ),
+        (
+            "No holder public key in SD-JWT",
+            {
+                "given_name": "John",
+                "family_name": "Doe",
+                "iss": "https://example.com/issuer",
+                "iat": 1683000000,
+                "exp": 1883000000,
+            },
+            {"alg": "ES256", "typ": "kb+jwt"},
+            "https://example.com/verifier",
+            "1234567890",
+        ),
+        (
+            "The holder_public_key_payload is malformed. It doesn't contain the claim jwk: ",
+            {
+                "given_name": "John",
+                "family_name": "Doe",
+                "iss": "https://example.com/issuer",
+                "iat": 1683000000,
+                "exp": 1883000000,
+                "cnf": {"y": "ZxjiWWbZMQGHVWKVQ4hbSIirsVfuecCE6t4jT9F2HZQ"},
+            },
+            {"alg": "ES256", "typ": "kb+jwt"},
+            "https://example.com/verifier",
+            "1234567890",
+        ),
+        (
+            "Invalid header typ",
+            {
+                "given_name": "John",
+                "family_name": "Doe",
+                "iss": "https://example.com/issuer",
+                "iat": 1683000000,
+                "exp": 1883000000,
+                "cnf": {
+                    "jwk": {
+                        "kty": "EC",
+                        "crv": "P-256",
+                        "x": "TCAER19Zvu3OHF4j4W4vfSVoHIP1ILilDls7vCeGemc",
+                        "y": "ZxjiWWbZMQGHVWKVQ4hbSIirsVfuecCE6t4jT9F2HZQ",
+                    }
+                },
+            },
+            {"alg": "ES256", "typ": "JWT"},
+            "https://example.com/verifier",
+            "1234567890",
+        ),
+        (
+            "Invalid audience",
+            {
+                "given_name": "John",
+                "family_name": "Doe",
+                "iss": "https://example.com/issuer",
+                "iat": 1683000000,
+                "exp": 1883000000,
+                "cnf": {
+                    "jwk": {
+                        "kty": "EC",
+                        "crv": "P-256",
+                        "x": "TCAER19Zvu3OHF4j4W4vfSVoHIP1ILilDls7vCeGemc",
+                        "y": "ZxjiWWbZMQGHVWKVQ4hbSIirsVfuecCE6t4jT9F2HZQ",
+                    }
+                },
+            },
+            {"alg": "ES256", "typ": "kb+jwt"},
+            "invalid_aud",
+            "1234567890",
+        ),
+        (
+            "Invalid nonce",
+            {
+                "given_name": "John",
+                "family_name": "Doe",
+                "iss": "https://example.com/issuer",
+                "iat": 1683000000,
+                "exp": 1883000000,
+                "cnf": {
+                    "jwk": {
+                        "kty": "EC",
+                        "crv": "P-256",
+                        "x": "TCAER19Zvu3OHF4j4W4vfSVoHIP1ILilDls7vCeGemc",
+                        "y": "ZxjiWWbZMQGHVWKVQ4hbSIirsVfuecCE6t4jT9F2HZQ",
+                    }
+                },
+            },
+            {"alg": "ES256", "typ": "kb+jwt"},
+            "https://example.com/verifier",
+            "invalid_nonce",
+        ),
+    ]
+
+    @pytest.mark.parametrize(
+        "error, payload, headers_kb, expected_aud, expected_nonce", test_input
+    )
+    @pytest.mark.asyncio
+    async def test_sd_jwt_key_binding_errors(
+        self,
+        payload,
+        error,
+        expected_nonce,
+        headers_kb,
+        expected_aud,
+        profile,
+        in_memory_wallet,
+    ):
+        did_info = await in_memory_wallet.create_local_did(KEY, ED25519, self.seed)
+        verification_method = None
+
+        signed = await sd_jwt_sign(
+            profile,
+            self.headers,
+            payload,
+            did=did_info.did,
+            verification_method=verification_method,
+        )
+        assert signed
+
+        # Key binding
+        payload_kb = {
+            "nonce": "1234567890",
+            "aud": "https://example.com/verifier",
+            "iat": 1688160483,
+        }
+        signed_kb = await jwt_sign(
+            profile,
+            headers_kb,
+            payload_kb,
+            did_info.did,
+            verification_method,
+        )
+        assert signed_kb
+
+        with pytest.raises(
+            ValueError,
+            match=error,
+        ):
+            await sd_jwt_verify(
+                profile,
+                f"{signed}{signed_kb}",
+                expected_aud=expected_aud,
+                expected_nonce=expected_nonce,
+            )
