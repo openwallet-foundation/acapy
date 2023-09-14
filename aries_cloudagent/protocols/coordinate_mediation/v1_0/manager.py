0b4b133683669272fe01c7f268fccf916f7f60bd
Alex Walker <alex.walker@indicio.tech>
2023-09-14 10:57:48 -0700

    fix: import normalize_from_public_Key
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- aries_cloudagent/protocols/coordinate_mediation/v1_0/manager.py
+++ aries_cloudagent/protocols/coordinate_mediation/v1_0/manager.py
@@ -26,7 +26,7 @@ from .messages.mediate_deny import MediationDeny
 from .messages.mediate_grant import MediationGrant
 from .messages.mediate_request import MediationRequest
 from .models.mediation_record import MediationRecord
-from .normalization import normalize_from_did_key
+from .normalization import normalize_from_did_key, normalize_from_public_key
 
 LOGGER = logging.getLogger(__name__)
 
