b43f174d01ff77a2969cd4527066cb2af314a394
Daniel Bluhm <dbluhm@pm.me>
2023-10-26 23:31:54 -0400

    fix: mock logger.exception isn't a coroutine
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- aries_cloudagent/protocols/issue_credential/v2_0/handlers/tests/test_cred_offer_handler.py
+++ aries_cloudagent/protocols/issue_credential/v2_0/handlers/tests/test_cred_offer_handler.py
@@ -116,7 +116,7 @@ class TestV20CredOfferHandler(IsolatedAsyncioTestCase):
             with mock.patch.object(
                 responder, "send_reply", mock.CoroutineMock()
             ) as mock_send_reply, mock.patch.object(
-                handler._logger, "exception", mock.CoroutineMock()
+                handler._logger, "exception", mock.MagicMock()
             ) as mock_log_exc:
                 await handler.handle(request_context, responder)
                 mock_log_exc.assert_called_once()
