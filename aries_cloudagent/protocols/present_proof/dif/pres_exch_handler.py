677cd14274a6079782a9c34a6824a8fcbbc4c00d
Robert Simpson <RobS.git9@MailScreen.com>
2023-11-25 20:14:42 +0000

    feature: apply_constraint failReason
    Signed-off-by: Robert Simpson <RobS.git9@MailScreen.com>

--- aries_cloudagent/protocols/present_proof/dif/pres_exch_handler.py
+++ aries_cloudagent/protocols/present_proof/dif/pres_exch_handler.py
@@ -1441,7 +1441,7 @@ class DIFPresExchHandler:
         self, constraint: Constraints, cred_dict: dict
     ) -> str:
         """Evaluate constraint from the request against received credential."""
-        """If successful, returns None, else returns string with failure reason"""
+        """If successful, returns None, otherwise returns string with reason for failure"""
         fields = constraint._fields
         field_paths = []
         credential = self.create_vcrecord(cred_dict)
@@ -1450,7 +1450,7 @@ class DIFPresExchHandler:
             if is_limit_disclosure:
                 field = await self.get_updated_field(field, cred_dict)
             if not await self.filter_by_field(field, credential):
-                return 'Credential is not applicable for field "{field}"'
+                return f'Credential is not applicable for field "{field}"'
             field_paths = field_paths + (
                 await self.restrict_field_paths_one_of_filter(
                     field_paths=field.paths, cred_dict=cred_dict
@@ -1509,7 +1509,7 @@ class DIFPresExchHandler:
         self, extracted_dict: dict, nested_attr_values: dict
     ) -> str:
         """Check if keys of extracted_dict exists in nested_attr_values."""
-        """If successful, returns None, else returns string with failure reason"""
+        """If successful, returns None, otherwise returns string with reason for failure"""
         for attrs in extracted_dict.keys():
             if attrs not in nested_attr_values:
                 return f'{"at least one of " if isinstance(attrs, list) else ""}{attrs} not in field paths'
