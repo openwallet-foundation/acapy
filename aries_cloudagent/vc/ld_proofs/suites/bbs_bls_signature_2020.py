d489877bb419a771a1c8d4da4ed8cc8f902d242c
Daniel Bluhm <dbluhm@pm.me>
2023-12-07 15:05:41 -0500

    fix: no milliseconds in proof created (take 2)
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- aries_cloudagent/vc/ld_proofs/suites/bbs_bls_signature_2020.py
+++ aries_cloudagent/vc/ld_proofs/suites/bbs_bls_signature_2020.py
@@ -63,7 +63,7 @@ class BbsBlsSignature2020(BbsBlsSignature2020Base):
             date = self.date or datetime.now(timezone.utc)
             if not date.tzinfo:
                 date = utc.localize(date)
-            proof["created"] = date.isoformat()
+            proof["created"] = date.isoformat(timespec="seconds")
 
         # Allow purpose to update the proof; the `proof` is in the
         # SECURITY_CONTEXT_URL `@context` -- therefore the `purpose` must
--- aries_cloudagent/vc/ld_proofs/suites/linked_data_signature.py
+++ aries_cloudagent/vc/ld_proofs/suites/linked_data_signature.py
@@ -100,7 +100,7 @@ class LinkedDataSignature(LinkedDataProof, metaclass=ABCMeta):
             date = self.date or datetime.now(timezone.utc)
             if not date.tzinfo:
                 date = utc.localize(date)
-            proof["created"] = date.isoformat()
+            proof["created"] = date.isoformat(timespec="seconds")
 
         # Allow purpose to update the proof; the `proof` is in the
         # SECURITY_CONTEXT_URL `@context` -- therefore the `purpose` must
