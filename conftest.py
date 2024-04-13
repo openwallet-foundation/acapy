478210954ca6201ae1cdc91683f3747a559fe63a
Daniel Bluhm <dbluhm@pm.me>
2024-04-13 15:34:27 -0400

    feat: drop indy sdk
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- conftest.py
+++ conftest.py
@@ -24,41 +24,6 @@ class Stub:
         self.inner and self.inner.stop()
 
 
-def stub_indy() -> Stub:
-    # detect indy module
-    try:
-        from indy.libindy import _cdll
-
-        _cdll()
-
-        return Stub(None)
-    except ImportError:
-        print("Skipping Indy-specific tests: python3-indy module not installed.")
-    except OSError:
-        print(
-            "Skipping Indy-specific tests: libindy shared library could not be loaded."
-        )
-
-    modules = {}
-    package_name = "indy"
-    modules[package_name] = mock.MagicMock()
-    for mod in [
-        "anoncreds",
-        "blob_storage",
-        "crypto",
-        "did",
-        "error",
-        "pool",
-        "ledger",
-        "non_secrets",
-        "pairwise",
-        "wallet",
-    ]:
-        submod = f"{package_name}.{mod}"
-        modules[submod] = mock.MagicMock()
-    return Stub(mock.patch.dict(sys.modules, modules))
-
-
 def stub_anoncreds() -> Stub:
     # detect anoncreds library
     try:
