65365f13ec30bd0ed59e1a08012584de4b7f4d2f
jamshale <jamiehalebc@gmail.com>
2024-06-14 16:26:53 +0000

    Add a couple more unit tests
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- aries_cloudagent/anoncreds/default/legacy_indy/tests/test_recover.py
+++ aries_cloudagent/anoncreds/default/legacy_indy/tests/test_recover.py
@@ -134,6 +134,23 @@ class TestLegacyIndyRecover(IsolatedAsyncioTestCase):
         )
         assert result == {}
 
+        # Logs waring when ledger has revoked indexes not in wallet
+        with mock.patch(
+            "aries_cloudagent.anoncreds.default.legacy_indy.recover.LOGGER"
+        ) as mock_logger:
+            result = await generate_ledger_rrrecovery_txn(
+                GENESIS,
+                RevList(
+                    issuer_id="CsQY9MGeD3CQP4EyuVFo5m",
+                    current_accumulator="21 124C594B6B20E41B681E92B2C43FD165EA9E68BC3C9D63A82C8893124983CAE94 21 124C5341937827427B0A3A32113BD5E64FB7AB39BD3E5ABDD7970874501CA4897 6 5438CB6F442E2F807812FD9DC0C39AFF4A86B1E6766DBB5359E86A4D70401B0F 4 39D1CA5C4716FFC4FE0853C4FF7F081DFD8DF8D2C2CA79705211680AC77BF3A1 6 70504A5493F89C97C225B68310811A41AD9CD889301F238E93C95AD085E84191 4 39582252194D756D5D86D0EED02BF1B95CE12AED2FA5CD3C53260747D891993C",
+                    revocation_list=[1, 0, 0, 0],
+                    timestamp=1669640864487,
+                    rev_reg_def_id="4xE68b6S5VRFrKMMG1U95M:4:4xE68b6S5VRFrKMMG1U95M:3:CL:59232:default:CL_ACCUM:4ae1cc6c-f6bd-486c-8057-88f2ce74e960",
+                ),
+            )
+            assert mock_logger.warning.called
+            assert result == {}
+
     @mock.patch.object(
         aiohttp,
         "ClientSession",
