5c883bf0b54a485b25497c00b97bf2579e2ad33a
Ricky Ng-Adam <ricky.ng-adam-ext@mcn.gouv.qc.ca>
2024-08-19 16:25:11 -0400

    fixes #3182: migrate from ptvsd to debugpy
    Signed-off-by: Ricky Ng-Adam <ricky.ng-adam-ext@mcn.gouv.qc.ca>

--- aries_cloudagent/__main__.py
+++ aries_cloudagent/__main__.py
@@ -20,16 +20,16 @@ def init_debug(args):
 
     # --debug to use microsoft's visual studio remote debugger
     if ENABLE_PTVSD or "--debug" in args:
+        PTVSD_HOST = os.getenv("PTVSD_HOST", "localhost")
+        PTVSD_PORT = os.getenv("PTVSD_PORT", 5678)
         try:
-            import ptvsd
+            import debugpy
 
-            ptvsd.enable_attach()
-            print("ptvsd is running")
+            debugpy.listen((PTVSD_HOST, PTVSD_PORT))
             print("=== Waiting for debugger to attach ===")
-            # To pause execution until the debugger is attached:
-            ptvsd.wait_for_attach()
+            debugpy.wait_for_client()
         except ImportError:
-            print("ptvsd library was not found")
+            print("debugpy library was not found")
 
     if ENABLE_PYDEVD_PYCHARM or "--debug-pycharm" in args:
         try:
