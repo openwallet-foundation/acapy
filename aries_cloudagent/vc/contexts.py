bf88b9ec8b502bb684a6ecbcd1230b311282ebcd
Patrick <patrick.st-louis@opsecid.ca>
2024-09-08 20:49:45 +0000

    group context urls
    Signed-off-by: Patrick <patrick.st-louis@opsecid.ca>

---
+++ aries_cloudagent/vc/contexts.py
@@ -0,0 +1,14 @@
+"""Context URLS."""
+
+DID_V1 = "https://www.w3.org/ns/did/v1"
+CREDENTIALS_V1 = "https://www.w3.org/2018/credentials/v1"
+CREDENTIALS_V2 = "https://www.w3.org/ns/credentials/v2"
+
+SECURITY_V1 = "https://w3id.org/security/v1"
+SECURITY_V2 = "https://w3id.org/security/v2"
+SECURITY_V3_UNSTABLE = "https://w3id.org/security/v3-unstable"
+SECURITY_DATA_INTEGRITY_V2 = "https://w3id.org/security/data-integrity/v2"
+SECURITY_BBS_V1 = "https://w3id.org/security/bbs/v1"
+SECURITY_SUITES_ED25519_2020_V1 = "https://w3id.org/security/suites/ed25519-2020/v1"
+SECURITY_MULTIKEY_V1 = "https://w3id.org/security/multikey/v1"
+SECURITY_JWK_V1 = "https://w3id.org/security/jwk/v1"
--- aries_cloudagent/vc/vc_di/cryptosuites/__init__.py
+++ aries_cloudagent/vc/vc_di/cryptosuites/__init__.py
@@ -1,7 +1,5 @@
 from .eddsa_jcs_2022 import EddsaJcs2022
 
-CONTEXTS = {"data-integrity-v2": "https://w3id.org/security/data-integrity/v2"}
-
 CRYPTOSUITES = {
     "eddsa-jcs-2022": EddsaJcs2022,
 }
