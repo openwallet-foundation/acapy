121af795a19f607cc328f019bfa3534ae208d303
Patrick <patrick.st-louis@opsecid.ca>
2024-09-15 23:28:28 +0000

    fix unit tests
    Signed-off-by: Patrick <patrick.st-louis@opsecid.ca>

--- aries_cloudagent/vc/vc_di/tests/test_cryptosuites.py
+++ aries_cloudagent/vc/vc_di/tests/test_cryptosuites.py
@@ -1,16 +1,17 @@
 """Test Data Integrity Cryptosuites."""
 
+from unittest import IsolatedAsyncioTestCase
 from ..cryptosuites import CRYPTOSUITES
 from ....wallet.key_type import ED25519
 from ....wallet.in_memory import InMemoryWallet
 from ....core.in_memory import InMemoryProfile
-from ....did.did_key import DIDKey
+from ....wallet.did_method import KEY, DIDMethods
 
+TEST_SEED = "00000000000000000000000000000000"
 
-class TestEddsaJcs2022:
+class TestEddsaJcs2022(IsolatedAsyncioTestCase):
     """Tests for DI sign and verify."""
-
-    test_seed = "00000000000000000000000000000000"
+    
     unsecured_document = {"hello": "world"}
     options = {
         "type": "DataIntegrityProof",
@@ -18,33 +19,37 @@ class TestEddsaJcs2022:
         "proofPurpose": "assertionMethod",
         "verificationMethod": "did:key:z6MkgKA7yrw5kYSiDuQFcye4bMaJpcfHFry3Bx45pdWh3s8i#z6MkgKA7yrw5kYSiDuQFcye4bMaJpcfHFry3Bx45pdWh3s8i",
     }
-    cryptosuite = CRYPTOSUITES[options["cryptosuite"]](
-        profile=InMemoryProfile.test_profile()
-    )
-
+    
     async def asyncSetUp(self):
-        await InMemoryWallet(InMemoryProfile.test_profile()).create_signing_key(
-            key_type=ED25519, seed=self.test_seed
+        self.profile = InMemoryProfile.test_profile({},{DIDMethods: DIDMethods()})
+        self.wallet = InMemoryWallet(self.profile)
+        await self.wallet.create_local_did(
+            method=KEY, key_type=ED25519, seed=TEST_SEED
+        )
+        self.cryptosuite = CRYPTOSUITES[self.options["cryptosuite"]](
+            profile=self.profile
         )
 
     async def test_add_proof(self):
         secured_document = await self.cryptosuite.add_proof(
-            self.unsecured_document, self.options
+            document=self.unsecured_document, proof_options=self.options
         )
         proof = secured_document.pop("proof", None)
         assert isinstance(proof, list)
         assert len(proof) == 1
-        assert proof["type"] == self.options["type"]
-        assert proof["cryptosuite"] == self.options["cryptosuite"]
-        assert proof["proofPurpose"] == self.options["proofPurpose"]
-        assert proof["verificationMethod"] == self.options["verificationMethod"]
-        assert proof["proofValue"]
+        assert proof[0]["type"] == self.options["type"]
+        assert proof[0]["cryptosuite"] == self.options["cryptosuite"]
+        assert proof[0]["proofPurpose"] == self.options["proofPurpose"]
+        assert proof[0]["verificationMethod"] == self.options["verificationMethod"]
+        assert proof[0]["proofValue"]
 
     async def test_proof_set(self):
         secured_document = await self.cryptosuite.add_proof(
             self.unsecured_document, self.options
         )
-        secured_document = await self.cryptosuite.add_proof(secured_document, self.options)
+        secured_document = await self.cryptosuite.add_proof(
+            secured_document, self.options
+        )
         proof_set = secured_document.pop("proof", None)
         assert isinstance(proof_set, list)
         assert len(proof_set) == 2
@@ -60,19 +65,21 @@ class TestEddsaJcs2022:
             self.unsecured_document, self.options
         )
         proof = secured_document.pop("proof", None)
-        assert await self.cryptosuite.verify_proof(secured_document, proof)
-        bad_proof = proof.copy()
+        assert await self.cryptosuite.verify_proof(secured_document, proof[0])
+        bad_proof = proof[0].copy()
         bad_proof["proofValue"] = bad_proof["proofValue"][:-1]
-        assert not await self.cryptosuite.verify_proof(secured_document, proof)
+        assert not await self.cryptosuite.verify_proof(secured_document, bad_proof)
 
     async def test_eddsa_jcs_2022_verify_proof_set(self):
         secured_document = await self.cryptosuite.add_proof(
             self.unsecured_document, self.options
         )
-        secured_document = await self.cryptosuite.add_proof(secured_document, self.options)
+        secured_document = await self.cryptosuite.add_proof(
+            secured_document, self.options
+        )
         proof_set = secured_document.pop("proof", None)
         for proof in proof_set:
             assert await self.cryptosuite.verify_proof(secured_document, proof)
             bad_proof = proof.copy()
             bad_proof["proofValue"] = bad_proof["proofValue"][:-1]
-            assert not await self.cryptosuite.verify_proof(secured_document, proof)
+            assert not await self.cryptosuite.verify_proof(secured_document, bad_proof)
