3ffde212e41aa305f0b8b426e4a218bcb102dc23
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2024-09-18 10:56:56 -0700

    Ensure that DAP_PORT is always an int
    Signed-off-by: Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>

--- aries_cloudagent/__main__.py
+++ aries_cloudagent/__main__.py
@@ -21,7 +21,7 @@ def init_debug(args):
     # --debug to use microsoft's visual studio remote debugger
     if ENABLE_PTVSD or "--debug" in args:
         DAP_HOST = os.getenv("PTVSD_HOST", None) or os.getenv("DAP_HOST", "localhost")
-        DAP_PORT = os.getenv("PTVSD_PORT", None) or os.getenv("DAP_PORT", 5678)
+        DAP_PORT = int(os.getenv("PTVSD_PORT", None) or os.getenv("DAP_PORT", 5678))
         try:
             import debugpy
 
