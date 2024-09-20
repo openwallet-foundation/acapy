87fdcca591dcf7470a62593e673085c1e26e1db5
PatStLouis <patrick.st-louis@opsecid.ca>
2024-09-20 18:09:23 +0000

    add 2 more tests
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- aries_cloudagent/wallet/keys/tests/test_key_operations.py
+++ aries_cloudagent/wallet/keys/tests/test_key_operations.py
@@ -10,18 +10,30 @@ class TestKeyOperations(IsolatedAsyncioTestCase):
     manager = MultikeyManager(profile=profile)
     seed = "00000000000000000000000000000000"
     multikey = "z6MkgKA7yrw5kYSiDuQFcye4bMaJpcfHFry3Bx45pdWh3s8i"
+    verkey = "2ru5PcgeQzxF7QZYwQgDkG2K13PRqyigVw99zMYg8eML"
     kid = "did:web:example.com#key-01"
 
     async def test_key_creation(self):
         key_info = await self.manager.create(seed=self.seed)
         assert key_info["multikey"] == self.multikey
         assert key_info["kid"] is None
+        
         key_info = await self.manager.from_multikey(multikey=self.multikey)
         assert key_info["multikey"] == self.multikey
         assert key_info["kid"] is None
+        
         key_info = await self.manager.update(multikey=self.multikey, kid=self.kid)
         assert key_info["multikey"] == self.multikey
         assert key_info["kid"] == self.kid
+        
         key_info = await self.manager.from_kid(kid=self.kid)
         assert key_info["multikey"] == self.multikey
         assert key_info["kid"] == self.kid
+        
+        assert self.manager.kid_exists(self.kid)
+        
+        
+
+    async def test_key_representations(self):
+        assert self.manager._multikey_to_verkey(self.multikey) == self.verkey
+        assert self.manager._verkey_to_multikey(self.verkey) == self.multikey
