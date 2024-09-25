e3ab4488cce49e40633f57ff527f9b518413b7e4
Stephen Curran <swcurran@gmail.com>
2024-09-25 12:53:00 -0700

    1.0.1rc0
    Signed-off-by: Stephen Curran <swcurran@gmail.com>

---
+++ scripts/genChangeLog.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if ! command -v gh 2>&1 >/dev/null; then
+   echo ERROR: This script requires that `gh` needs to be installed to run.
+   exit 1
+fi
+
+if ! command -v jq 2>&1 >/dev/null; then
+   echo ERROR: This script requires that `jq` needs to be installed to run.
+   exit 1
+fi
+
+if [ $# -eq 0 ]; then
+    echo ${0}: Generate a list of PRs to include in the Changelog for a release.
+    echo "You must supply a date argument in the format '2024-08-12'"
+    echo "The date must be the date of the day before the last ACA-Py release -- to make sure you get all of the relevant PRs."
+    echo "The output is the list of non-dependabot PRs, plus some markdown to reference the dependabot PRs"
+    exit 1
+fi
+
+gh pr list -S "merged:>${1}"  -L 1000 --state merged --json number,title,author | \
+   jq ' .[] | ["  -",.title,"WwW",.number,"XxX",.number,"YyY",.author.login,"ZzZ",.author.login] | @tsv' | \
+   sed -e "s/\\\t/ /g" \
+      -e "s/\"//g" \
+      -e "s/WwW /\[\\\#/" \
+      -e "s# XxX #\\](https://github.com/hyperledger/aries-cloudagent-python/pull/#" \
+      -e "s/ YyY /) \\[/" \
+      -e "s# ZzZ #\\](https://github.com/#" \
+      -e "s/$/)/" \
+      -e "/app.dependabot/d"
+now=$(date +%Y-%m-%d)
+echo "- Dependabot PRs"
+echo "  - [Link to list of Dependabot PRs in this release](https://github.com/hyperledger/aries-cloudagent-python/pulls?q=is%3Apr+is%3Amerged+merged%3A${1}..${now}+author%3Aapp%2Fdependabot+)"
