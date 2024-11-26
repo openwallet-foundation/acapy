9af952e6e196b11e133c23d833afc741ba521a4c
jamshale <jamiehalebc@gmail.com>
2024-11-26 18:32:52 +0000

    Add test wallet config option
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- acapy_agent/askar/profile.py
+++ acapy_agent/askar/profile.py
@@ -324,7 +324,9 @@ class AskarProfileManager(ProfileManager):
     ) -> Profile:
         """Provision a new instance of a profile."""
         store_config = AskarStoreConfig(config)
-        opened = await store_config.open_store(provision=True)
+        opened = await store_config.open_store(
+            provision=True, in_memory=config.get("test")
+        )
         return AskarProfile(opened, context)
 
     async def open(
@@ -332,7 +334,9 @@ class AskarProfileManager(ProfileManager):
     ) -> Profile:
         """Open an instance of an existing profile."""
         store_config = AskarStoreConfig(config)
-        opened = await store_config.open_store(provision=False)
+        opened = await store_config.open_store(
+            provision=False, in_memory=config.get("test")
+        )
         return AskarProfile(opened, context)
 
     @classmethod
