6f61aebcfef11d27c7ac9d85d7be7c7cbed98b9d
Colton Wolkins (Laptop) <colton@indicio.tech>
2024-12-04 02:45:01 -0700

    feat: Add trust-ping protocol
    Signed-off-by: Colton Wolkins (Laptop) <colton@indicio.tech>

--- acapy_agent/protocols_v2/trustping/v1_0/message_types.py
+++ acapy_agent/protocols_v2/trustping/v1_0/message_types.py
@@ -14,33 +14,95 @@ SPEC_URI = (
 # Message types
 PING = "trust_ping/1.0/ping"
 PING_RESPONSE = "trust_ping/1.0/ping_response"
-DEBUG = "https://didcomm.org/basicmessage/2.0/message"
+PING = "https://didcomm.org/trust-ping/2.0/ping"
+BASIC_MESSAGE = "https://didcomm.org/basicmessage/2.0/message"
+QUERIES = "https://didcomm.org/discover-features/2.0/queries"
+DISCLOSE = "https://didcomm.org/discover-features/2.0/disclose"
+EMPTY = "https://didcomm.org/empty/1.0/empty"
 
 PROTOCOL_PACKAGE = "acapy_agent.protocols_v2.trustping.v1_0"
 
-class test_func:
+class trust_ping:
     async def __call__(self, *args, **kwargs):
         await self.handle(*args, **kwargs)
     @staticmethod
     async def handle(context, responder, payload):
         logger = logging.getLogger(__name__)
+        if not payload["body"].get("response_requested", False):
+            return
+        their_did = context.message_receipt.sender_verkey.split('#')[0]
+        our_did = context.message_receipt.recipient_verkey.split('#')[0]
+        error_result = V2AgentMessage(
+            message={
+                "type": "https://didcomm.org/trust-ping/2.0/ping-response",
+                "thid": payload["id"],
+                "body": {},
+                "to": [their_did],
+                "from": our_did,
+            }
+        )
+        await responder.send_reply(error_result)
+
+
+class basic_message:
+    async def __call__(self, *args, **kwargs):
+        await self.handle(*args, **kwargs)
+    @staticmethod
+    async def handle(context, responder, payload):
+        logger = logging.getLogger(__name__)
+        their_did = context.message_receipt.sender_verkey.split('#')[0]
+        our_did = context.message_receipt.recipient_verkey.split('#')[0]
         error_result = V2AgentMessage(
             message={
                 "type": "https://didcomm.org/basicmessage/2.0/message",
                 "body": {
                     "content": "Hello from acapy",
                 },
-                "to": [context.message_receipt.sender_verkey.split('#')[0]],
-                "from": context.message_receipt.recipient_verkey.split('#')[0],
+                "to": [their_did],
+                "from": our_did,
                 "lang": "en",
             }
         )
         await responder.send_reply(error_result)
 
+
+class discover_features:
+    async def __call__(self, *args, **kwargs):
+        await self.handle(*args, **kwargs)
+    @staticmethod
+    async def handle(context, responder, payload):
+        logger = logging.getLogger(__name__)
+        their_did = context.message_receipt.sender_verkey.split('#')[0]
+        our_did = context.message_receipt.recipient_verkey.split('#')[0]
+        error_result = V2AgentMessage(
+            message={
+                "type": DISCLOSE,
+                "thid": payload["id"],
+                "body": {
+                    "disclosures": [
+                        {
+                            "feature-type": "protocol",
+                            "id": protocol.rsplit("/", 1)[0],
+                        }
+                        for protocol in [PING, BASIC_MESSAGE, QUERIES, EMPTY]
+                    ],
+                },
+                "to": [their_did],
+                "from": our_did,
+            }
+        )
+        await responder.send_reply(error_result)
+
+
+
 HANDLERS = {
-    DEBUG: f"{PROTOCOL_PACKAGE}.message_types.test_func",
+    PING: f"{PROTOCOL_PACKAGE}.message_types.trust_ping",
+    BASIC_MESSAGE: f"{PROTOCOL_PACKAGE}.message_types.basic_message",
+    QUERIES: f"{PROTOCOL_PACKAGE}.message_types.discover_features",
 }.items()
 
 MESSAGE_TYPES = {
-        DEBUG: f"{PROTOCOL_PACKAGE}.message_types.test_func",
+        PING: f"{PROTOCOL_PACKAGE}.message_types.trust_ping",
+        BASIC_MESSAGE: f"{PROTOCOL_PACKAGE}.message_types.basic_message",
+        QUERIES: f"{PROTOCOL_PACKAGE}.message_types.discover_features",
 }
