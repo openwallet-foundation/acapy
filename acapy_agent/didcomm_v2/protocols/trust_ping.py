be0dfeecfdbf27eabba8540e2c85e7b9b6ec2c65
Colton Wolkins (Laptop) <colton@indicio.tech>
2024-12-20 10:46:53 -0700

    chore: Cleanup PR
    Signed-off-by: Colton Wolkins (Laptop) <colton@indicio.tech>

--- acapy_agent/didcomm_v2/protocols/trust_ping.py
+++
@@ -1,8 +0,0 @@
-"""Handler for the trust ping protocol."""
-
-from acapy_agent.transport.inbound.message import InboundMessage
-
-
-async def handle_trust_ping(message: InboundMessage):
-    """."""
-    return False
