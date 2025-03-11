1b0b6e592f966d179d62e9e42eba25033b2e4b44
PatStLouis <patrick.st-louis@opsecid.ca>
2025-03-11 04:30:01 +0000

    return entire record
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- acapy_agent/protocols/out_of_band/v1_0/routes.py
+++ acapy_agent/protocols/out_of_band/v1_0/routes.py
@@ -253,7 +253,7 @@ async def invitation_fetch(request: web.BaseRequest):
     except StorageError as err:
         raise web.HTTPBadRequest(reason=err.roll_up) from err
 
-    return web.json_response(record.get("invitation"))
+    return web.json_response(record)
 
 
 @docs(
