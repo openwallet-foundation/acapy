b77505f0bd4a70722583f88a1cb655d74e106c7b
ff137 <ff137@proton.me>
2025-04-30 14:47:41 +0200

    :loud_sound: Logging in `emit_events`
    Signed-off-by: ff137 <ff137@proton.me>

--- acapy_agent/protocols/issue_credential/v2_0/models/cred_ex_record.py
+++ acapy_agent/protocols/issue_credential/v2_0/models/cred_ex_record.py
@@ -193,6 +193,10 @@ class V20CredExRecord(BaseExchangeRecord):
         """
 
         if not self.RECORD_TOPIC:
+            LOGGER.warning(
+                "Emit event called but RECORD_TOPIC is not set for %s",
+                self.RECORD_TYPE,
+            )
             return
 
         if self.state:
@@ -202,11 +206,13 @@ class V20CredExRecord(BaseExchangeRecord):
 
         # serialize payload before checking for webhook contents
         if not payload:
+            LOGGER.debug("Serializing payload for %s record", self.RECORD_TYPE)
             payload = self.serialize()
         if not session.profile.settings.get("debug.webhooks"):
             payload = V20CredExRecordWebhook(**payload)
             payload = payload.__dict__
 
+        LOGGER.debug("Emitting %s event for profile %s", topic, session.profile.name)
         await session.profile.notify(topic, payload)
 
     @property
