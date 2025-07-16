2c661bf17ad3e24501e09d4ae23193a5aff13bc7
ff137 <ff137@proton.me>
2025-07-16 22:14:23 +0300

    :bug:
    Signed-off-by: ff137 <ff137@proton.me>

--- acapy_agent/anoncreds/revocation/revocation.py
+++ acapy_agent/anoncreds/revocation/revocation.py
@@ -850,10 +850,8 @@ class AnonCredsRevocation:
         options = options or {}
 
         try:
-            rev_list_result = RevListResult.deserialize(result)
-
             # Store the revocation list
-            await self.store_revocation_registry_list(rev_list_result)
+            await self.store_revocation_registry_list(result)
 
             # Emit success event
             event = RevListStoreResponseEvent.with_payload(
