91355a44c14b559b1b4a24e189dbf47f18cc3f8a
ff137 <ff137@proton.me>
2025-08-25 23:19:13 +0200

    :sparkles: init module for new anoncreds.revocation package
    Signed-off-by: ff137 <ff137@proton.me>

---
+++ acapy_agent/anoncreds/revocation/__init__.py
@@ -0,0 +1,33 @@
+"""AnonCreds Revocation Package.
+
+This package contains all revocation-related functionality for AnonCreds,
+including credential revocation, registry management, and recovery operations.
+"""
+
+from .manager import RevocationManager, RevocationManagerError
+from .recover import RevocRecoveryException, fetch_txns, generate_ledger_rrrecovery_txn
+from .revocation import (
+    CATEGORY_REV_LIST,
+    CATEGORY_REV_REG_DEF,
+    CATEGORY_REV_REG_DEF_PRIVATE,
+    AnonCredsRevocation,
+    AnonCredsRevocationError,
+    AnonCredsRevocationRegistryFullError,
+)
+from .setup import DefaultRevocationSetup
+
+__all__ = [
+    "AnonCredsRevocationModuleResponseSchema",
+    "AnonCredsRevocationRegistryFullError",
+    "CATEGORY_REV_LIST",
+    "CATEGORY_REV_REG_DEF",
+    "CATEGORY_REV_REG_DEF_PRIVATE",
+    "RevocationManager",
+    "RevocationManagerError",
+    "RevocRecoveryException",
+    "fetch_txns",
+    "generate_ledger_rrrecovery_txn",
+    "AnonCredsRevocation",
+    "AnonCredsRevocationError",
+    "DefaultRevocationSetup",
+]
