910411cbb1d73552d060a93aa4411216ae8313f6
ff137 <ff137@proton.me>
2025-08-26 16:38:24 +0200

    :art: Remove importing test utils, requiring pytest
    Signed-off-by: ff137 <ff137@proton.me>

--- acapy_agent/anoncreds/routes/common/__init__.py
+++ acapy_agent/anoncreds/routes/common/__init__.py
@@ -11,32 +11,17 @@ from .schemas import (
     RevRegIdMatchInfoMixin,
     SchemaQueryFieldsMixin,
 )
-from .testing import (
-    BaseAnonCredsRouteTestCase,
-    BaseAnonCredsRouteTestCaseWithOutbound,
-    create_mock_request,
-    create_standard_rev_reg_def,
-    create_standard_rev_reg_def_value,
-)
 from .utils import (
     get_request_body_with_profile_check,
     get_revocation_registry_definition_or_404,
 )
 
 __all__ = [
-    # Schema mixins and definitions
-    "EndorserOptionsSchema",
-    "SchemaQueryFieldsMixin",
     "CredRevRecordQueryStringMixin",
+    "EndorserOptionsSchema",
     "RevRegIdMatchInfoMixin",
     "RevocationIdsDictMixin",
-    # Route utilities
-    "get_revocation_registry_definition_or_404",
+    "SchemaQueryFieldsMixin",
     "get_request_body_with_profile_check",
-    # Test fixtures and data
-    "BaseAnonCredsRouteTestCase",
-    "BaseAnonCredsRouteTestCaseWithOutbound",
-    "create_mock_request",
-    "create_standard_rev_reg_def",
-    "create_standard_rev_reg_def_value",
+    "get_revocation_registry_definition_or_404",
 ]
