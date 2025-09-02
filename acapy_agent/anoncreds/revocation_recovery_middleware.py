a5fc603856208f4a58299f535022feb02cf7afac
ff137 <ff137@proton.me>
2025-09-02 10:01:29 +0200

    :art: Make auto-recovery check disabled by default

--- acapy_agent/anoncreds/revocation_recovery_middleware.py
+++ acapy_agent/anoncreds/revocation_recovery_middleware.py
@@ -169,7 +169,7 @@ async def revocation_recovery_middleware(request: web.BaseRequest, handler: Coro
 
     # Check if automatic revocation recovery is enabled
     auto_recovery_enabled = profile.settings.get_bool(
-        "anoncreds.revocation.auto_recovery_enabled", default=True
+        "anoncreds.revocation.auto_recovery_enabled", default=False
     )
     LOGGER.debug(
         "Auto recovery enabled for profile %s: %s", profile_name, auto_recovery_enabled
