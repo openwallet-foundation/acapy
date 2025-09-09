47cc505cd3aa44aa552f1940a74289bbd71293d6
ff137 <ff137@proton.me>
2025-09-09 19:32:44 +0200

    :test_tube: Conftest with event loop fixture
    Signed-off-by: ff137 <ff137@proton.me>

---
+++ acapy_agent/core/tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+import asyncio
+
+
+@pytest.fixture(scope="function")
+def event_loop():
+    """
+    Custom function-scoped event loop.
+    """
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    asyncio.set_event_loop(loop)
+    yield loop
+    loop.close()
